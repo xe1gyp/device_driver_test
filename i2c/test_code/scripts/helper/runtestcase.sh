aed905162a019913a6c421c95d978f17a4e63e7c
Abraham Arce <x0066660@ti.com>
2008-12-09 11:02:30 -0600

    TestSuites: I2C: General Variables
    Adding missing variables to configuration files
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

--- i2c/test_code/scripts/helper/runtestcase.sh
+++ i2c/test_code/scripts/helper/runtestcase.sh
@@ -25,21 +25,22 @@ case "$TESTCASEID" in
 	# Transfer Cancellation to Existing Device
 	4)
 		verifyspeed.sh $MODE || exit 1 && for i in $I2C_ADDRESSES; do i2cdump -y $I2C_ADAPTER_1 $i b || exit 1; done
-		verifyspeed.sh $MODE || exit 1 && ((testrw.sh $I2C_ADAPTER_1 $I2C_REG_1 $I2C_REG_1_VALUE_INITIAL &) && testrw.sh $I2C_ADAPTER_1 $I2C_REG_2 $I2C_REG_2_VALUE_INITIAL &) && killall testrw.sh || exit 1
+		verifyspeed.sh $MODE || exit 1 && ((testrw.sh $I2C_ADAPTER_1 $I2C_REG_1 $I2C_REG_1_VALUE_INITIAL &) && testrw.sh $I2C_ADAPTER_1 $I2C_REG_2 $I2C_REG_2_VALUE_INITIAL &) && sleep 1 && killall testrw.sh || exit 1
 		verifyspeed.sh $MODE || exit 1 && for i in $I2C_ADDRESSES; do (i2cdump -y $I2C_ADAPTER_1 $i b &) && killall i2cdump || exit 1; done
 		verifyspeed.sh $MODE || exit 1 && for i in $I2C_ADDRESSES; do i2cdump -y $I2C_ADAPTER_1 $i b || exit 1; done
 	;;
-	# Transfer using Interrupt Mode
+	# Transfer using Interrupt Mode, the interrupts should be different due to i2c transfers
 	5)
 		verifyspeed.sh $MODE || exit 1 && for i in $I2C_ADDRESSES; do i2cdump -y $I2C_ADAPTER_1 $i b || exit 1; done
-		verifyspeed.sh $MODE || exit 1 && echo `cat $PROCFS_INTERRUPTS | grep $INT_24XX_I2C1_IRQ: | sed -e "s/ */ /g" | cut -d ' ' -f3` > $TEMP_FILE_1 || exit 1
+		verifyspeed.sh $MODE || exit 1 && echo `cat $PROCFS_INTERRUPTS | grep $INTERRUPT_PREFIX | grep $INT_24XX_I2C1_IRQ: | sed -e "s/ */ /g" | cut -d ' ' -f3` > $TEMP_FILE_1 || exit 1
 		verifyspeed.sh $MODE || exit 1 && for i in $I2C_ADDRESSES; do i2cdump -y $I2C_ADAPTER_1 $i b || exit 1; done
-		verifyspeed.sh $MODE || exit 1 && echo `cat $PROCFS_INTERRUPTS | grep $INT_24XX_I2C1_IRQ: | sed -e "s/ */ /g" | cut -d ' ' -f3` > $TEMP_FILE_2 || exit 1
+		verifyspeed.sh $MODE || exit 1 && echo `cat $PROCFS_INTERRUPTS | grep $INTERRUPT_PREFIX | grep $INT_24XX_I2C1_IRQ: | sed -e "s/ */ /g" | cut -d ' ' -f3` > $TEMP_FILE_2 || exit 1
 		verifyspeed.sh $MODE || exit 1 && cat $TEMP_FILE_1 && cat $TEMP_FILE_2; cmp $TEMP_FILE_1 $TEMP_FILE_2 && exit 1
 		verifyspeed.sh $MODE || exit 1 && for i in $I2C_ADDRESSES; do i2cdump -y $I2C_ADAPTER_1 $i b || exit 1; done
 	;;
 	*)
-		echo hola;
+		echo "No Test Case ID Found!";
+		return 1
 	;;
 esac
 
