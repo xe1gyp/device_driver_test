14abeaaa86f87740a5aaa2cd1a3bf83546675a50
Abraham Arce <x0066660@ti.com>
2008-12-08 19:52:41 -0600

    TestSuites: I2C: General Update
    Update scenarios according to new TestDesignSpec format
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

---
+++ i2c/test_code/scripts/helper/verifyspeed.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ADAPTER_INFO=`cat $DMESG_PATH | grep i2c | grep "bus 1"`
+SPEED_RECEIVED=$1
+SPEED_ACTUAL=`echo $ADAPTER_INFO | sed -e "s/ */ /g" | cut -d ' ' -f8`
+
+if [ "$SPEED_RECEIVED" = "$SPEED_ACTUAL" ]; then
+	return 0
+else
+	echo
+	echo "Adapter speed is not set to the required one"
+	echo "Requesting -> $SPEED_RECEIVED"
+	echo "Actual -> $SPEED_ACTUAL"
+	echo $ADAPTER_INFO
+	echo
+	return 1
+fi
+
+# End of file
