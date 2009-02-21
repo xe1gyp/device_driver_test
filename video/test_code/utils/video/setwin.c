5e76b3ebf8159966b37fc7b825e59d51f9c05d2f
Rene Gonzalez Salinas <renegs.2378@gmail.com>
2009-02-20 18:30:33 -0600

    TestSuits: Video: Coding style clean up
    This patch fix the file video/test_code/utils/video/setwin.c
    script in agreement to Linux coding style.
    
    Signed-off-by: Rene Gonzalez Salinas <renegs.2378@gmail.com>

--- video/test_code/utils/video/setwin.c
+++ video/test_code/utils/video/setwin.c
@@ -51,10 +51,10 @@ int main(int argc, char *argv[])
 		printf("Could not open %s\n",
 			(video_device == 1) ? VIDEO_DEVICE1 : VIDEO_DEVICE2);
 		return 1;
-	}
-	else
+	} else {
 		printf("openned %s\n",
 			(video_device == 1) ? VIDEO_DEVICE1 : VIDEO_DEVICE2);
+	}
 
 	result = ioctl(file_descriptor, VIDIOC_S_FMT, &format);
 	if (result != 0) {
