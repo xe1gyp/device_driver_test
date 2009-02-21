714afe3dcfcb0fdfae3c7c7aa66b21324cc12893
Rene Gonzalez Salinas <renegs.2378@gmail.com>
2009-02-20 18:46:17 -0600

    TestSuits: Video: Coding style clean up
    This patch fix the file video/test_code/utils/video/showinfo.c
    script in agreement to Linux coding style.
    
    Signed-off-by: Rene Gonzalez Salinas <renegs.2378@gmail.com>

--- video/test_code/utils/video/showinfo.c
+++ video/test_code/utils/video/showinfo.c
@@ -43,10 +43,10 @@ int main(int argc, char *argv[])
 		printf("Could not open %s\n",
 			(video_device == 1) ? VIDEO_DEVICE1 : VIDEO_DEVICE2);
 		return 1;
-	}
-	else
+	} else {
 		printf("openned %s\n",
 			(video_device == 1) ? VIDEO_DEVICE1 : VIDEO_DEVICE2);
+	}
 
 	result = show_info(file_descriptor);
 
