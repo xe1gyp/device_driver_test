fd478623a4e927429db7c9c38676811a5c33875e
David Miramontes <miramontes.david@gmail.com>
2009-03-09 12:51:08 -0600

    VIDEO: Scenario changed winstream
    The scenario shows the video with different size,
    increasing the heigth and width.
    
    Signed-off-by: David Miramontes <miramontes.david@gmail.com>

--- video/test_code/scripts/helper/winstream.sh
+++ video/test_code/scripts/helper/winstream.sh
@@ -1,24 +1,50 @@
 #!/bin/sh
 
 VIDEO_PIPELINE=$1
-SETWIN_PARAMETERS=$2
-STREAMING_PARAMETERS=$3
+SETIMG_PARAMETERS=$2
+SETCROP_PARAMETERS=$3
+STREAMING_PARAMETERS=$4
+ACTUAL_SIZE=30
+INCREMENT=20
+EXIT_WHILE=0
 RESULT=0
 
-# Usage: setcrop <vid> <left> <top> <width> <height>
-$TESTBIN/setcrop $VIDEO_PIPELINE 0 0 120 160
+
+$TESTBIN/setimg $VIDEO_PIPELINE $SETIMG_PARAMETERS > $TMPBASE\temp_setimg.tmp
+var=`cat $TMPBASE\temp_setimg.tmp | grep window | sed -e 's/[a-zA-Z-]*//g' | sed -e 's/=//g'`
 RESULT=`command_tracking.sh $RESULT $?`
 
-# Usage: setwin <vid> <left> <top> <width> <height>
-$TESTBIN/setwin $VIDEO_PIPELINE $SETWIN_PARAMETERS
-RESULT=`command_tracking.sh $RESULT $?`
+set $var
+WIDTH=$3
+HEIGTH=$4
 
-# Usage: streaming <vid> <inputfile> [<n>]
-$TESTBIN/streaming $VIDEO_PIPELINE $STREAMING_PARAMETERS
-RESULT=`command_tracking.sh $RESULT $?`
+while [ $EXIT_WHILE != 1 ];
+do
+	# Usage: setcrop <vid> <left> <top> <width> <height>
+	$TESTBIN/setcrop $VIDEO_PIPELINE $SETCROP_PARAMETERS
+	RESULT=`command_tracking.sh $RESULT $?`
 
-if [ -z "$STRESS" ]; then
-	stress_message.sh
-fi
+	# Usage: setwin <vid> <left> <top> <width> <height>
+	$TESTBIN/setwin $VIDEO_PIPELINE 0 0 $ACTUAL_SIZE $ACTUAL_SIZE
+	RESULT=`command_tracking.sh $RESULT $?`
+
+	# Usage: streaming <vid> <inputfile> [<n>]
+	$TESTBIN/streaming $VIDEO_PIPELINE $STREAMING_PARAMETERS
+	RESULT=`command_tracking.sh $RESULT $?`
+
+	if [ -z "$STRESS" ]; then
+		stress_message.sh
+	fi
+
+	sleep 1
+	ACTUAL_SIZE=`expr "$ACTUAL_SIZE" "+" "$INCREMENT"`
+
+	if [ "$WIDTH" -lt "$ACTUAL_SIZE" ]; then
+		if [ "$HEIGTH" -lt "$ACTUAL_SIZE" ]; then
+			EXIT_WHILE=`expr "$EXIT_WHILE" "+" 1`
+		fi
+	fi
+
+done
 
 exit $RESULT
