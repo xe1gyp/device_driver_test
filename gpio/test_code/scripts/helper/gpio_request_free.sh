15a79e532a5e9378bbd92b6baf9e25e6af79b21b
Moiz Sonasath <m-sonasath@ti.com>
2009-08-03 17:34:38 -0500

    This patch includes the new test suite for gpio driver on android 2.6.29 branch
    The patch includes the following:
    
    - Modifying the Makefile, to stop compiling gpio.c test module as it is based on kernel preemption feature (not enabled in android 2.6.29). Not
    needed now for testing.
    - Test module (gpio_test.c) written in a new way, generalising it to check any gpio lines present (OMAP:0-191 + Triton: 192-209), procfs entry
    created to pass information to user space test script and error checking introduced for TITAN to report failures.
    - Modifying the conf.sh file to check all the Gpio lines, OMAP:0-191 + Triton: 192-209. Also, disabling the creation of gpio.ko test module as not
    needed.
    - Removing L_DD_GPIO_0001_0001, L_DD_GPIO_0001_0002, L_DD_GPIO_0001_0003, L_DD_GPIO_0002_0001, L_DD_GPIO_0002_0002, L_DD_GPIO_0003_0002,
    L_DD_GPIO_ALL test scenarios as they dont map with the SRD requirements.
    - New test scenarios which are mapped to SRD are added:
    	L_DD_GPIO_0003_0001--> gpio_input_output.sh  Input/Output configuration
    	L_DD_GPIO_0004_0001--> gpio_read.sh          Input line read
    	L_DD_GPIO_0005_0001--> gpio_write.sh         Output line set
    	L_DD_GPIO_0007_0001--> gpio_request_free.sh  Reserve/Free gpio line
    	L_DD_GPIO_0008_0001--> gpio_irq.sh           Edge sensitivity

---
+++ gpio/test_code/scripts/helper/gpio_request_free.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -x
+error=0
+COUNT=$FIRST_GPIO_LINE
+while [ $COUNT -le $MAX_GPIO_LINES ]
+do
+	insmod $TEST_MODULE test=1 gpio=$COUNT value=1
+	cat /proc/driver/gpio_test_result | grep FAILED
+	if [ $? -eq 0 ]
+	then
+		error=`expr $error + 1`
+	fi
+	rmmod $TEST_MODULE
+	COUNT=`expr $COUNT + 1`
+done
+
+if [ $error -gt 0 ]
+then
+	echo " The number of errors = $error, Please refer the log for more details "
+	return 1
+else
+	return 0
+fi
---
+++ gpio/test_code/scripts/helper/gpio_write.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -x
+error=0
+COUNT=$FIRST_GPIO_LINE
+while [ $COUNT -le $MAX_GPIO_LINES ]
+do
+	if [ $COUNT -ne 152 ]
+	then
+		insmod $TEST_MODULE test=4 gpio=$COUNT value=1
+		cat /proc/driver/gpio_test_result | grep FAILED
+		if [ $? -eq 0 ]
+		then
+			error=`expr $error + 1`
+		fi
+		rmmod $TEST_MODULE
+	fi
+	COUNT=`expr $COUNT + 1`
+done
+
+if [ $error -gt 0 ]
+then
+	echo " The number of errors = $error, Please refer the log for more details "
+	return 1
+else
+	return 0
+fi
