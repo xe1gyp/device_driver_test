1e4ac9111687d889987b5d577408a2cc097ab866
Dominic Curran <dcurran@ti.com>
2009-09-17 14:20:13 -0500

    TestSuites: Camera: New L_DD_CAMERA_0122 scenario for Zoom2
    New scenario for Zoom2: L_DD_CAMERA_0122
    Similar to SDP scenario: L_DD_CAMERA_0022
    
    - Changed original L_DD_CAMERA_0022 to take new $DEVICE parameter.
    - Removed unused comments in abslayer.sh script file.
    
    Signed-off-by: Dominic Curran <dcurran@ti.com>

--- camera/test_code/scripts/helper/abslayer.sh
+++ camera/test_code/scripts/helper/abslayer.sh
@@ -1,42 +1,20 @@
 #!/bin/sh
 
-TEST=$1
-OPTION=$2
-MODE=$3
+DEVICE=$1
+TEST=$2
+OPTION=$3
+MODE=$4
+
 
 if [ -z "$MODE" ];then
    echo "Using defualt Mode = 1, before streamon"
    MODE=1
 fi
 
-# Usage: Test_Case_xxx <vid> <option>
-#if [ "$TEST" = "CCDC" ];then
-#	$TESTBIN/Test_Case_ccdc5 1 $OPTION
-#fi
+$TESTBIN/streaming_abs $DEVICE $TEST $OPTION $MODE YUYV QVGA 1 100
+RESULT=$?
+echo "Test returned $RESULT"
 
-#if [ "$TEST" = "PREVIEW" ]; then
-#        $TESTBIN/Test_Case_preview5 1 $OPTION
-#fi
-
-#if [ "$TEST" = "TABLES" ];then
-#	if [ "$OPTION" = "lsc" ];then
-#        	$TESTBIN/Test_Case_ccdc5 1 "lc"
-#	fi
-#        $TESTBIN/Test_Case_tables5 1 $OPTION
-#fi
-
-# Usage: streaming_abs [camDevice] [absLayerGroup] [absLayerOption] [absLayerMode] [pixelFormat] [<sizeW> <sizeH>] [(vid)] [<count>] [<file>]
-#if [ "$OPTION" = "bcl" ]; then
-#	$TESTBIN/streaming_abs 1 $TEST $OPTION 1 $MODE YUYV QVGA 1 100
-#else 
-	#Using UYVY provisionally. It is necesary to use with image analyzer software.
-        $TESTBIN/streaming_abs 1 $TEST $OPTION $MODE YUYV QVGA 1 100
-	#$TESTBIN/streaming_abs 1 $TEST $OPTION $MODE UYVY VGA 1 1 $TMPBASE/VGA_"$OPTION".yuv
-	RESULT=$?
-	echo "Test returned $RESULT"
-#fi
-
-#chmod 744 $TMPBASE/VGA_"$OPTION".yuv
 
 if [ $RESULT -eq 255 ]; then
   ERR=1
