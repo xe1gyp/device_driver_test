8110627ef99d8d7b2679d73a8c412fdb5ea403c7
Dominic Curran <dcurran@ti.com>
2009-09-17 14:20:12 -0500

    TestSuites: Camera: New L_DD_CAMERA_0114 scenario for Zoom2
    New scenario for Zoom2: L_DD_CAMERA_0114
    Similar to SDP scenario: L_DD_CAMERA_0014
    
    Added $DEVICE arg parameter to AWB-AE-HIST.sh.
    
    Signed-off-by: Dominic Curran <dcurran@ti.com>

--- camera/test_code/scripts/helper/AWB-AE-HIST.sh
+++ camera/test_code/scripts/helper/AWB-AE-HIST.sh
@@ -2,14 +2,19 @@
 
 TEST=$1
 PARAMETER=$2
+DEVICE=$3
+
+if [ -z "$DEVICE" ]; then
+  echo "Defaulting to use Device 1"
+  DEVICE=1
+fi
 
 if [ $TEST = "AWB" ]; then
   echo "";echo "Running h3a_stream program to configure the H3A engine and to collect auto white balance statistics."
    echo "";echo "To obtain again the AWB statistics, press S20."
   QUESTION="Was the camera able to capture video, and let you configure H3A engine and collect AWB statistics?"
   sleep 5
-  #$TESTBIN/ioctl QCIF YUYV
-  $TESTBIN/h3a_stream 1 30 1 $PARAMETER
+  $TESTBIN/h3a_stream $DEVICE 30 1 $PARAMETER
   RESULT=$?
   echo "Test returned $RESULT"
 elif [ $TEST = "AE" ]; then
@@ -17,8 +22,7 @@ elif [ $TEST = "AE" ]; then
   echo "";echo "To obtain again the AE/AWB statistics, press S20."
   QUESTION="Was the camera able to capture video, and let you configure H3A engine and collect AWB statistics?"
   sleep 5
-  #$TESTBIN/ioctl QCIF YUYV
-  $TESTBIN/h3a_stream 1 30 1 $PARAMETER
+  $TESTBIN/h3a_stream $DEVICE 30 1 $PARAMETER
   RESULT=$?
   echo "Test returned $RESULT"
 elif [ $TEST="HIST" ]; then
