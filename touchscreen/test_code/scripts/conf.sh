ce817a671b40d2674297098ce97777bc9f003b47
Abraham Arce <x0066660@ti.com>
2009-09-22 18:59:39 -0500

    TestSuites: Touchscreen: Add Initial Baseline
    Adding the initial testsuite for touchscreen driver, 3 scenarios
    related to SMP:
    
    L_DD_TOUCHSCREEN_0005_0001 - Test Scheduler Priority
    L_DD_TOUCHSCREEN_0005_0002 - Test CPU affinity
    L_DD_TOUCHSCREEN_0005_0003 - Test IRQ affinity
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

---
+++ touchscreen/test_code/scripts/conf.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Testsuite variables
+export POSTFIX=`date "+%Y%m%d-%H%M%S"`
+export TESTROOT=${PWD}
+export TESTBIN=${PWD}/../binaries
+export MODDIR=${PWD}/../mods
+export TESTSCRIPT=${PWD}/helper
+export TMPBASE=${TESTROOT}/tmp
+export TMPFILE=${TMPBASE}/tmp.$POSTFIX
+export CMDFILE=cmd.$POSTFIX
+export TESTDIR=${TESTROOT}/test
+export PRETTY_PRT=""
+export VERBOSE=""
+export OUTPUTFILE=${TESTROOT}/output.$POSTFIX
+export LOGFILE=${TESTROOT}/log.$POSTFIX
+export DURATION=""
+export PATH="${TESTROOT}:${TESTBIN}:${TESTSCRIPT}:${PATH}"
+export TC_SCENARIO="${TESTROOT}/scenarios"
+export SCENARIO_NAMES=""
+export STRESS=""
+
+# External Utilities
+. ${TESTROOT}/../../utils/configuration/general.configuration
+export UTILBIN=${TESTROOT}/../../utils/bin
+
+# General variables
+export DMESG_FILE=/var/log/dmesg
+
+# Keypad devfs node
+TEMP_EVENT=`ls /sys/class/input/ | grep event`
+set $TEMP_EVENT
+
+for i in $TEMP_EVENT
+do
+	cat /sys/class/input/$i/device/modalias | grep "ads7846"
+	IS_THIS_OUR_DRIVER=`echo $?`
+	if [ "$IS_THIS_OUR_DRIVER" -eq "0" ]
+	then
+		export DEVFS_TOUCHSCREEN=/dev/input/$i
+		echo "Keypad node is " $DEVFS_TOUCHSCREEN
+	fi
+done
+
+if [ ! -e "$DEVFS_TOUCHSCREEN" ]
+then
+	echo "FATAL: Touchscreen node cannot be found -> $DEVFS_TOUCHSCREEN"
+	exit 1
+fi
+
+# End of file
