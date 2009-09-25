1d5e42c48e7b488b76292ecba09d2298f027bb7b
Ricardo Perez Olivares <x0081762@ti.com>
2009-09-25 18:49:27 -0500

    TestSuites: VIDEO: Changing value of rotation, and background IOCTL
    1.Rotation cide is given a relative value. But the base value is not defined.
    Changing this value to absolute.
    
    2. Background was earlier set using ioctl interface as VIDIOC_S_OMAP2_BGCOLOR
    Now it is being set as cid and hence changed the interface.
    
    Signed-off-by: Kishore Y <x0114913@ti.com>
    Signed-off-by: Ricardo Perez Olivares <x0081762@ti.com>

--- video/test_code/utils/video/setbgcolor.c
+++ video/test_code/utils/video/setbgcolor.c
@@ -15,50 +15,54 @@
 #include <linux/kernel.h>
 #include <linux/videodev.h>
 
-
 #include "lib.h"
+
 
 static int usage(void)
 {
-	printf("Usage: setbgcolor <output device [1: LCD 2: TV]> <24 bit RGB value>\n");
+	printf("Usage: roatation <video_device> <degree>\n");
 	return 1;
 }
 
 int main(int argc, char *argv[])
 {
-	int output_device, file_descriptor, result;
-	struct omap24xxvout_bgcolor bgcolor = {0};
+	int video_device, file_descriptor, result;
+	int degree;
+	struct v4l2_control control;
+	memset(&control, 0 , sizeof(control));
+	control.id = V4L2_CID_BG_COLOR;
 
 	if (argc < 3)
 		return usage();
 
-	output_device = atoi(argv[1]) + 3;
+	video_device = atoi(argv[1]);
 	
-	if ((output_device != OMAP24XX_OUTPUT_LCD) &&
-		(output_device != OMAP24XX_OUTPUT_TV)) {
-		printf("Output device should be 1 for LCD or 2 for TV \n");
+	if ((video_device != 1) && (video_device != 2)) {
+		printf("video_device has to be 1 or 2!\n");
 		return usage();
 	}
 
-	file_descriptor = open(VIDEO_DEVICE1, O_RDONLY);
+	file_descriptor =
+		open((video_device == 1) ? VIDEO_DEVICE1 : VIDEO_DEVICE2,
+		O_RDONLY);
 	if (file_descriptor <= 0) {
-		printf("Could not open %s\n",VIDEO_DEVICE1);
+		printf("Could not open %s\n",
+		(video_device == 1) ? VIDEO_DEVICE1 : VIDEO_DEVICE2);
 		return 1;
 	} else {
-		printf("openned %s\n", VIDEO_DEVICE1);
+		printf("openned %s\n",
+		(video_device == 1) ? VIDEO_DEVICE1 : VIDEO_DEVICE2);
 	}
 
-	bgcolor.output_dev = output_device;
-	bgcolor.color      = atoi(argv[2]);
- 
-	result = ioctl(file_descriptor, VIDIOC_S_OMAP2_BGCOLOR, &bgcolor);
-
+	degree = atoi(argv[2]);
+	control.value = degree;
+	result = ioctl(file_descriptor, VIDIOC_S_CTRL, &control);
 	if (result != 0) {
-		perror("VIDIOC_S_OMAP2_BGCOLOR");
-		return 1;
+		perror("VIDIOC_S_CTRL");
+		return 0;
 	}
 
-	printf("Back Ground color is set to %x \n", bgcolor.color);
+	printf("Background set to %d degree\n", degree);
 	close(file_descriptor);
 
 	return 0;
