614c75d4c88293e3e6c628358047264763880a44
Dominic Curran <dcurran@ti.com>
2009-09-29 14:06:27 -0500

    TestSuites: Camera: Reset PREVIEW config after streaming_abs test.
    The PREVIEWER configuration was not getting reset after every test.
    Thus image setting were being applied on top of one another causing the image to become black very quickly.
    
    The patch resets the PREVIEWER settings after every test.
    
    Signed-off-by: Dominic Curran <dcurran@ti.com>

--- camera/test_code/utils/testcode/absLayer.h
+++ camera/test_code/utils/testcode/absLayer.h
@@ -290,8 +290,6 @@ int resetCcdc(int fd, char *ccdcOption)
 
 int updatePreview(int fd, char *previewOption)
 {
-	struct v4l2_queryctrl queryctrl;
-	struct v4l2_control control;
 	int ret;
 
 	struct ispprev_hmed hmed_p;
@@ -471,6 +469,8 @@ int updatePreview(int fd, char *previewOption)
 	} else if ((!strcasecmp(previewOption, "all"))) {
 		preview_struct.update = ~0;
 		preview_struct.flag = ~0;
+	} else if ((!strcasecmp(previewOption, "none"))) {
+		return 1;
 	} else {
 		printf("Wrong option %s, please enter correct"
 				" option\n", previewOption);
@@ -478,8 +478,216 @@ int updatePreview(int fd, char *previewOption)
 		return 0;
 	}
 
-	memset(&queryctrl, 0, sizeof(queryctrl));
-	memset(&control, 0, sizeof(control));
+	ret = ioctl(fd, VIDIOC_PRIVATE_ISP_PRV_CFG, &preview_struct);
+
+	if (ret == -1) {
+		printf("\nerror\n");
+		return 0;
+	} else
+		printf("abs Layer VIDIOC_S_CTRL successful\n");
+
+	return 1;
+}
+
+/**
+ * resetPreview - Reset Abstraction layer to default values.
+ * @fd: File descriptor
+ * @previewOption: Text string indicating option to reset
+ **/
+int resetPreview(int fd, char *previewOption)
+{
+	int ret;
+	struct ispprev_hmed hmed_p;
+
+	hmed_p.odddist = 0x2; /* For Bayer sensor */
+	hmed_p.evendist = 0x2;
+	hmed_p.thres = 0x10;
+
+	static __u32 redgamma_table[] = {
+	#include "redgamma_table.h"
+	};
+
+	static __u32 greengamma_table[] = {
+	#include "greengamma_table.h"
+	};
+
+	static __u32 bluegamma_table[] = {
+	#include "bluegamma_table.h"
+	};
+
+	static __u32 cfa_coef_table[] = {
+		#include "cfa_coef_table.h"
+	};
+
+	static __u32 luma_enhance_table[] = {
+		#include "luma_enhance_table.h"
+	};
+
+	struct ispprev_cfa cfa_p;
+
+	cfa_p.cfafmt = CFAFMT_BAYER;
+	cfa_p.cfa_gradthrs_vert = 0x28; /* Default values */
+	cfa_p.cfa_gradthrs_horz = 0x28; /* Default values */
+	cfa_p.cfa_table = cfa_coef_table;
+
+	struct ispprev_csup csup_p;
+
+	csup_p.gain = 0x0D;
+	csup_p.thres = 0xEB;
+	csup_p.hypf_en = 0;
+
+	/* Structure for White Balance */
+	struct ispprev_wbal wbal_p;
+
+	wbal_p.dgain = 0x100;
+	wbal_p.coef3 = 0x23;
+	wbal_p.coef2 = 0x20;
+	wbal_p.coef1 = 0x20;
+	wbal_p.coef0 = 0x39;
+
+	struct ispprev_blkadj blkadj_p;
+
+	blkadj_p.red = 0x0;
+	blkadj_p.green = 0x0;
+	blkadj_p.blue = 0x0;
+
+	struct ispprev_rgbtorgb rgb2rgb_p;
+
+	rgb2rgb_p.matrix[0][0] = 0x01E2;
+	rgb2rgb_p.matrix[0][1] = 0x0F30;
+	rgb2rgb_p.matrix[0][2] = 0x0FEE;
+	rgb2rgb_p.matrix[1][0] = 0x0F9B;
+	rgb2rgb_p.matrix[1][1] = 0x01AC;
+	rgb2rgb_p.matrix[1][2] = 0x0FB9;
+	rgb2rgb_p.matrix[2][0] = 0x0FE0;
+	rgb2rgb_p.matrix[2][1] = 0x0EC0;
+	rgb2rgb_p.matrix[2][2] = 0x0260;
+
+	rgb2rgb_p.offset[0] = 0x0000;
+	rgb2rgb_p.offset[1] = 0x0000;
+	rgb2rgb_p.offset[2] = 0x0000;
+
+	struct ispprev_csc csc_p = {
+		/*CSC Coef Matrix No Effect*/
+		{
+			{66, 129, 25},
+			{-38, -75, 112},
+			{112, -94, -18}
+		},
+		/* CSC Offset */
+		{0x0, 0x0, 0x0}
+	};
+
+	struct ispprev_yclimit yclimit_p;
+
+	yclimit_p.minC = 0x00;	/* Default values */
+	yclimit_p.maxC = 0xFF;
+	yclimit_p.minY = 0x00;
+	yclimit_p.maxY = 0xFF;
+
+	struct ispprev_dcor ispprev_dcor_t;
+
+	ispprev_dcor_t.couplet_mode_en = 1;
+	ispprev_dcor_t.detect_correct[0] = 0xE;	/* Default values */
+	ispprev_dcor_t.detect_correct[1] = 0xE;
+	ispprev_dcor_t.detect_correct[2] = 0xE;
+	ispprev_dcor_t.detect_correct[3] = 0xE;
+
+	struct ispprv_update_config preview_struct;
+
+	__u32 table_t[64];
+	int all;
+	struct ispprev_nf ispprev_nf_t;
+
+	for (all = 0; all < 32; all++)
+		table_t[all] = 40; /*16 Default*/
+
+	for (all = 32; all < 64; all++)
+		table_t[all] = 20; /*31 Default*/
+
+	ispprev_nf_t.spread = 3;
+	memcpy(ispprev_nf_t.table, table_t, sizeof(ispprev_nf_t.table));
+
+	preview_struct.red_gamma = (void *)redgamma_table;
+	preview_struct.green_gamma = (void *)greengamma_table;
+	preview_struct.blue_gamma = (void *)bluegamma_table;
+	preview_struct.prev_nf = &ispprev_nf_t;
+
+
+	preview_struct.yen = luma_enhance_table;
+	preview_struct.shading_shift = 0;
+	preview_struct.prev_hmed = &hmed_p;
+	preview_struct.prev_cfa = &cfa_p;
+	preview_struct.csup = &csup_p;
+	preview_struct.prev_wbal = &wbal_p;
+	preview_struct.prev_blkadj = &blkadj_p;
+	preview_struct.rgb2rgb = &rgb2rgb_p;
+	preview_struct.prev_csc = &csc_p;
+	preview_struct.yclimit = &yclimit_p;
+	preview_struct.prev_dcor = &ispprev_dcor_t;
+
+	if ((!strcasecmp(previewOption, "le"))) {
+		preview_struct.update = ISP_ABS_PREV_LUMAENH;
+
+	} else if ((!strcasecmp(previewOption, "ialaw"))) {
+		preview_struct.update = ISP_ABS_PREV_INVALAW;
+
+	} else if ((!strcasecmp(previewOption, "hm"))) {
+		preview_struct.update = ISP_ABS_PREV_HRZ_MED;
+
+	} else if ((!strcasecmp(previewOption, "cfa"))) {
+		preview_struct.update = ISP_ABS_PREV_CFA;
+
+	} else if ((!strcasecmp(previewOption, "cs"))) {
+		preview_struct.update = ISP_ABS_PREV_CHROMA_SUPP;
+
+	} else if ((!strcasecmp(previewOption, "wb"))) {
+		preview_struct.update = ISP_ABS_PREV_WB;
+		/* NOTE: Added default table */
+	} else if ((!strcasecmp(previewOption, "ba"))) {
+		preview_struct.update = ISP_ABS_PREV_BLKADJ;
+		preview_struct.flag = ISP_ABS_PREV_BLKADJ;
+		/* NOTE: Table is same as kernels default */
+	} else if ((!strcasecmp(previewOption, "r"))) {
+		preview_struct.update = ISP_ABS_PREV_RGB2RGB;
+		preview_struct.flag = ISP_ABS_PREV_RGB2RGB;
+		/* NOTE: Table is same as kernels default */
+	} else if ((!strcasecmp(previewOption, "cc"))) {
+		preview_struct.update = ISP_ABS_PREV_COLOR_CONV;
+		preview_struct.flag = ISP_ABS_PREV_COLOR_CONV;
+		/* NOTE: Added default table */
+	} else if ((!strcasecmp(previewOption, "yc"))) {
+		preview_struct.update = ISP_ABS_PREV_YC_LIMIT;
+		preview_struct.flag = ISP_ABS_PREV_YC_LIMIT;
+		/* NOTE: Table is same as kernels default */
+	} else if ((!strcasecmp(previewOption, "dc"))) {
+		preview_struct.update = ISP_ABS_PREV_DEFECT_COR;
+
+	} else if ((!strcasecmp(previewOption, "gb"))) {
+
+	} else if ((!strcasecmp(previewOption, "nf"))) {
+		preview_struct.update = ISP_ABS_TBL_NF;
+
+	} else if ((!strcasecmp(previewOption, "rg"))) {
+		preview_struct.update = ISP_ABS_TBL_REDGAMMA;
+		preview_struct.flag = ISP_ABS_TBL_GREENGAMMA;
+		/* NOTE: Table is same as kernels default */
+	} else if ((!strcasecmp(previewOption, "gg"))) {
+		preview_struct.update = ISP_ABS_TBL_GREENGAMMA;
+		preview_struct.flag = ISP_ABS_TBL_GREENGAMMA;
+		/* NOTE: Table is same as kernels default */
+	} else if ((!strcasecmp(previewOption, "bg"))) {
+		preview_struct.update = ISP_ABS_TBL_BLUEGAMMA;
+		preview_struct.flag = ISP_ABS_TBL_BLUEGAMMA;
+		/* NOTE: Table is same as kernels default */
+	} else if ((!strcasecmp(previewOption, "none"))) {
+		return 1;
+	} else {
+		printf("Wrong option %s, please enter correct"
+				" option\n", previewOption);
+		return 0;
+	}
+
 
 	ret = ioctl(fd, VIDIOC_PRIVATE_ISP_PRV_CFG, &preview_struct);
 
