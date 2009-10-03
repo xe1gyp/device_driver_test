cc4537b7ff62261a061c83e8809347cb97d8b881
Ricardo Perez Olivares <x0081762@ti.com>
2009-10-02 21:06:42 -0500

    TestSuites: LMBENCH: Adding support for 24x branch
    Adding lmbench-3.0-a9 baseline to 24x branch

---
+++ lmbench/test_code/scripts/test_runner.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#-----------------------
+# Based on runltp script  from LTP 
+# much of the functions are copied over from there - copyright remains
+#-----------------------
+
+cd ../utils/lmbench-3.0-a9/scripts/
+./config-run
