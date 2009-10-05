274ac5da42daf980943cbf4ee87d39aeb70c5d79
Ricardo Perez Olivares <ricardo.perez.olivares@gmail.com>
2009-10-05 17:38:15 -0500

    TestSuites: DMA: DMA SMP Test support
    Adding modifications to support SMP Test Support
    Patch 1/3
    
    Signed-off-by: Venkatraman S <svenkatr@ti.com>
    Signed-off-by: Ricardo Perez Olivares <ricardo.perez.olivares@gmail.com>

--- dma/test_code/utils/dma/dma_single_channel.c
+++ dma/test_code/utils/dma/dma_single_channel.c
@@ -31,14 +31,14 @@ static int test_passed = -1;
  * It will not return until the test sets a value different than -1
  */
 static int dma_read_proc(char *buf, char **start, off_t offset,
-				int count, int *eof, void *data){
+				int count, int *eof, void *data)
+{
 	int len;
 
-    while(test_passed == -1){
+    while (test_passed == -1)
         mdelay(TEST_FINISHED_POLL_TIME);
-    }
 
-    if(test_passed){
+    if (test_passed) {
         len = sprintf(buf,"Test PASSED\n");
     }else{
         len = sprintf(buf,"Test FAILED\n");
@@ -50,8 +50,9 @@ static int dma_read_proc(char *buf, char **start, off_t offset,
 /*
  * Set if the test passed or not
  */
-void set_test_passed(int passed){
-     if(passed){
+void set_test_passed(int passed)
+{
+     if (passed) {
         printk("\nTest PASSED\n");
      }else{
         printk("\nTest FAILED\n");
@@ -61,9 +62,31 @@ void set_test_passed(int passed){
 EXPORT_SYMBOL(set_test_passed);
 
 /*
+ * Checks that the destination buffers were written correctly
+ */
+int check_dma_transfer_complete(struct dma_transfer *transfers,
+				int num_transfer)
+{
+	int i;
+	int returncode = 0;
+	/* Check that all the transfers finished */
+	for (i = 0; i < num_transfer; i++) {
+		if (!transfers[i].data_correct) {
+			printk(KERN_INFO "Transfer id %d failed\n",
+					transfers[i].transfer_id);
+			return -(i);
+		}
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL(check_dma_transfer_complete);
+
+/*
  * Creates a read proc entry in the procfs
  */
-void create_dma_proc(char *proc_name){
+void create_dma_proc(char *proc_name)
+{
 	create_proc_read_entry(proc_name, 0, NULL, dma_read_proc, NULL);
 }
 EXPORT_SYMBOL(create_dma_proc);
@@ -71,7 +94,8 @@ EXPORT_SYMBOL(create_dma_proc);
 /*
  * Removes a proc entry from the procfs
  */
-void remove_dma_proc(char *proc_name){
+void remove_dma_proc(char *proc_name)
+{
     remove_proc_entry(proc_name, NULL);
 }
 EXPORT_SYMBOL(remove_dma_proc);
@@ -81,7 +105,8 @@ EXPORT_SYMBOL(remove_dma_proc);
  * Function used to verify the source an destination buffers of a dma transfer
  * are equal in content
  */
-int verify_buffers(struct dma_buffers_info *buffers) {
+int verify_buffers(struct dma_buffers_info *buffers)
+{
     int i;
     u8 *src_address = (u8*) buffers->src_buf;
     u8 *dest_address = (u8*) buffers->dest_buf;
@@ -106,37 +131,38 @@ EXPORT_SYMBOL(verify_buffers);
 /*
  * Callback function that dma framework will invoke after transfer is done
  */
-void dma_callback(int transfer_id, u16 transfer_status, void *data) {
-       struct dma_transfer *transfer = (struct dma_transfer *) data;
-       int error = 1;
-       transfer->data_correct = 0;
-       transfer->finished = 1;
-       printk("\nTransfer complete for id %d, checking destination buffer\n",
-           transfer->transfer_id);
-       /* Check if the transfer numbers are equal */
-       if(transfer->transfer_id != transfer_id){
-           printk(" WARNING: Transfer id %d differs from the one"
-                " received in callback (%d)\n", transfer->transfer_id,
-                transfer_id);
-       }
+void dma_callback(int transfer_id, u16 transfer_status, void *data)
+{
+	struct dma_transfer *transfer = (struct dma_transfer *) data;
+	int error = 1;
+	transfer->data_correct = 0;
+	transfer->finished = 1;
+	printk("\nTransfer complete for id %d, checking destination buffer\n",
+	transfer->transfer_id);
+	/* Check if the transfer numbers are equal */
+	if (transfer->transfer_id != transfer_id) {
+		printk(KERN_WARNING "WARN:Transfer id %d differs from the one"
+		" received in callback (%d)\n", transfer->transfer_id,
+		transfer_id);
+	}
        /* Check the transfer status is acceptable */
-       if((transfer_status & OMAP_DMA_BLOCK_IRQ) || (transfer_status == 0)){
-           /* Verify the contents of the buffer are equal */
-           error = verify_buffers(&(transfer->buffers));
-       }else{
-           printk(" Verification failed, transfer id %d status is not "
-                "acceptable\n", transfer->transfer_id);
-           return;
-       }
+	if ((transfer_status & OMAP_DMA_BLOCK_IRQ) || (transfer_status == 0)) {
+		/* Verify the contents of the buffer are equal */
+		error = verify_buffers(&(transfer->buffers));
+	} else {
+		printk(KERN_ERR " Verification failed, trfr id %d status is not"
+		"acceptable\n", transfer->transfer_id);
+		return;
+	}
 
-       if(error){
-           printk(" Verification failed, transfer id %d source and destination"
-                " buffers differ\n", transfer->transfer_id);
-       }else{
-           printk(" Verification succeeded for transfer id %d\n",
-                transfer->transfer_id);
-           transfer->data_correct = 1;
-       }
+	if (error) {
+		printk(KERN_ERR "Verification failed, transfer id %d source"
+		"and destination buffers differ\n", transfer->transfer_id);
+	} else {
+		printk(KERN_INFO " Verification succeeded for transfer id %d\n",
+		transfer->transfer_id);
+		transfer->data_correct = 1;
+	}
 }
 EXPORT_SYMBOL(dma_callback);
 
@@ -144,7 +170,8 @@ EXPORT_SYMBOL(dma_callback);
  * This function allocates 2 dma buffers with the same size for the source
  * and destination.
  */
-int create_transfer_buffers( struct dma_buffers_info *buffers){
+int create_transfer_buffers(struct dma_buffers_info *buffers)
+{
        printk("Allocating non-cacheable source and destination buffers\n");
 
        /* Allocate source buffer */
@@ -164,21 +191,21 @@ int create_transfer_buffers( struct dma_buffers_info *buffers){
                 0);
 
        /* Check the buffers have been allocated correctly */
-       if( !buffers->src_buf ){
-           printk(" Unable to allocate %d bytes for the source transfer"
-                " buffer\n", buffers->buf_size);
-           return 1;
-       }else if( !buffers->dest_buf ){
-           printk(" Unable to allocate %d bytes for the destination transfer"
-                " buffer\n", buffers->buf_size);
-           return 1;
-       }else{
-           printk(" Buffers allocated successfully (%d bytes per buffer)\n",
-                buffers->buf_size);
-           printk(" Source buffer on address 0x%x\n",
-                buffers->src_buf);
-           printk(" Destination buffer on address 0x%x\n",
-                buffers->dest_buf);
+       if (!buffers->src_buf) {
+		printk(KERN_ERR "Unable to allocate %d bytes for the transfer"
+		" buffer\n", buffers->buf_size);
+		return 1;
+       } else if (!buffers->dest_buf) {
+		printk(KERN_ERR "Unable to allocate %d bytes for the transfer"
+		" buffer\n", buffers->buf_size);
+		return 1;
+       } else {
+		printk(KERN_INFO "Buffers allocated (%d bytes per buffer)\n",
+			buffers->buf_size);
+		printk(KERN_INFO "Source buffer on address 0x%x\n",
+			buffers->src_buf);
+		printk(KERN_INFO "Destination buffer on address 0x%x\n",
+			buffers->dest_buf);
        }
        return 0;
 }
@@ -187,7 +214,8 @@ EXPORT_SYMBOL(create_transfer_buffers);
 /*
  * Fill the source buffer with bytes using a pseudo-random value generator
  */
-void fill_source_buffer(struct dma_buffers_info *buffers){
+void fill_source_buffer(struct dma_buffers_info *buffers)
+{
        int i;
        u8 *src_buf_byte;
        printk("Filling source buffer 0x%x with %d bytes...", buffers->src_buf,
@@ -203,7 +231,8 @@ EXPORT_SYMBOL(fill_source_buffer);
 /*
  * Requests a dma transfer
  */
-int request_dma(struct dma_transfer *transfer){
+int request_dma(struct dma_transfer *transfer)
+{
        int success;
        transfer->finished = 0;
        printk("\nRequesting OMAP DMA transfer\n");
@@ -229,121 +258,119 @@ EXPORT_SYMBOL(request_dma);
 /*
  * Setup the source, destination and global transfer parameters
  */
-void setup_dma_transfer(struct dma_transfer *transfer){
+void setup_dma_transfer(struct dma_transfer *transfer)
+{
+	/* Determine the elements present in a frame */
+	printk(KERN_INFO "Setting up transfer id %d\n", transfer->transfer_id);
+	transfer->frame_count = 1;
+	switch (transfer->data_type) {
+	case OMAP_DMA_DATA_TYPE_S8:
+	   transfer->elements_in_frame = transfer->buffers.buf_size;
+	   break;
+	case OMAP_DMA_DATA_TYPE_S16:
+	   transfer->elements_in_frame = transfer->buffers.buf_size / 2;
+	   break;
+	case OMAP_DMA_DATA_TYPE_S32:
+	   transfer->elements_in_frame = transfer->buffers.buf_size / 4;
+	   break;
+	default:
+		printk(KERN_ERR "Invalid transfer data type\n");
+	}
+	/* Set dma transfer parameters */
+	omap_set_dma_transfer_params(
+		transfer->transfer_id,
+		transfer->data_type,
+		transfer->elements_in_frame,
+		transfer->frame_count,
+		transfer->sync_mode,
+		transfer->device_id,
+		0x0);
 
-       /* Determine the elements present in a frame */
-       printk("Setting up transfer id %d\n", transfer->transfer_id);
-       transfer->frame_count = 1;
-       switch(transfer->data_type){
-          case OMAP_DMA_DATA_TYPE_S8:
-               transfer->elements_in_frame = transfer->buffers.buf_size;
-               break;
-          case OMAP_DMA_DATA_TYPE_S16:
-               transfer->elements_in_frame = transfer->buffers.buf_size / 2;
-               break;
-          case OMAP_DMA_DATA_TYPE_S32:
-               transfer->elements_in_frame = transfer->buffers.buf_size / 4;
-               break;
-          default:
-               printk(" Invalid transfer data type\n");
-       }
+	/* Configure the source parameters */
+	omap_set_dma_src_params(
+		transfer->transfer_id,
+		0,
+		transfer->addressing_mode,
+		transfer->buffers.src_buf_phys,
+		0x0,
+		0x0);
 
-       /* Set dma transfer parameters */
-       omap_set_dma_transfer_params(
-                transfer->transfer_id,
-                transfer->data_type,
-                transfer->elements_in_frame,
-                transfer->frame_count,
-                transfer->sync_mode,
-                transfer->device_id,
-                0x0);
+	omap_set_dma_src_burst_mode(
+		transfer->transfer_id,
+		transfer->data_burst);
 
-        /* Configure the source parameters */
-        omap_set_dma_src_params(
-                transfer->transfer_id,
-                0,
-                transfer->addressing_mode,
-                transfer->buffers.src_buf_phys,
-                0x0,
-                0x0);
+	/* Configure the destination parameters */
+	omap_set_dma_dest_params(
+		transfer->transfer_id,
+		0,
+		transfer->addressing_mode,
+		transfer->buffers.dest_buf_phys,
+		0x0,
+		0x0);
 
-        /*omap_set_dma_src_endian_type(
-                transfer->transfer_id,
-                transfer->endian_type);*/
+	omap_set_dma_dest_burst_mode(
+		transfer->transfer_id,
+		transfer->data_burst);
 
-        omap_set_dma_src_burst_mode(
-                transfer->transfer_id,
-                transfer->data_burst);
+	/* Global dma configuration parameters */
+	omap_dma_set_global_params(
+		0x3,
+		DMA_DEFAULT_FIFO_DEPTH,
+		0);
 
-        /* Configure the destination parameters */
-        omap_set_dma_dest_params(
-                transfer->transfer_id,
-                0,
-                transfer->addressing_mode,
-                transfer->buffers.dest_buf_phys,
-                0x0,
-                0x0);
+	/* Transfer priority */
+	omap_dma_set_prio_lch(
+		transfer->transfer_id,
+		transfer->priority,
+		transfer->priority);
 
-        omap_set_dma_dest_burst_mode(
-                transfer->transfer_id,
-                transfer->data_burst);
-
-        /* Global dma configuration parameters */
-        omap_dma_set_global_params(
-                0x3,
-                DMA_DEFAULT_FIFO_DEPTH,
-                0);
-
-        /* Transfer priority */
-        omap_dma_set_prio_lch(
-                transfer->transfer_id,
-                transfer->priority,
-                transfer->priority);
-
-        printk(" Transfer with id %d is ready\n", transfer->transfer_id);
+	printk(KERN_INFO "Transfer with id %d is ready\n",
+			transfer->transfer_id);
 }
 EXPORT_SYMBOL(setup_dma_transfer);
 
 /*
  * Starts a dma transfer
  */
-void start_dma_transfer(struct dma_transfer *transfer){
-       char * priority;
-       if(transfer->priority == DMA_CH_PRIO_LOW){
-           priority = "low priority";
-       }else{
-           priority = "high priority";
-       }
-       printk("\nStarting dma transfer for id %d with %s\n",
-           transfer->transfer_id, priority);
-       omap_start_dma(transfer->transfer_id);
+void start_dma_transfer(struct dma_transfer *transfer)
+{
+	char *priority;
+	if (transfer->priority == DMA_CH_PRIO_LOW)
+		priority = "low priority";
+	else
+		priority = "high priority";
+
+	printk(KERN_INFO "Starting dma transfer for id %d with %s\n",
+	transfer->transfer_id, priority);
+	omap_start_dma(transfer->transfer_id);
 }
 EXPORT_SYMBOL(start_dma_transfer);
 
 /*
  * Stops a dma transfer and free used resources
  */
-void stop_dma_transfer(struct dma_transfer *transfer){
-       /* Stop the dma transfer */
-       if(transfer->request_success){
-           omap_stop_dma(transfer->transfer_id);
-           omap_free_dma(transfer->transfer_id);
-       }
-       /* Free the source and destination buffers*/
-       if(transfer->buffers.src_buf){
-           dma_free_coherent(
-               NULL,
-               transfer->buffers.buf_size,
-               (void *) transfer->buffers.src_buf,
-               transfer->buffers.src_buf_phys);
-       }
-       if(transfer->buffers.dest_buf){
-           dma_free_coherent(
-               NULL,
-               transfer->buffers.buf_size,
-               (void *) transfer->buffers.dest_buf,
-               transfer->buffers.dest_buf_phys);
-       }
+void stop_dma_transfer(struct dma_transfer *transfer)
+{
+	/* Stop the dma transfer */
+	if (transfer->request_success) {
+		omap_stop_dma(transfer->transfer_id);
+		omap_free_dma(transfer->transfer_id);
+	}
+	/* Free the source and destination buffers*/
+	if (transfer->buffers.src_buf) {
+		dma_free_coherent(
+			NULL,
+			transfer->buffers.buf_size,
+			(void *) transfer->buffers.src_buf,
+			transfer->buffers.src_buf_phys);
+	}
+	if (transfer->buffers.dest_buf) {
+		dma_free_coherent(
+			NULL,
+			transfer->buffers.buf_size,
+			(void *) transfer->buffers.dest_buf,
+			transfer->buffers.dest_buf_phys);
+	}
 }
 EXPORT_SYMBOL(stop_dma_transfer);
 
@@ -351,7 +378,8 @@ EXPORT_SYMBOL(stop_dma_transfer);
  * Queries for information about an on going dma transfer
  */
 int dma_channel_query(struct dma_transfer *transfer,
-     struct dma_query *query){
+     struct dma_query *query)
+{
       int error = 0;
       /* Get element and frame index */
       /*error = omap_get_dma_index(transfer->transfer_id,
@@ -361,8 +389,8 @@ int dma_channel_query(struct dma_transfer *transfer,
       query->src_addr_counter = omap_get_dma_src_pos(transfer->transfer_id);
       query->dest_addr_counter = omap_get_dma_dst_pos(transfer->transfer_id);
 
-      if(error){
-           printk("Unable to query index data from transfer id %d\n",
+	if (error) {
+		printk(KERN_INFO "Unable to query index data from transfer id %d\n",
                 transfer->transfer_id);
            return 1;
       }
