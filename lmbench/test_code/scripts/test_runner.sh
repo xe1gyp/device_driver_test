8f511771ee5ac8de1d673d62f808b322d3fbd5fa
Ricardo Perez Olivares <ricardo.perez.olivares@gmail.com>
2009-10-05 16:42:49 -0500

    TestSuites: LMBENCH: Erasing unused scripts
    Erasing conf.sh and test_runner.sh due are not used to run LMBENCH
    
    Signed-off-by: Ricardo Perez Olivares <ricardo.perez.olivares@gmail.com>

--- lmbench/test_code/scripts/test_runner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#-----------------------
-# Based on runltp script  from LTP 
-# much of the functions are copied over from there - copyright remains
-#-----------------------
-
-cd ../utils/lmbench-3.0-a9/scripts/
-./config-run
