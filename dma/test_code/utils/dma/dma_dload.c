3a8d6de160aae4e6a1a7bc9c1d25ec38bbb31de2
Venkatraman S <svenkatr@ti.com>
2009-10-07 12:43:57 +0530

    TestSuites: DMA: Cleanup descriptor load test logic
    Signed-off-by: Venkatraman S <svenkatr@ti.com>

--- dma/test_code/utils/dma/dma_dload.c
+++ dma/test_code/utils/dma/dma_dload.c
@@ -19,19 +19,27 @@
 /********************** GENERAL VARS *****************/
 #define num_elements_in_list (7)
 #define PROC_FILE  "driver/dma_descriptor_load"
-#define test_element_size 30
+#define PAUSE_AT_ELEMENT (4)
+#define test_element_size 100
 int maximum_transfers = 5;	/* max transfers per channel */
 int buf_size = PAGE_SIZE;	/* Buffer size for each channel */
+
 int prio = -1;
 int test_result;
-/* Source */
-unsigned int bsptest_dma_src_addr[num_elements_in_list];
-unsigned int bsptest_dma_src_addr_phy[num_elements_in_list];
-/* Dest */
-unsigned int *bsptest_dma_dst_addr;
-unsigned int *bsptest_dma_dst_addr_phy;
-#define TOTAL_TRANSFERR_WORD  (num_elements_in_list*test_element_size)
-#define TOTAL_TRANSFER_BYTES  (TOTAL_TRANSFERR_WORD*4)
+
+struct tlist_transfer {
+	/* Source */
+	unsigned int bsptest_dma_src_addr[num_elements_in_list];
+	unsigned int bsptest_dma_src_addr_phy[num_elements_in_list];
+	unsigned int transfer_sizes[num_elements_in_list];
+	unsigned int total_num_elements;
+	/* Dest */
+	unsigned int *bsptest_dma_dst_addr;
+	unsigned int *bsptest_dma_dst_addr_phy;
+	int sglist_id;
+	struct omap_dma_sglist_node *user_sglist;
+};
+static struct tlist_transfer tls1;
 
 static struct omap_dma_channel_params transfer_params = {
 	.data_type = OMAP_DMA_DATA_TYPE_S32,	/* data type 8,16,32 */
@@ -54,23 +62,30 @@ static struct omap_dma_channel_params transfer_params = {
 	.ie = 0,		/* interrupt enabled */
 	.burst_mode = OMAP_DMA_DATA_BURST_4,
 };
-static int sglist_id;
-static struct omap_dma_sglist_node *user_sglist;
-static int total_transferred_elem;
 
 /********************** TEST LOGIC *****************/
-static void verify_dest_buffer(void)
+static int verify_dest_buffer(struct tlist_transfer *tl, int nelems)
 {
-	int j, destn = 0;
-	int dest = 0xDEADBEEF;
-	for (j = 0; j < total_transferred_elem; j++) {
-		destn = (int)*(bsptest_dma_dst_addr+j);
-		if (destn != dest)
-			printk(KERN_INFO "dest buffer at %d=%x", j, destn);
-		dest = destn;
+	int j, i;
+	char *src1;
+	char *dst1;
+
+	dst1 = (char *)tl->bsptest_dma_dst_addr;
+	for (i = 0; i < nelems; i++) {
+		src1 = (char *)tl->bsptest_dma_src_addr[i];
+		for (j = 0 ; j < tl->transfer_sizes[i] * 4; j++) {
+			if (*src1 != *dst1) {
+				printk(KERN_ERR "Data doesn't match at buff %d, byte %d",
+					i, j);
+				return -1;
+			}
+			src1++;
+			dst1++;
+		}
 	}
-	printk(KERN_INFO " Last element %d ptr=%x,val=%x", j,
-		       (int)((int *)(bsptest_dma_dst_addr)+j), destn);
+
+	printk(KERN_INFO "Verifcation succeed for %d transfers\n", nelems);
+	return 0;
 }
 
 static void dma_sglist_cb_final(int sglistid, u16 ch_status, void *data)
@@ -78,37 +93,42 @@ static void dma_sglist_cb_final(int sglistid, u16 ch_status, void *data)
 	int ncur = omap_get_completed_sglist_nodes(sglistid);
 
 	printk(KERN_INFO "Final callback status %x, ncur=%d", ch_status, ncur);
-	verify_dest_buffer();
-	printk(KERN_INFO "test PASSED\n");
+	if (verify_dest_buffer(data, num_elements_in_list) == 0)
+		printk(KERN_INFO "Test PASSED\n");
 }
 
 static void dma_sglist_cb_inter(int sglistid, u16 ch_status, void *data)
 {
-	int ncur = omap_get_completed_sglist_nodes(sglistid);
 	int pausestate;
+	int stat;
+	int ncur = omap_get_completed_sglist_nodes(sglistid);
 
 	pausestate = omap_dma_sglist_is_paused(sglistid);
 	printk(KERN_INFO "Inter callbk status=%x, ncur=%d pausestate=%d \n ",
 		ch_status, ncur, pausestate);
-	verify_dest_buffer();
-	omap_set_dma_callback(sglistid, dma_sglist_cb_final, NULL);
+	stat = verify_dest_buffer(data, PAUSE_AT_ELEMENT);
+	if (stat) {
+		printk(KERN_INFO "Test FAILED\n");
+		return;
+	}
+	omap_set_dma_callback(sglistid, dma_sglist_cb_final, data);
 	omap_resume_dma_sglist_transfers(sglistid, -1);
 }
 
-static void dmatest_populate_sglist(struct omap_dma_sglist_node *sglist,
+static void dmatest_populate_sglist(struct tlist_transfer *ptls,
 		int nelem)
 {
 	struct omap_dma_sglist_node *listelem = NULL;
 	int i;
+	struct omap_dma_sglist_node *sglist = ptls->user_sglist;
 
 	listelem = sglist;
 	listelem->desc_type = OMAP_DMA_SGLIST_DESCRIPTOR_TYPE2a;
-	listelem->num_of_elem = 10;
-	total_transferred_elem += 10;
+	listelem->num_of_elem = ptls->transfer_sizes[0];
 
 	listelem->flags = OMAP_DMA_LIST_SRC_VALID | OMAP_DMA_LIST_DST_VALID;
-	listelem->sg_node.t2a.src_addr = (int)bsptest_dma_src_addr_phy[0];
-	listelem->sg_node.t2a.dst_addr = (int) bsptest_dma_dst_addr_phy;
+	listelem->sg_node.t2a.src_addr = (int)ptls->bsptest_dma_src_addr_phy[0];
+	listelem->sg_node.t2a.dst_addr = (int) ptls->bsptest_dma_dst_addr_phy;
 	listelem->sg_node.t2a.cfn_fn = 1;
 	listelem->sg_node.t2a.cicr = 0x922;
 	listelem->sg_node.t2a.dst_elem_idx = 0;
@@ -118,29 +138,38 @@ static void dmatest_populate_sglist(struct omap_dma_sglist_node *sglist,
 	for (i = 1; i < nelem; i++) {
 		listelem = listelem->next;
 		listelem->desc_type = OMAP_DMA_SGLIST_DESCRIPTOR_TYPE3b;
-		listelem->num_of_elem = i + 10;
-		total_transferred_elem += listelem->num_of_elem;
+		listelem->num_of_elem = ptls->transfer_sizes[i];
 		listelem->sg_node.t3b.src_or_dest_addr =
-			(int) bsptest_dma_src_addr_phy[i];
+			(int) ptls->bsptest_dma_src_addr_phy[i];
 		listelem->flags = OMAP_DMA_LIST_SRC_VALID;
 	}
 }
 
-static int dmasglist_test1(void)
+static int dmasglist_test1(void *info)
 {
 	int rc;
 	int i;
+	struct tlist_transfer *tls = (struct tlist_transfer *)info;
+	int *bsptest_dma_src_addr = tls->bsptest_dma_src_addr;
+	int *bsptest_dma_src_addr_phy = tls->bsptest_dma_src_addr_phy;
+	int *bsptest_dma_dst_addr = tls->bsptest_dma_dst_addr;
+	int *bsptest_dma_dst_addr_phy = tls->bsptest_dma_dst_addr_phy;
+	int sglist_id = tls->sglist_id;
+	struct omap_dma_sglist_node *user_sglist = tls->user_sglist;
+
 	/* fill a test pattern into source elements */
 	for (i = 0; i < num_elements_in_list; ++i) {
+		tls->transfer_sizes[i] = (i + 10) * test_element_size;
 		bsptest_dma_src_addr[i] =
 			(unsigned int) dma_alloc_coherent(NULL,
-			(10+i) * 4, &(bsptest_dma_src_addr_phy[i]), 0);
-	int dma_lch;
-	int rc;
-		memset((void *)bsptest_dma_src_addr[i], i+0x41, (10+i)*4);
-		printk("\n Scatter src %d ptr = %x, phy=%x",
+					tls->transfer_sizes[i] * 4,
+					&(bsptest_dma_src_addr_phy[i]), 0);
+		memset((void *)bsptest_dma_src_addr[i], i+0x41,
+			tls->transfer_sizes[i] * 4);
+		printk(KERN_INFO "\n Scatter src %d ptr = %x, phy=%x",
 			i, bsptest_dma_src_addr[i],
 			bsptest_dma_src_addr_phy[i]);
+		tls->total_num_elements += tls->transfer_sizes[i];
 	}
 
 	/* Debug Block */
@@ -151,20 +180,18 @@ static int dmasglist_test1(void)
 		printk("\n Src data pattern %x %x", x, y);
 	}
 	bsptest_dma_dst_addr = dma_alloc_coherent(NULL,
-		num_elements_in_list * test_element_size * 4,
+		tls->total_num_elements * 4,
 		(dma_addr_t *)&bsptest_dma_dst_addr_phy, 0);
+
 	/* Clean up the destination for verification */
 	memset(bsptest_dma_dst_addr, 0xFF,
-			num_elements_in_list * test_element_size * 4);
-	printk("\n Gather Dest ptr =%x, phy=%x", bsptest_dma_dst_addr,
-			bsptest_dma_dst_addr_phy);
-	for (i = 0; i < num_elements_in_list * test_element_size; i++) {
-		if (*(bsptest_dma_dst_addr+i) != 0xFFFFFFFF) {
-			printk("\nERR::: non0 %x at %d",
-					*(bsptest_dma_dst_addr+i), i);
-			break;
-		}
-	}
+			tls->total_num_elements * 4);
+
+	printk("\n Gather Dest ptr =%x, phy=%x", (int) bsptest_dma_dst_addr,
+			(int)bsptest_dma_dst_addr_phy);
+	tls->bsptest_dma_dst_addr = bsptest_dma_dst_addr;
+	tls->bsptest_dma_dst_addr_phy = bsptest_dma_dst_addr_phy;
+
 	rc = omap_request_dma_sglist(OMAP_DMA_NO_DEVICE,
 			"SGlist transfer", dma_sglist_cb_final,
 			&sglist_id, num_elements_in_list, &user_sglist);
@@ -172,32 +199,34 @@ static int dmasglist_test1(void)
 		return rc;
 	if (NULL == user_sglist)
 		return -ENOBUFS;
-	printk("\nSGLIST id = %d, mem = %x, phy = %x\n",
-		sglist_id, (int)user_sglist,
-		(int)virt_to_dma(NULL, user_sglist));
-	dmatest_populate_sglist(user_sglist, num_elements_in_list);
+	tls->user_sglist = user_sglist;
+	tls->sglist_id = sglist_id;
+	dmatest_populate_sglist(tls, num_elements_in_list);
 	rc = omap_set_dma_sglist_params(sglist_id,
 			user_sglist, &transfer_params);
 	if (rc)
 		return rc;
+
 	omap_dma_set_prio_lch(sglist_id, DMA_CH_PRIO_HIGH, DMA_CH_PRIO_HIGH);
 	omap_dma_set_sglist_fastmode(sglist_id, 1);
-	omap_set_dma_callback(sglist_id, dma_sglist_cb_inter, NULL);
+	omap_set_dma_callback(sglist_id, dma_sglist_cb_inter, tls);
 
-	rc = omap_start_dma_sglist_transfers(sglist_id, 4);
+	rc = omap_start_dma_sglist_transfers(sglist_id, PAUSE_AT_ELEMENT);
 
 	return rc;
 }
 static void __exit dmatest_cleanup(void)
 {
 	int i;
-	dma_free_coherent(NULL, num_elements_in_list * test_element_size * 4,
-		bsptest_dma_dst_addr, bsptest_dma_dst_addr_phy);
+	dma_free_coherent(NULL, tls1.total_num_elements * 4,
+		tls1.bsptest_dma_dst_addr, (int)tls1.bsptest_dma_dst_addr_phy);
 	for (i = 0; i < num_elements_in_list; ++i)
 		dma_free_coherent(NULL,
-			(10+i)*4,
-			bsptest_dma_src_addr[i], bsptest_dma_src_addr_phy[i]);
-	i = omap_release_dma_sglist(sglist_id);
+				tls1.transfer_sizes[i] * 4,
+				(void *)tls1.bsptest_dma_src_addr[i],
+				(int)tls1.bsptest_dma_src_addr_phy[i]);
+	i = omap_release_dma_sglist(tls1.sglist_id);
+
 }
 static int __init dmatest_init(void)
 {
@@ -209,7 +238,7 @@ static int __init dmatest_init(void)
 				DMA_DEFAULT_FIFO_DEPTH,
 				DMA_THREAD_RESERVE_ONET |
 				DMA_THREAD_FIFO_25);
-	ret = dmasglist_test1();
+	ret = dmasglist_test1(&tls1);
 	return ret;
 }
 
