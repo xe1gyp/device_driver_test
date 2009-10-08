0cdbba2442f06fbbfbafb8481b2222b5418bf88f
Ricardo Perez Olivares <x0081762@ti.com>
2009-10-07 22:27:29 -0500

    TestSuites: LMBENCH: Minor fix to default script
    Fixing a '"' missing who create an unexpected behavior

--- lmbench/test_code/scripts/default.sh
+++ lmbench/test_code/scripts/default.sh
@@ -5,5 +5,5 @@ echo -e "1\n 1\n $TEST_MB\n all\n \n \n \n \n \n \n \n no\n" |./config-run
 ./results
 echo "==========================================================================================="
 echo "Search the results file SDP3430-3430.0 or .X depends of the times that you run the test \n"
-echo "it's located in ../utils/lmbench-3.0-a9/results/
+echo "it's located in ../utils/lmbench-3.0-a9/results/"
 echo "==========================================================================================="
