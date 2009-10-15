72935a9694a2f3eef04130d791b5ebf3aa604db7
Govindraj R <govindraj.raja@ti.com>
2009-10-15 12:21:32 +0530

    HSUART test code cleanup This patch updates hsuart test application with latest test code. Also removes hs_uart test module used to test hsuart library which is not available in Liunx-Omap tree
    Signed-off-by: Govindraj R <govindraj.raja@ti.com>

--- hsuart/test_code/utils/hsuart_applications/common.h
+++ hsuart/test_code/utils/hsuart_applications/common.h
@@ -1,36 +1,45 @@
-#include <stdio.h>   /* Standard input/output definitions */
-#include <string.h>  /* String function definitions */
-#include <unistd.h>  /* UNIX standard function definitions */
-#include <fcntl.h>   /* File control definitions */
-//#include <sys/time.h>    /* To calulate time taken for operaton */
-#include <time.h>
-#include <errno.h>   /* Error number definitions */
-#include <termios.h> /* terminal control definitions */
+#include <stdio.h>	/* Standard input/output definitions */
+#include <string.h>	/* String function definitions */
+#include <unistd.h>	/* UNIX standard function definitions */
+#include <fcntl.h>	/* File control definitions */
+#include <sys/time.h>	/* To calulate time taken for operaton */
+#include <errno.h>	/* Error number definitions */
+#include <termios.h>	/* terminal control definitions */
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <signal.h>
-
+#include <sys/ioctl.h>
 
 #define ERROR -1
 #define SUCCESS 1
 
-static int bufsize = 4096 ;             /* buffer of size 4K */
+#define UART_DEV_NAME   "/dev/ttyO0"
+
+static int bufsize = 4096 ;	/* buffer of size 4K */
+//static int bufsize = 2048;	/* buffer of size 4K */
 
 /* contains varibles required for UART PORT TESTING */
 struct uart_test {
-        int fd;						/* varibale to store the port file descriptor value */
-        long int baudrate;			        /* baudrate to be set for Tx and Rx */
-        int flow_cntrl;			              /* flow control data */
-        struct timeval tx_start_time, tx_end_time;      /* used to calulate time intervals */
-        long int tx_sec,tx_usec;                        /* used to store time interval */
+        int		fd;          	               		/* varibale to store the port file descriptor value */
+        long int	baudrate;       	 	        /* baudrate to be set for Tx and Rx */
+        int		flow_cntrl;  		       		/* flow control data */
+        struct timeval start_time, end_time, diff_time;  	/* used to calulate time intervals */
 };
 
+/**
+* Structure used to store existing port informatiom
+* before we re-configure the port as per our testing
+* requirements.
+*/
 struct termios oldtio;
 
 int fd1,fd2;
 struct uart_test ut;
+char tx_rx;
+int read_flag;
+FILE *md5_fd;
 
 int writeport(int *fd, unsigned char *chars,int len);
 
@@ -45,3 +54,5 @@ int initport(int fd,long int baudrate,int flow_ctrl);
 void close_port();
 
 void display_intro();
+
+void signalHandler();
