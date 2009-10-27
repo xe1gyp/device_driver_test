5ef28778cad587e9cca694759a7490e2d509bb49
Venkatraman S <svenkatr@ti.com>
2009-10-27 16:08:16 +0530

    Battery: SMP/Thread test scenarios
    Signed-off-by: Venkatraman S <svenkatr@ti.com>

---
+++ battery/test_code/scripts/helper/battery_thread.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# (C) Texas Instruments 2009
+# Initial Version 21.Oct.2009
+#
+# battery_thread.sh - Access Battery params from multiple threads
+
+if [ $# != 2 ]
+then
+	echo "***Error: Missing argument"
+	echo "     Invoke as $0 <Test Platform type> <numiterations>"
+	echo "     Example: $0 OMAP 400"
+	exit -1
+fi
+export TEST_PLATFORM=$1
+#Include the common utilities needed for Battery tests
+. battery_util.sh
+
+#Global variables
+Global_test_result=0 # Set Pass
+Global_ERR=0
+
+function access_battery_params()
+{
+	loopcount="0"
+	loopmax=$1
+	for (( loopcount = 0 ; loopcount <= loopmax ; loopcount++ ))
+	do
+		battery_test_get_battery_state retval1 retval2 retval3 retval4 retval5
+		if [ $Global_ERR != 0 ]
+		then
+			echo "Error encountered: $$ failed on $loopcount iteration"
+			loopcount=$loopmax
+		fi
+	done
+	echo "Pid $$ done"
+
+}
+
+access_battery_params $2
+
