690c98789a2ca83817e16267f794f984a546738e
Cristopherson Torres <x0096871@ti.com>
2009-11-13 12:23:42 -0600

    TestSuites: Video: Symbolic link deprecated
    lib.h header was using a symbolic link which does not exist in Android.
    Pointing to the video device is needed in order to avoid issues related
    with FS structure.
    
    Signed-off-by: Cristopherson Torres <x0096871@ti.com>

--- video/test_code/utils/video/lib.h
+++ video/test_code/utils/video/lib.h
@@ -1,7 +1,7 @@
 #include "omap24xxvout.h"
 
-#define VIDEO_DEVICE1 "/dev/v4l/video1"
-#define VIDEO_DEVICE2 "/dev/v4l/video2"
+#define VIDEO_DEVICE1 "/dev/video1"
+#define VIDEO_DEVICE2 "/dev/video2"
 
 #define OMAP2_V1	1
 #define OMAP2_V2	2
