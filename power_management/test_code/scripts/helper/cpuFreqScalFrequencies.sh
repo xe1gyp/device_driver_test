cd6cfba51f8e9e82283a73795e61fb984477fe03
David Miramontes <david.miramontes@ti.com>
2009-11-30 19:45:44 -0600

    Testsuites:Power_Management: Negative scenario invalid cpu Freq
    Try to set invalid CPU frequency and check for the system stability.
    System should be stable.
    
    Signed-off-by: David Miramontes <david.miramontes@ti.com>

--- power_management/test_code/scripts/helper/cpuFreqScalFrequencies.sh
+++ power_management/test_code/scripts/helper/cpuFreqScalFrequencies.sh
@@ -44,6 +44,38 @@ elif [ "$LOCAL_COMMAND" = "set" ]; then
   		fi
   done
 
+elif [ "$LOCAL_COMMAND" = "set_fail" ]; then
+
+  # Changing through all the frequencies not available
+  not_available_frequencies="600000 550000 500000 250000 125000 700000 800000 750000 225000 25000"
+
+  for i in $not_available_frequencies
+	 do
+  		# Obtain the available freq to compare with freq not available
+  		available_frequencies=`cat $SCALING_AVAILABLE_FREQUENCIES`
+
+  		for j in $available_frequencies
+   			do
+   				if [ "$i" = "$j" ]
+   				then
+   					
+   					continue 2
+   				fi
+   		done
+
+  		echo "Setting Frequency to " $i
+  		echo "echo $i > $SCALING_SET_SPEED"
+  		echo $i > $SCALING_SET_SPEED
+  		cur_frequency=`cat $SCALING_CUR_FREQ`
+  		if [ "$i" != "$cur_frequency" ]
+  		then
+  			echo "Current frequency is different from the set one, as expected"
+   		else
+  			echo "Fatal: Current frequency is the same from the set one"
+  			exit 1
+  		fi
+  done
+
 else
   echo "Command in cpuFrequencyScaling is not supported"
   exit 1
