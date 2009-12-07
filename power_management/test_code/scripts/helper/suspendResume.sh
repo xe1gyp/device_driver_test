d4e3ce9e0d4df3db7b8b8a351275994a8d88adac
Aharon Campos <aharon.campos@ti.com>
2009-12-07 15:11:56 -0600

    Testsuites:Power:Increase delay to avoid message loss
    The suspendResume.sh script needs a delay after returning from suspend and
    before reading /var/log/messages to ensure every printk message appears on
    the file. Increase this delay to decrease the possibility of losing any
    message.
    
    Signed-off-by: Aharon Campos <aharon.campos@ti.com>

--- power_management/test_code/scripts/helper/suspendResume.sh
+++ power_management/test_code/scripts/helper/suspendResume.sh
@@ -10,7 +10,7 @@ if [ "$LOCAL_COMMAND" = "suspend" ]; then
   echo $LOCAL_WAKEUP_TIME > $WAKEUP_TIMER_SECONDS
   echo $MEM > $STATE
   echo $DISABLE > $WAKEUP_TIMER_SECONDS
-  sleep 1
+  sleep 25
   cat $KERNEL_MESSAGES | grep "Successfully"
   
   if [ $? != 0 ]; then
