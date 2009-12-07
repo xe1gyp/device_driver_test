6ec65e104b74f28cc105022563273b9521b6a630
Carlos Lopez <carlos.cruz@sasken.com>
2009-12-07 15:55:26 -0600

    Testsuites: Framebuffer: fbflipfb.sh script call a binary that that does not exist due to a typo, this patch remove typo and the script call the required binary
    Signed-off-by: Carlos Lopez <carlos.cruz@sasken.com>

--- framebuffer/test_code/scripts/helper/fbflipfb.sh
+++ framebuffer/test_code/scripts/helper/fbflipfb.sh
@@ -3,7 +3,7 @@
   $TESTBIN/fbmode 16
   
   #Usage: fbflipbut
-  $TESTBIN/fbflipbut
+  $TESTBIN/fbflipbuf
 
 if [ -z "$STRESS" ]; then
   echo "";echo "Are the second and third buffers filled with 2 different colors and the FB screen color changed fully and finally the screen flips back to the 1st FB, is the FB showing whatever it was displaying before running the test?"; echo ""
