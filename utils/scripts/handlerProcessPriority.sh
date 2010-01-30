62025cfab4febcc9aed8fdae84979b189fd3e426
Cristopherson Torres <x0096871@ti.com>
2010-01-29 18:24:43 -0600

    TestSuites: Utils: Scripts to test process priority.
    Those scripts are being ported from 24x branch and will be used to test
    some process priority test cases.
    
    Signed-off-by: Cristopherson Torres <x0096871@ti.com>

---
+++ utils/scripts/handlerProcessPriority.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+LOCAL_OPERATION=$1
+
+if [ "$LOCAL_OPERATION" = "add" ]; then
+	LOCAL_COMMAND_NUMBER=$2
+	LOCAL_COMMAND_LINE=$3
+	LOCAL_COMMAND_PRIORITY=$4
+
+	echo $LOCAL_COMMAND_LINE > $HPP_COMMAND_LINE.$LOCAL_COMMAND_NUMBER
+	echo $LOCAL_COMMAND_PRIORITY > $HPP_COMMAND_PRIORITY_VALUE.$LOCAL_COMMAND_NUMBER
+
+elif [ "$LOCAL_OPERATION" = "execute" ]; then
+
+	LOCAL_TIMES=$2
+
+	tempPriorityValue1=`cat $HPP_COMMAND_PRIORITY_VALUE.1`
+	tempPriorityValue2=`cat $HPP_COMMAND_PRIORITY_VALUE.2`
+
+	if [ $tempPriorityValue1 -lt $tempPriorityValue2 ]; then
+    echo 1 > $HPP_COMMAND_PRIORITY_HIGHER
+	elif [ $tempPriorityValue1 -gt $tempPriorityValue2 ]; then
+    echo 2 > $HPP_COMMAND_PRIORITY_HIGHER
+  elif [ $tempPriorityValue1 -eq $tempPriorityValue2 ]; then
+    echo equal > $HPP_COMMAND_PRIORITY_HIGHER
+	fi
+
+  $UTILSCRIPTS/handlerProcessPriorityExecutor.sh "1" "`cat $HPP_COMMAND_LINE.1`" "$LOCAL_TIMES" "`cat $HPP_COMMAND_PRIORITY_VALUE.1`" &
+  $UTILSCRIPTS/handlerProcessPriorityExecutor.sh "2" "`cat $HPP_COMMAND_LINE.2`" "$LOCAL_TIMES" "`cat $HPP_COMMAND_PRIORITY_VALUE.2`" &
+
+  wait
+
+elif [ "$LOCAL_OPERATION" = "verify" ]; then
+
+	echo -e "\nInfo: Program 1 | Initial | Final"
+	echo -e "`cat $HPP_PROCFS_PID_SCHED_START.1`\n`cat $HPP_PROCFS_PID_SCHED_CURRENT.1`\n"
+	echo -e "\nInfo: Program 2 | Initial | Final"
+	echo -e "`cat $HPP_PROCFS_PID_SCHED_START.2`\n`cat $HPP_PROCFS_PID_SCHED_CURRENT.2`\n"
+
+	val1=`cat $HPP_PROCFS_PID_SCHED_START.1 | awk '{print $3}'`
+	val2=`cat $HPP_PROCFS_PID_SCHED_CURRENT.1  | awk '{print $3}'`
+	fvalue1=`echo "$val2-$val1" | bc`
+	fvalue1=`echo $fvalue1 | awk '{printf "%.0f\n", $1}'`
+	echo "Info: Program 1 | The final computed value for process 1 is $fvalue1"
+
+	val1=`cat $HPP_PROCFS_PID_SCHED_START.2 | awk '{print $3}'`
+	val2=`cat $HPP_PROCFS_PID_SCHED_CURRENT.2  | awk '{print $3}'`
+	fvalue2=`echo "$val2-$val1" | bc`
+	fvalue2=`echo $fvalue2 | awk '{printf "%.0f\n", $1}'`
+	echo "Info: Program 2 | The final computed value for process 2 is $fvalue2"
+
+	LOCAL_PROCESS_TO_FINISH_FIRST=`cat $HPP_COMMAND_PRIORITY_HIGHER`
+
+  if [ "$LOCAL_PROCESS_TO_FINISH_FIRST" = "equal" ]; then
+    echo -e "\nInfo: We don't know what process should have finished first\n"
+    return 0
+  else
+    echo -e "\nInfo: Process with higher priority is $LOCAL_PROCESS_TO_FINISH_FIRST"
+	  if [ "$LOCAL_PROCESS_TO_FINISH_FIRST" = "1" ]; then
+		  if [ "$fvalue1" -lt "$fvalue2" ]; then
+			echo -e "Info: Passed\n"
+		else
+			echo -e "Info: Failed!\n"
+			return 1
+		fi
+	  elif [ "$LOCAL_PROCESS_TO_FINISH_FIRST" = "2" ]; then
+		if [ "$fvalue2" -lt "$fvalue1" ]; then
+			echo -e "Info: Passed!\n"
+		else
+			echo -e "Info: Failed!\n"
+			return 1
+		fi
+	fi
+	fi
+
+fi
+
+# End of file
---
+++ utils/scripts/handlerProcessPriorityExecutor.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+LOCAL_SCRIPT_PID=`echo $$`
+
+LOCAL_COMMAND_NUMBER=$1
+LOCAL_COMMAND_LINE=$2
+LOCAL_EXECUTION_TIMES=$3
+LOCAL_COMMAND_PRIORITY=$4
+
+cat /proc/$LOCAL_SCRIPT_PID/sched | grep se.sum_exec_runtime > $HPP_PROCFS_PID_SCHED_START.$LOCAL_COMMAND_NUMBER
+
+$UTILBIN/scheduler_priority_set $LOCAL_SCRIPT_PID $LOCAL_COMMAND_PRIORITY
+
+count=0
+while [ $count -lt $LOCAL_EXECUTION_TIMES ]
+do
+	eval $LOCAL_COMMAND_LINE
+	echo -e "Info: Program $LOCAL_COMMAND_NUMBER | PID $LOCAL_SCRIPT_PID | Priority $LOCAL_COMMAND_PRIORITY | Count $count of $LOCAL_EXECUTION_TIMES"
+	cat /proc/$LOCAL_SCRIPT_PID/sched | grep se.sum_exec_runtime > $HPP_PROCFS_PID_SCHED_CURRENT.$LOCAL_COMMAND_NUMBER
+	count=`expr $count + 1`
+done
+
+# End of file
