7b29b7c85d0dc604abde9ba8ce245ecd7337e6e5
Cristopherson Torres <x0096871@ti.com>
2010-02-02 12:45:22 -0600

    TestSuites: Audio: Updating aplay and amixer names.
    aplay and amixer binaries use different names on Android FS.
    To fix this issue, environment variables are used instead of static names.
    
    Signed-off-by: Cristopherson Torres <x0096871@ti.com>

--- audio-alsa/test_code/scripts/helper/decrease_gain_test.sh
+++ audio-alsa/test_code/scripts/helper/decrease_gain_test.sh
@@ -2,10 +2,10 @@
 
 GAIN=18
 
-aplay $STEREO_AUDIO_FILE"11025.wav" &
+$APLAY $STEREO_AUDIO_FILE"11025.wav" &
 while [ $GAIN -ge 0 ]
 do
-	amixer cset numid=7 $GAIN
+	$AMIXER cset numid=7 $GAIN
 	sleep 1
 	GAIN=`expr $GAIN - 1`
 done
--- audio-alsa/test_code/scripts/helper/increase_gain_test.sh
+++ audio-alsa/test_code/scripts/helper/increase_gain_test.sh
@@ -2,10 +2,10 @@
 
 GAIN=0
 
-aplay $STEREO_AUDIO_FILE"11025.wav" &
+$APLAY $STEREO_AUDIO_FILE"11025.wav" &
 while [ $GAIN -le 18 ]
 do
-	amixer cset numid=7 $GAIN
+	$AMIXER cset numid=7 $GAIN
 	sleep 1
 	GAIN=`expr $GAIN + 1`
 done
