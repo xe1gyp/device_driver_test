35b9347185422fc602a10bbf0a62b1224e4f5b52
Mayuresh Janorkar <mayur@ti.com>
2010-02-09 00:00:00 -0600

    FBTEST adding Run-All script
    This patch adds a script sothat all the
    	fb scenarios are run for OMAP4
    
    Signed-off-by: Mayuresh Janorkar <mayur@ti.com>

---
+++ framebuffer/test_code/scripts/run_all.sh
@@ -0,0 +1,9 @@
+./test_runner.sh -S L_DD_FB_0001
+./test_runner.sh -S L_DD_FB_0005
+./test_runner.sh -S L_DD_FB_0007
+./test_runner.sh -S L_DD_FB_0008
+./test_runner.sh -S L_DD_FB_0011
+./test_runner.sh -S L_DD_FB_0017
+./test_runner.sh -S L_DD_FB_0021
+./test_runner.sh -S L_DD_FB_0039
+
