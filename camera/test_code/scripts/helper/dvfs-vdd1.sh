08c858210eb8f135bcb04537ce6eed3a23787be4
Akbar Ansari <x0114916@ti.com>
2010-02-10 16:03:51 +0530

    Testsuites:camera:dvfs test senerio get change.
    Signed-off-by: Akbar Ansari <x0114916@ti.com>
    Signed-off-by: Dominic Curran <dcurran@ti.com>

--- camera/test_code/scripts/helper/dvfs-vdd1.sh
+++ camera/test_code/scripts/helper/dvfs-vdd1.sh
@@ -5,12 +5,8 @@ FPS=$2
 FORMAT=$3
 SIZE=$4
 
-MINCOUNT=0
-vdd1_opp_no=5
 
-echo -n $vdd1_opp_no > /sys/power/vdd1_lock
-
-#Run streaming at background
+# Run streaming in background
 if [ "$SIZE" = "VGA" ]; then
 time $TESTBIN/streaming_frame $DEVICE $FPS $FORMAT $SIZE &
 else
@@ -18,15 +14,39 @@ time $TESTBIN/fps $DEVICE $FORMAT $SIZE 250 $FPS &
 fi
 
 
-while [ "$vdd1_opp_no" -ne $MINCOUNT ]
+# Changing the governor to userspace.
+GOV=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor`
+echo "userspace" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+
+# Checking if scaling_setspeed has been created
+if [ ! -e /sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed ]
+then
+	echo "Fatal: scaling_setspeed is not available"
+	exit 1
+fi
+
+# Changing through all the frequencies available
+available_frequencies=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies`
+
+for i in $available_frequencies
 do
 	sleep 5
-	echo -n $vdd1_opp_no > /sys/power/vdd1_lock
-	echo VDD1:
-	cat /sys/power/vdd1_opp
-	vdd1_opp_no=`expr $vdd1_opp_no - 1`
+	echo ""
+	echo "Setting ARM Frequency to" $i "KHz"
+	echo ""
+	echo $i > /sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
+	cur_freq=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq`
+	if [ "$i" != "$cur_freq" ]; then
+		echo "ERROR: Current frequency $cur_freq KHz is different from requested one!"
+	fi
 done
 sleep 5
+
+
+# Switch back to original governor.
+echo $GOV > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "Reset Governor to:" `cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor`
+
 
 if [ -z "$STRESS" ]; then
     echo "";echo " Was streaming video at $FPS fbs and OPP values displayed cleanly on LCD screen?";echo ""
