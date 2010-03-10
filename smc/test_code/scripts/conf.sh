6fa26cca85d56534ebcde5080aea344aa0f60ca1
Praneeth Bajjuri <praneeth@ti.com>
2010-03-10 16:37:16 -0500

    Testsuites:SMC Security MiddleWare Component for OMAP3xxx
    This change is to introduce testsuite for security middleware component
    which provides a highly portable environment for running
    security-sensitive code in a service-oriented fashion by exposing TF API
    thru its built-in services
    
    Signed-off-by: Praneeth Bajjuri <praneeth@ti.com>

---
+++ smc/test_code/scripts/conf.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Configuration Parameters for SMC testsuite
+# Testsuite variables
+export POSTFIX=`date "+%Y%m%d-%H%M%S"`
+export TESTROOT=${PWD}
+export TESTBIN=${TESTROOT}/../bin
+export UTILBIN=${TESTROOT}/../../utils/bin
+export TESTMODS=/smc25
+export TESTSCRIPT=${TESTROOT}/helper
+export TMPBASE=${TESTROOT}/tmp
+export TMPFILE=${TMPBASE}/tmp.$POSTFIX
+export CMDFILE=cmd.$POSTFIX
+export TESTDIR=${TESTROOT}/test
+export PRETTY_PRT=""
+export VERBOSE=""
+export OUTPUTFILE=${TESTROOT}/output.$POSTFIX
+export LOGFILE=${TESTROOT}/log.$POSTFIX
+export DURATION=""
+export PATH="${PATH}:${TESTROOT}:${TESTBIN}:${TESTSCRIPT}"
+export TC_SCENARIO="${TESTROOT}/scenarios"
+export SCENARIO_NAMES=""
+
+# SMC specific variables
+export PROCFS_DEVICES=/proc/devices
+# End of file
