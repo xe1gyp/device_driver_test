58390d74f3798e72391eb2c2d651d114dd85c4c2
Abraham Arce <x0066660@ti.com>
2010-03-30 21:47:05 -0600

    TestSuites: Utils: Process Priority SMP Tests
    The framework will allow now to test the following scenarios
    
    - 2 process running at a specific priority starting at the same time
    - N process running at a specific priority starting at the same time
    - N process running at a specific priority starting at different times
      The difference in starting times will be given by the framework based on
      a random generated number between 1 and 5 seconds
    
    To Do
    
    - N process running at a random priority starting at the same time
    - N process running at a random priority starting at different times
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

--- utils/handlers/handlerProcessPriority.sh
+++ utils/handlers/handlerProcessPriority.sh
@@ -16,105 +16,170 @@ LOCAL_OPERATION=$1
 # Main
 # =============================================================================
 
-test -f /proc/1/sched
-if [ $? -eq 1 ]
-then
-	echo "Fatal: missing /proc/<process>/sched, cannot continue, directory looks like"
-	echo "Info : please enable in kernel menuconfig the macro CONFIG_SCHED_DEBUG"
-	echo "Info : Kernel Hacking -> Kernel Debugging -> Collect scheduler debugging info"
-	ls /proc/1/
+handlerError.sh "test"
+if [ $? -eq 1 ]; then
 	return 1
 fi
 
-handlerError.sh "test"
-if [ $? -eq 1 ]; then
-  return 1
+test -f /proc/1/sched
+if [ $? -eq 1 ]
+then
+	handlerError.sh "log" "$?" "halt" "handlerProcessPriority.sh"
+	echo "Fatal: missing /proc/<process>/sched, cannot continue, directory looks like"
+	echo "Info : please enable in kernel menuconfig the macro CONFIG_SCHED_DEBUG"
+	echo "Info : Kernel Hacking -> Kernel Debugging -> Collect scheduler debugging info"
+	return 1
 fi
 
-if [ "$LOCAL_OPERATION" = "run" ]; then
+if [ "$LOCAL_OPERATION" = "clean" ]; then
 
-	LOCAL_COMMAND_LINE=$2
-	eval $LOCAL_COMMAND_LINE
+	test -f $HPPR_LIST_CMDS_TOTALS && rm $HPPR_LIST_CMDS_TOTALS
+	test -f $HPPR_LIST_CMDS_PASSED && rm $HPPR_LIST_CMDS_PASSED
+	test -f $HPPR_LIST_PIDS_TOTALS && rm $HPPR_LIST_PIDS_TOTALS
+
+	touch $HPPR_LIST_CMDS_TOTALS
+	touch $HPPR_LIST_CMDS_PASSED
+	touch $HPPR_LIST_PIDS_TOTALS
+
+	touch $HPPR_LIST_CMDS_FAILED
+	test -f $HPPR_LIST_CMDS_FAILED && rm $HPPR_LIST_CMDS_FAILED
 
 elif [ "$LOCAL_OPERATION" = "add" ]; then
+
 	LOCAL_COMMAND_NUMBER=$2
 	LOCAL_COMMAND_LINE=$3
 	LOCAL_COMMAND_PRIORITY=$4
 
-	echo $LOCAL_COMMAND_LINE > $HPP_COMMAND_LINE.$LOCAL_COMMAND_NUMBER
-	echo $LOCAL_COMMAND_PRIORITY > $HPP_COMMAND_PRIORITY_VALUE.$LOCAL_COMMAND_NUMBER
+	echo $LOCAL_COMMAND_LINE > $HPPR_COMMAND_LINE.$LOCAL_COMMAND_NUMBER
+	echo $LOCAL_COMMAND_PRIORITY > $HPPR_COMMAND_PRIORITY.$LOCAL_COMMAND_NUMBER
+
+	echo "add" > $HPPR_OPERATION_CURRENT
+
+elif [ "$LOCAL_OPERATION" = "assign" ]; then
+
+	LOCAL_COMMAND_LINE=$2
+	LOCAL_COMMAND_PRIORITY=$3
+
+	echo "$LOCAL_COMMAND_LINE:$LOCAL_COMMAND_PRIORITY" >> $HPPR_LIST_CMDS_TOTALS
+
+	echo "assign" > $HPPR_OPERATION_CURRENT
 
 elif [ "$LOCAL_OPERATION" = "execute" ]; then
 
-	LOCAL_TIMES=$2
+	LOCAL_CURRENT_OPERATION=`cat $HPPR_OPERATION_CURRENT`
+	LOCAL_COMMAND_DELAY="0"
 
-	tempPriorityValue1=`cat $HPP_COMMAND_PRIORITY_VALUE.1`
-	tempPriorityValue2=`cat $HPP_COMMAND_PRIORITY_VALUE.2`
+	if [ "$LOCAL_CURRENT_OPERATION" = "add" ]; then
 
-	if [ $tempPriorityValue1 -lt $tempPriorityValue2 ]; then
-    echo 1 > $HPP_COMMAND_PRIORITY_HIGHER
-    $UTILS_DIR_HANDLERS/handlerProcessPriorityExecutor.sh "1" "`cat $HPP_COMMAND_LINE.1`" "$LOCAL_TIMES" "`cat $HPP_COMMAND_PRIORITY_VALUE.1`" &
-    $UTILS_DIR_HANDLERS/handlerProcessPriorityExecutor.sh "2" "`cat $HPP_COMMAND_LINE.2`" "$LOCAL_TIMES" "`cat $HPP_COMMAND_PRIORITY_VALUE.2`" &
-	elif [ $tempPriorityValue1 -gt $tempPriorityValue2 ]; then
-    echo 2 > $HPP_COMMAND_PRIORITY_HIGHER
-    $UTILS_DIR_HANDLERS/handlerProcessPriorityExecutor.sh "2" "`cat $HPP_COMMAND_LINE.2`" "$LOCAL_TIMES" "`cat $HPP_COMMAND_PRIORITY_VALUE.2`" &
-    $UTILS_DIR_HANDLERS/handlerProcessPriorityExecutor.sh "1" "`cat $HPP_COMMAND_LINE.1`" "$LOCAL_TIMES" "`cat $HPP_COMMAND_PRIORITY_VALUE.1`" &
-  elif [ $tempPriorityValue1 -eq $tempPriorityValue2 ]; then
-    echo equal > $HPP_COMMAND_PRIORITY_HIGHER
-    $UTILS_DIR_HANDLERS/handlerProcessPriorityExecutor.sh "1" "`cat $HPP_COMMAND_LINE.1`" "$LOCAL_TIMES" "`cat $HPP_COMMAND_PRIORITY_VALUE.1`" &
-    $UTILS_DIR_HANDLERS/handlerProcessPriorityExecutor.sh "2" "`cat $HPP_COMMAND_LINE.2`" "$LOCAL_TIMES" "`cat $HPP_COMMAND_PRIORITY_VALUE.2`" &
+		LOCAL_REPEATABILITY=$2
+
+		tempPriorityValue1=`cat $HPPR_COMMAND_PRIORITY.1`
+		tempPriorityValue2=`cat $HPPR_COMMAND_PRIORITY.2`
+
+		FIRST_CMD="1"
+		SECOND_CMD="2"
+
+		if [ $tempPriorityValue1 -lt $tempPriorityValue2 ]; then
+			echo 1 > $HPPR_COMMAND_PRIORITY_HIGHER
+		elif [ $tempPriorityValue1 -gt $tempPriorityValue2 ]; then
+			echo 2 > $HPPR_COMMAND_PRIORITY_HIGHER
+			FIRST_CMD="2"
+			SECOND_CMD="1"
+		elif [ $tempPriorityValue1 -eq $tempPriorityValue2 ]; then
+			echo equal > $HPPR_COMMAND_PRIORITY_HIGHER
+		fi
+
+		handlerProcessPriorityExecutor.sh $FIRST_CMD "`cat $HPPR_COMMAND_LINE.$FIRST_CMD`" "$LOCAL_REPEATABILITY" "`cat $HPPR_COMMAND_PRIORITY.$FIRST_CMD`" $LOCAL_COMMAND_DELAY &
+		handlerProcessPriorityExecutor.sh $SECOND_CMD "`cat $HPPR_COMMAND_LINE.$SECOND_CMD`" "$LOCAL_REPEATABILITY" "`cat $HPPR_COMMAND_PRIORITY.$SECOND_CMD`" $LOCAL_COMMAND_DELAY &
+
+		wait
+
+	elif [ "$LOCAL_CURRENT_OPERATION" = "assign" ]; then
+
+		LOCAL_WITH_RANDOM_DELAY=$2
+
+		LOCAL_INSTANCE=0
+
+		while read LOCAL_LINE
+		do
+			LOCAL_INSTANCE=`expr $LOCAL_INSTANCE + 1`
+			LOCAL_COMMAND_LINE=`echo $LOCAL_LINE | cut -d: -f1`
+			LOCAL_COMMAND_PRIORITY=`echo $LOCAL_LINE | cut -d: -f2`
+			LOCAL_COMMAND_DELAY="0"
+
+			if [ "$LOCAL_WITH_RANDOM_DELAY" = "withrandomdelay" ]
+			then
+
+				LOCAL_COMMAND_DELAY_TEMP=`dd if=/dev/urandom count=1 2> /dev/null | cksum | cut -f1 -d" "`
+				LOCAL_COMMAND_DELAY=`echo "$LOCAL_COMMAND_DELAY%5" | bc`
+
+			fi
+
+			handlerProcessPriorityExecutor.sh "$LOCAL_INSTANCE" "$LOCAL_COMMAND_LINE" "1" "$LOCAL_COMMAND_PRIORITY" "$LOCAL_COMMAND_DELAY" &
+
+		done < $HPPR_LIST_CMDS_TOTALS
+
+		wait
+
+		echo -e "\nInfo: Passed! > Instance | PID | Command\n"
+		cat $HPPR_LIST_CMDS_PASSED
+		echo
+
+		if [ -f $HPPR_LIST_CMDS_FAILED ]; then
+			echo -e "\nInfo: Failed! > Instance | PID | Command\n"
+			cat $HPPR_LIST_CMDS_FAILED
+			echo
+			exit 1
+		fi
 	fi
-  wait
 
 elif [ "$LOCAL_OPERATION" = "verify" ]; then
 
 	echo -e "\nInfo: Program 1 | Initial | Final"
-	echo -e "`cat $HPP_PROCFS_PID_SCHED_START.1`\n`cat $HPP_PROCFS_PID_SCHED_CURRENT.1`\n"
+	echo -e "`cat $HPPR_PROCFS_PID_SCHED_START.1`\n`cat $HPPR_PROCFS_PID_SCHED_CURRENT.1`\n"
 	echo -e "\nInfo: Program 2 | Initial | Final"
-	echo -e "`cat $HPP_PROCFS_PID_SCHED_START.2`\n`cat $HPP_PROCFS_PID_SCHED_CURRENT.2`\n"
+	echo -e "`cat $HPPR_PROCFS_PID_SCHED_START.2`\n`cat $HPPR_PROCFS_PID_SCHED_CURRENT.2`\n"
 
-	val1=`cat $HPP_PROCFS_PID_SCHED_START.1 | awk '{print $3}'`
+	val1=`cat $HPPR_PROCFS_PID_SCHED_START.1 | awk '{print $3}'`
 	val1=0
-	val2=`cat $HPP_PROCFS_PID_SCHED_CURRENT.1  | awk '{print $3}'`
+	val2=`cat $HPPR_PROCFS_PID_SCHED_CURRENT.1  | awk '{print $3}'`
 	fvalue1=`echo "scale=6; $val2-$val1" | bc`
-	#fvalue1=`echo $fvalue1 | awk '{printf "%.0f\n", $1}'`
 	echo "Info: Program 1 | Final value for process 1 is $fvalue1"
 
-	val1=`cat $HPP_PROCFS_PID_SCHED_START.2 | awk '{print $3}'`
+	val1=`cat $HPPR_PROCFS_PID_SCHED_START.2 | awk '{print $3}'`
 	val1=0
-	val2=`cat $HPP_PROCFS_PID_SCHED_CURRENT.2  | awk '{print $3}'`
+	val2=`cat $HPPR_PROCFS_PID_SCHED_CURRENT.2  | awk '{print $3}'`
 	fvalue2=`echo "scale=6; $val2-$val1" | bc`
-	#fvalue2=`echo $fvalue2 | awk '{printf "%.0f\n", $1}'`
 	echo "Info: Program 2 | Final value for process 2 is $fvalue2"
 
-	LOCAL_PROCESS_TO_FINISH_FIRST=`cat $HPP_COMMAND_PRIORITY_HIGHER`
+	LOCAL_PROCESS_TO_FINISH_FIRST=`cat $HPPR_COMMAND_PRIORITY_HIGHER`
 
-  if [ "$LOCAL_PROCESS_TO_FINISH_FIRST" = "equal" ]; then
-    echo -e "\nInfo: We don't know what process should have finished first\n"
-    return 0
-  else
-    echo -e "\nInfo: Process with higher priority is $LOCAL_PROCESS_TO_FINISH_FIRST"
-	  if [ "$LOCAL_PROCESS_TO_FINISH_FIRST" = "1" ]; then
-      LOCAL_RESULT=`echo "scale=6; $fvalue1 > $fvalue2" | bc`
-	    if [ "$LOCAL_RESULT" == "0" ]; then
-          echo -e "Info: Failed!\n"
-		      return 1
-	      else
-          echo -e "Info: Passed\n"
-          return 0
-      fi
-	  elif [ "$LOCAL_PROCESS_TO_FINISH_FIRST" = "2" ]; then
-      LOCAL_RESULT=`echo "scale=6; $fvalue2 > $fvalue1" | bc`
-	    if [ "$LOCAL_RESULT" == "0" ]; then
-          echo -e "Info: Failed!\n"
-		      return 1
-	      else
-          echo -e "Info: Passed\n"
-          return 0
-      fi
-    fi
+	if [ "$LOCAL_PROCESS_TO_FINISH_FIRST" = "equal" ]; then
+		echo -e "\nInfo: We don't know what process should have finished first\n"
+		return 0
+	else
+		echo -e "\nInfo: Process with higher priority is $LOCAL_PROCESS_TO_FINISH_FIRST"
+		if [ "$LOCAL_PROCESS_TO_FINISH_FIRST" = "1" ]; then
+			LOCAL_RESULT=`echo "scale=6; $fvalue1 > $fvalue2" | bc`
+			if [ "$LOCAL_RESULT" == "0" ]; then
+				echo -e "Info: Failed!\n"
+				return 1
+			else
+				echo -e "Info: Passed\n"
+				return 0
+			fi
+
+		elif [ "$LOCAL_PROCESS_TO_FINISH_FIRST" = "2" ]; then
+			LOCAL_RESULT=`echo "scale=6; $fvalue2 > $fvalue1" | bc`
+			if [ "$LOCAL_RESULT" == "0" ]; then
+				echo -e "Info: Failed!\n"
+				return 1
+			else
+				echo -e "Info: Passed\n"
+				return 0
+			fi
+		fi
 	fi
-
 fi
 
 # End of file
--- utils/handlers/handlerProcessPriorityExecutor.sh
+++ utils/handlers/handlerProcessPriorityExecutor.sh
@@ -6,10 +6,11 @@
 
 LOCAL_SCRIPT_PID=`echo $$`
 
-LOCAL_COMMAND_NUMBER=$1
+LOCAL_COMMAND_INSTANCE=$1
 LOCAL_COMMAND_LINE=$2
-LOCAL_EXECUTION_TIMES=$3
+LOCAL_COMMAND_REPEATABILITY=$3
 LOCAL_COMMAND_PRIORITY=$4
+LOCAL_COMMAND_DELAY=$5
 
 # =============================================================================
 # Functions
@@ -21,18 +22,34 @@ LOCAL_COMMAND_PRIORITY=$4
 # Main
 # =============================================================================
 
-cat /proc/$LOCAL_SCRIPT_PID/sched | grep se.sum_exec_runtime > $HPP_PROCFS_PID_SCHED_START.$LOCAL_COMMAND_NUMBER
+sleep $LOCAL_COMMAND_DELAY
 
-$UTILS_DIR_BIN/scheduler_priority_set $LOCAL_SCRIPT_PID $LOCAL_COMMAND_PRIORITY
+echo "Info: Started! > Instance $LOCAL_COMMAND_INSTANCE | PID $LOCAL_SCRIPT_PID | Command $LOCAL_COMMAND_LINE"
+echo $LOCAL_SCRIPT_PID >> $HPPR_LIST_PIDS_TOTALS
 
-count=0
-while [ $count -lt $LOCAL_EXECUTION_TIMES ]
+cat /proc/$LOCAL_SCRIPT_PID/sched | grep se.sum_exec_runtime > $HPPR_PROCFS_PID_SCHED_START.$LOCAL_COMMAND_INSTANCE
+
+scheduler_priority_set $LOCAL_SCRIPT_PID $LOCAL_COMMAND_PRIORITY
+
+LOCAL_COUNT=0
+while [ $LOCAL_COUNT -lt $LOCAL_COMMAND_REPEATABILITY ]
 do
 	eval $LOCAL_COMMAND_LINE
-	count=`expr $count + 1`
-  echo -e "Info: Program $LOCAL_COMMAND_NUMBER | PID $LOCAL_SCRIPT_PID | Priority $LOCAL_COMMAND_PRIORITY | Count $count of $LOCAL_EXECUTION_TIMES"
+	if [ $? -ne 0 ]
+	then
+		echo "$LOCAL_COMMAND_INSTANCE:$LOCAL_SCRIPT_PID:$LOCAL_COMMAND_LINE" >> $HPPR_LIST_CMDS_FAILED
+		exit 1
+	fi
+
+	LOCAL_COUNT=`expr $LOCAL_COUNT + 1`
+	echo "Info: Instance $LOCAL_COMMAND_INSTANCE | PID $LOCAL_SCRIPT_PID | Priority $LOCAL_COMMAND_PRIORITY | Count $LOCAL_COUNT of $LOCAL_EXECUTION_TIMES"
 done
 
-cat /proc/$LOCAL_SCRIPT_PID/sched | grep se.sum_exec_runtime > $HPP_PROCFS_PID_SCHED_CURRENT.$LOCAL_COMMAND_NUMBER
+cat /proc/$LOCAL_SCRIPT_PID/sched | grep se.sum_exec_runtime > $HPPR_PROCFS_PID_SCHED_CURRENT.$LOCAL_COMMAND_INSTANCE
+
+sed -i "/${LOCAL_SCRIPT_PID}/d" $HPPR_LIST_PIDS_TOTALS
+
+echo "Info: Finished! > Instance $LOCAL_COMMAND_INSTANCE | PID $LOCAL_SCRIPT_PID | Command $LOCAL_COMMAND_LINE"
+echo "$LOCAL_COMMAND_INSTANCE:$LOCAL_SCRIPT_PID:$LOCAL_COMMAND_LINE" >> $HPPR_LIST_CMDS_PASSED
 
 # End of file
