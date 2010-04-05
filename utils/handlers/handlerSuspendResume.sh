90c602ec3e3c63ca4b241d681c14ed3fed76b6c9
Abraham Arce <x0066660@ti.com>
2010-04-05 11:33:02 -0500

    TestSuites: Utils: Suspend Resume Handler
    Execute Suspend/Resume by modifying wakeup_timer_seconds,
    2 operations available:
    
    - handlerSuspendResume.sh "suspend" "5"
      @operation  operation to perform, suspend
      @time       seconds to wait in suspend state
    
      will suspend/resume, suspend will last the time given by the user
    
    - handlerSuspendResume.sh "run" "5" "command"
      @operation  operation to perform, run
      @time       seconds to wait in suspend state
      @command    command to execute
    
      will keep suspending/resuming while executing the given command
      until command is finished, suspend will last the time given by the
      user
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

---
+++ utils/handlers/handlerSuspendResume.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+LOCAL_OPERATION=$1
+LOCAL_WAKEUP_TIME=$2
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+suspendResume() {
+
+	LOCAL_WAKEUP_TIME=$1
+	LOCAL_COMMAND_LINE=$2
+	LOCAL_ERROR=0
+
+	if [ -n $LOCAL_COMMAND_LINE ]; then
+		$LOCAL_COMMAND_LINE &
+		LOCAL_COMMAND_PID=`echo $!`
+	fi
+
+	echo $LOCAL_WAKEUP_TIME > $PM_WAKEUP_TIMER_SECONDS
+
+	while [ 1 ]; then
+
+		echo > /var/log/messages
+		echo mem > /sys/power/state
+
+		cat /var/log/messages | grep "Successfully"
+		if [ $? != 0 ]; then
+			echo "Error: all powerdomains did not enter target state"
+			cat /var/log/messages
+			LOCAL_ERROR=1
+		fi
+
+		if [ -n $LOCAL_COMMAND_LINE ]; then
+			test -d $LOCAL_COMMAND_PID || break
+		else
+			break
+		fi
+	done
+
+	wait
+	exit $LOCAL_ERROR
+}
+
+# =============================================================================
+# Main
+# =============================================================================
+
+handlerError.sh "test"
+if [ $? -eq 1 ]; then
+	return 1
+fi
+
+if [ "$LOCAL_OPERATION" = "suspend" ]; then
+	suspendResume $LOCAL_WAKEUP_TIME
+if [ "$LOCAL_OPERATION" = "run" ]; then
+	LOCAL_COMMAND_LINE=$3
+	suspendResume $LOCAL_WAKEUP_TIME $LOCAL_COMMAND_LINE
+fi
+
+# End of file
