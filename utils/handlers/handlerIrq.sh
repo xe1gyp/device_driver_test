b8b8497ecd1c00deb49175c668febde8d8a10b21
Cristopherson Torres <x0096871@ti.com>
2010-04-08 00:03:33 -0500

    UTILS: Implementing IRQ handler
    The handlerIrq.sh script will extract the irq number which corresponds
    to some specific device driver.
    The script will store the irq number in a file and then it can be accessed
    by user when needed.
    
    Usage: 	handlerIrq.sh [operation] [device_name] [file]
    	operation: at this moment the supported operation is get which will
    		extract the irq number
    	device_name: the device id which is showed when cat /proc/interrupts
    		command is executed
    	file: the file name where the result will be stored.
    
    Example: handlerIrq.sh "get" "rtc" "/tmp/rtc_irq"
    
    Signed-off-by: Cristopherson Torres <x0096871@ti.com>

---
+++ utils/handlers/handlerIrq.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+LOCAL_OPERATION=$1
+LOCAL_DRIVER=$2
+LOCAL_FILE=$3
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+if [ "$LOCAL_OPERATION" == "get" ]; then
+	if [ "$LOCAL_DRIVER" == "" ]; then
+		echo "Error: driver value was not provided"
+		exit 1
+	fi
+
+	echo > "$LOCAL_FILE"
+	for i in `cat /proc/interrupts | grep -i $LOCAL_DRIVER | awk '{print$1}' | cut -d: -f1`
+	do
+		 echo $i >> $LOCAL_FILE
+	done
+else
+	echo "Fatal: Unsupported operation"
+	exit 1
+fi
+
+# End of file
