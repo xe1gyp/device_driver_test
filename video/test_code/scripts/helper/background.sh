62f51709eec92f6775114156888cfb033fe62441
Sudeep Basavaraj <sudeep.basavaraj@ti.com>
2010-04-14 01:09:32 +0530

    Testsuite:Video: Updated background.sh
    Enable  back ground color based on the user input &
    once again set it to  default value
    
    Signed-off-by: Sudeep Basavaraj <sudeep.basavaraj@ti.com>

--- video/test_code/scripts/helper/background.sh
+++ video/test_code/scripts/helper/background.sh
@@ -3,8 +3,18 @@
 BG_COLOR=$1
 RESULT=0
 
-#Usage: setbgcolor <output device [1: LCD 2: TV]> <24 bit RGB value>
-$TESTBIN/setbgcolor $BG_COLOR
+DEF_BG_COLOR=`cat /sys/devices/platform/omapdss/manager0/default_color`
+OVERLAY=`echo /sys/devices/platform/omapdss/overlay0/enabled`
+
+#Usage: setbgcolor <24 bit RGB value>
+
+	echo 0 > $OVERLAY
+	$TESTBIN/setbgcolor $BG_COLOR
+	sleep 2
+	$TESTBIN/setbgcolor $DEF_BG_COLOR
+	sleep 2
+	echo 1 > $OVERLAY
+
 RESULT=`command_tracking.sh $RESULT $?`
 
 if [ -z "$STRESS" ]; then
