ac494058582562872ee38b1eec2936bd7d5ce09d
Abraham Arce <x0066660@ti.com>
2010-04-27 01:25:55 -0500

    TestSuites: Utils: Throughput Argument Change +
    - 2 options are available to handlerFileThroughput.sh as a file size:
    
      1. The path of a file
         Useful when exact size of the file written is needed
    
      2. The size of a file
         Useful when no real file was created
    
    - Major clean up on indentation in all the file
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

--- utils/handlers/handlerFileThroughput.sh
+++ utils/handlers/handlerFileThroughput.sh
@@ -4,7 +4,7 @@
 # Variables
 # =============================================================================
 
-LOCAL_COMMAND=$1
+LOCAL_OPERATION=$1
 
 # =============================================================================
 # Functions                                     
@@ -18,51 +18,66 @@ LOCAL_COMMAND=$1
 
 handlerError.sh "test"
 if [ $? -eq 1 ]; then
-  return 1
+	return 1
 fi
 
-if [ "$LOCAL_COMMAND" = "testusb" ]; then
+if [ "$LOCAL_OPERATION" = "testusb" ]; then
   
-  LOCAL_FIELD=$2
-  LOCAL_ITERATIONS=$3
-  LOCAL_FILE_SIZE=$4
-  LOCAL_TIME=$5
+	LOCAL_FIELD=$2
+	LOCAL_ITERATIONS=$3
+	LOCAL_FILE_SIZE=$4
+	LOCAL_TIME=$5
   
-  #             <iterations> <buffersize> <8>
-  # througput = -----------------------------
-  #                  1000 * 1000 * sec
+	#             <iterations> <buffersize> <8>
+	# througput = -----------------------------
+	#                  1000 * 1000 * sec
 
-  throughput=`echo "scale=4; ${LOCAL_ITERATIONS}*${LOCAL_FILE_SIZE}*8/(1000*1000*${LOCAL_TIME})" | bc`
-  echo "Throughput (Mbps): ${throughput}"
-  echo "Throughput (MB/s): "
-  echo $throughput > $THROUGHPUT_VALUE
+	throughput=`echo "scale=4; ${LOCAL_ITERATIONS}*${LOCAL_FILE_SIZE}*8/(1000*1000*${LOCAL_TIME})" | bc`
+	echo "Throughput (Mbps): ${throughput}"
+	echo "Throughput (MB/s): "
+	echo $throughput > $THROUGHPUT_VALUE
 
-  if [ "$LOCAL_FIELD" = "average" ]; then
-	echo $throughput > $GENERAL_THROUGHPUT_Mbps_AVR
-  elif [ "$LOCAL_FIELD" = "maximum" ]; then
-	echo $throughput > $GENERAL_THROUGHPUT_Mbps_MIN
-  elif [ "$LOCAL_FIELD" = "minimum" ]; then
-	echo $throughput > $GENERAL_THROUGHPUT_Mbps_MAX
-  else
-	 exec
-  fi
+	if [ "$LOCAL_FIELD" = "average" ]; then
+		echo $throughput > $GENERAL_THROUGHPUT_Mbps_AVR
+	elif [ "$LOCAL_FIELD" = "maximum" ]; then
+		echo $throughput > $GENERAL_THROUGHPUT_Mbps_MIN
+	elif [ "$LOCAL_FIELD" = "minimum" ]; then
+		echo $throughput > $GENERAL_THROUGHPUT_Mbps_MAX
+	else
+		exec
+	fi
 
 else
   
-  LOCAL_INSTANCE=$2
-  LOCAL_SIZE=$3
-  LOCAL_TIME_REAL_CMD=$4
-  LOCAL_TIME_REAL_SYNC=$5
+	LOCAL_INSTANCE=$2
+	LOCAL_FILE_PATH=$3
+	LOCAL_TIME_REAL_CMD=$4
+	LOCAL_TIME_REAL_SYNC=$5
   
-  #                             LOCAL_SIZE
-  # throughput = ------------------------------------------
-  #              LOCAL_TIME_REAL_CMD + LOCAL_TIME_REAL_SYNC
+	#                             LOCAL_SIZE
+	# throughput = ------------------------------------------
+	#              LOCAL_TIME_REAL_CMD + LOCAL_TIME_REAL_SYNC
   
-  THROUGHPUT_MBps=`echo "scale=4; ${LOCAL_SIZE}/(${LOCAL_TIME_REAL_CMD}+${LOCAL_TIME_REAL_SYNC})" | bc`
-  THROUGHPUT_Mbps=`echo "scale=4; ${THROUGHPUT_MBps}*8" | bc`
+	if [ -e "$LOCAL_FILE_PATH" ]; then
+		LOCAL_FILE_SIZE=`ls -al $LOCAL_FILE_PATH | awk '{print $5}'`
+		LOCAL_FILE_SIZE=`echo "($LOCAL_FILE_SIZE/1024)/1024" | bc`
+		echo $LOCAL_FILE_SIZE > $HFT_THROUGHPUT_REAL_FILE_SIZE.$LOCAL_INSTANCE
+	else
+		LOCAL_FILE_SIZE=$LOCAL_FILE_PATH
+		echo $LOCAL_FILE_SIZE > $HFT_THROUGHPUT_REAL_FILE_SIZE.$LOCAL_INSTANCE
+	fi
 
-  echo $THROUGHPUT_MBps > $HFT_THROUGHPUT_MBps.$LOCAL_INSTANCE
-  echo $THROUGHPUT_Mbps > $HFT_THROUGHPUT_Mbps.$LOCAL_INSTANCE
+	if [ ! -z "$LOCAL_TIME_REAL_SYNC" ]; then
+		LOCAL_TIME_REAL_TOTAL=`echo "scale=4; $LOCAL_TIME_REAL_CMD+$LOCAL_TIME_REAL_SYNC" | bc`
+	else
+		LOCAL_TIME_REAL_TOTAL=$LOCAL_TIME_REAL_CMD
+	fi
+
+	THROUGHPUT_MBps=`echo "scale=4; ${LOCAL_FILE_SIZE}/($LOCAL_TIME_REAL_TOTAL)" | bc`
+	THROUGHPUT_Mbps=`echo "scale=4; ${THROUGHPUT_MBps}*8" | bc`
+
+	echo $THROUGHPUT_MBps > $HFT_THROUGHPUT_MBps.$LOCAL_INSTANCE
+	echo $THROUGHPUT_Mbps > $HFT_THROUGHPUT_Mbps.$LOCAL_INSTANCE
 
 fi
 
