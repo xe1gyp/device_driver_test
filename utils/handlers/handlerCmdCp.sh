b905b5e99dd62a3cfa862130a580d3f12a4ed8df
Abraham Arce <x0066660@ti.com>
2010-04-27 01:35:14 -0500

    TestSuites: Utils: Arguments in Cp Command +
    - Add a 4th argument which is a "nosync" operation
    
      1. If not argument is passed, then a synchronous cp operation
         will be executed
      2. If "nosync" argument is passed, then an asynchronous cp operation
         will be executed
    
    - Cp will be run through handlerAppTime.sh
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

--- utils/handlers/handlerCmdCp.sh
+++ utils/handlers/handlerCmdCp.sh
@@ -4,53 +4,69 @@
 # Variables
 # =============================================================================
 
-LOCAL_INSTANCE=$1
-LOCAL_SOURCE=$2
-LOCAL_DESTINATION=$3
+LOCAL_CP_INSTANCE=$1
+LOCAL_CP_SOURCE=$2
+LOCAL_CP_DESTINATION=$3
+LOCAL_CP_SYNC=$4
 
 # =============================================================================
 # Functions
 # =============================================================================
 
 logIt() {
-  echo -e "$1"
-  echo -e "$1" >> $HCCP_CP_LOG.$LOCAL_INSTANCE
+	echo -e "$1"
+	echo -e "$1" >> $HCCP_CP_LOG.$LOCAL_CP_INSTANCE
 }
 
 # =============================================================================
 # Main
 # =============================================================================
 
-echo > $HCCP_CP_LOG.$LOCAL_INSTANCE
+echo > $HCCP_CP_LOG.$LOCAL_CP_INSTANCE
 
-$UTILS_DIR_HANDLERS/handlerAppTop2.sh $LOCAL_INSTANCE "start"
-$UTILS_DIR_BIN/time -p -o $HCCP_EXECUTION_TIME_CP.$LOCAL_INSTANCE cp $LOCAL_SOURCE $LOCAL_DESTINATION
-$UTILS_DIR_BIN/time -p -o $HCCP_EXECUTION_TIME_SYNC.$LOCAL_INSTANCE sync
-$UTILS_DIR_HANDLERS/handlerAppTop2.sh $LOCAL_INSTANCE "stop"
-$UTILS_DIR_HANDLERS/handlerAppTop2.sh $LOCAL_INSTANCE "parse" "User"
-$UTILS_DIR_HANDLERS/handlerAppTop2.sh $LOCAL_INSTANCE "parse" "System"
+handlerAppTop2.sh "start" $LOCAL_CP_INSTANCE
+
+handlerAppTime.sh "start" $LOCAL_CP_INSTANCE "cp" "cp $LOCAL_CP_SOURCE $LOCAL_CP_DESTINATION"
+
+if [ "$LOCAL_CP_SYNC" != "nosync" ]; then
+	handlerAppTime.sh "start" $LOCAL_CP_INSTANCE "sync" "sync"
+fi
+
+handlerAppTop2.sh "stop"  $LOCAL_CP_INSTANCE
+handlerAppTop2.sh "parse" $LOCAL_CP_INSTANCE "User"
+handlerAppTop2.sh "parse" $LOCAL_CP_INSTANCE "System"
+handlerAppTime.sh "parse" $LOCAL_CP_INSTANCE "cp"
+
+if [ "$LOCAL_CP_SYNC" != "nosync" ]; then
+	handlerAppTime.sh "parse" $LOCAL_CP_INSTANCE "sync"
+fi
 
 echo 3 > /proc/sys/vm/drop_caches
 
-#cat $HAT_CPU_USAGE_VALUE_AVR.$LOCAL_INSTANCE
-#cat $HAT_CPU_USAGE_VALUE_MAX.$LOCAL_INSTANCE
-#cat $HAT_CPU_USAGE_VALUE_MIN.$LOCAL_INSTANCE
+TEMP_EXEC_TIME_CP=`cat $HATI_TIME_FINAL.$LOCAL_CP_INSTANCE.cp`
 
-LOCAL_FILE_SIZE=`ls -al ${LOCAL_DESTINATION} | awk '{print $5}'`
-LOCAL_FILE_SIZE=`echo "(${LOCAL_FILE_SIZE}/1024)/1024" | bc`
-TEMP_EXEC_TIME_CP=`cat $HCCP_EXECUTION_TIME_CP.$LOCAL_INSTANCE | grep real | sed -e 's/[ a-z]//g'`
-TEMP_EXEC_TIME_SYNC=`cat $HCCP_EXECUTION_TIME_SYNC.$LOCAL_INSTANCE | grep real | sed -e 's/[ a-z]//g'`
-$UTILS_DIR_HANDLERS/handlerFileThroughput.sh none $LOCAL_INSTANCE $LOCAL_FILE_SIZE $TEMP_EXEC_TIME_CP $TEMP_EXEC_TIME_SYNC
-THROUGHPUT_MBps=`cat $HFT_THROUGHPUT_MBps.$LOCAL_INSTANCE`
-THROUGHPUT_Mbps=`cat $HFT_THROUGHPUT_Mbps.$LOCAL_INSTANCE`
+if [ "$LOCAL_CP_SYNC" != "nosync" ]; then
+	TEMP_EXEC_TIME_SYNC=`cat $HATI_TIME_FINAL.$LOCAL_CP_INSTANCE.sync`
+fi
 
-logIt "\nInfo: cp $LOCAL_SOURCE $LOCAL_DESTINATION\n"
-logIt "Info: Real File Size (MB) : $LOCAL_FILE_SIZE"
+$UTILS_DIR_HANDLERS/handlerFileThroughput.sh none $LOCAL_CP_INSTANCE $LOCAL_CP_DESTINATION $TEMP_EXEC_TIME_CP $TEMP_EXEC_TIME_SYNC
+
+THROUGHPUT_SIZE=`cat $HFT_THROUGHPUT_REAL_FILE_SIZE.$LOCAL_CP_INSTANCE`
+THROUGHPUT_MBps=`cat $HFT_THROUGHPUT_MBps.$LOCAL_CP_INSTANCE`
+THROUGHPUT_Mbps=`cat $HFT_THROUGHPUT_Mbps.$LOCAL_CP_INSTANCE`
+
+logIt "\nInfo: cp $LOCAL_CP_SOURCE $LOCAL_CP_DESTINATION\n"
+logIt "Info: Real File Size (MB) : $THROUGHPUT_SIZE"
 logIt "Info: Exec Time cp   (s)  : $TEMP_EXEC_TIME_CP"
-logIt "Info: Exec Time sync (s)  : $TEMP_EXEC_TIME_SYNC"
+
+if [ "$LOCAL_CP_SYNC" != "nosync" ]; then
+	logIt "Info: Exec Time sync (s)  : $TEMP_EXEC_TIME_SYNC"
+fi
+
 logIt "Info: Throughput (MB/s)   : $THROUGHPUT_MBps"
 logIt "Info: Throughput (Mbps)   : $THROUGHPUT_Mbps"
 
-cat $HCCP_CP_LOG.$LOCAL_INSTANCE >> $HS_STATISTICS_GENERAL_FILE
+cat $HCCP_CP_LOG.$LOCAL_CP_INSTANCE >> $HS_STATISTICS_GENERAL_FILE
 
 # End of file
+
