dd45292d6857f8c2ea10c3d5907a0b593ebe059f
Abraham Arce <x0066660@ti.com>
2010-04-27 01:46:23 -0500

    TestSuites: Utils: Arguments in Dd Command +
    - Add a 4th argument which is a "calculation" flag
    
       1. If argument equals "to" || "from" then give a path to throughput
          handler so throughput can be calculated with real file size
       2. If argument equals "none" then give the file size to throuhput
          handler so throughput can be calculated
    
    - Add a 6th argument which is a "nosync" operation
    
       1. If not argument is passed, then a synchronous dd operation
          will be executed
       2. If "nosync" argument is passed, then a asynchronous dd operation
          will be executed
    
    - Dd will be run through handlerAppTime.sh
    
    - Indentation changes
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

--- utils/handlers/handlerCmdDd.sh
+++ utils/handlers/handlerCmdDd.sh
@@ -4,60 +4,81 @@
 # Variables
 # =============================================================================
 
-LOCAL_INSTANCE=$1
-LOCAL_OF=$2
-LOCAL_SIZE=$3
-
-LOCAL_BS="1M"
-LOCAL_IF=/dev/urandom
+LOCAL_DD_INSTANCE=$1
+LOCAL_DD_IF=$2
+LOCAL_DD_OF=$3
+LOCAL_DD_SIZE_CALC=$4
+LOCAL_DD_SIZE=$5
+LOCAL_DD_SYNC=$6
 
 # =============================================================================
 # Functions
 # =============================================================================
 
 logIt() {
-  echo -e "$1"
-  echo -e "$1" >> $HCDD_DD_LOG.$LOCAL_INSTANCE
+	echo -e "$1"
+	echo -e "$1" >> $HCDD_DD_LOG.$LOCAL_DD_INSTANCE
 }
 
 # =============================================================================
 # Main
 # =============================================================================
 
-echo > $HCDD_DD_LOG.$LOCAL_INSTANCE
+echo > $HCDD_DD_LOG.$LOCAL_DD_INSTANCE
 
-$UTILS_DIR_HANDLERS/handlerAppTop2.sh $LOCAL_INSTANCE "start"
-$UTILS_DIR_BIN/time -p -o $HCDD_EXECUTION_TIME_DD.$LOCAL_INSTANCE dd if=${LOCAL_IF} of=${LOCAL_OF} bs=${LOCAL_BS} count=${LOCAL_SIZE}
-$UTILS_DIR_BIN/time -p -o $HCDD_EXECUTION_TIME_SYNC.$LOCAL_INSTANCE sync
-$UTILS_DIR_HANDLERS/handlerAppTop2.sh $LOCAL_INSTANCE "stop"
-$UTILS_DIR_HANDLERS/handlerAppTop2.sh $LOCAL_INSTANCE "parse" "User"
-$UTILS_DIR_HANDLERS/handlerAppTop2.sh $LOCAL_INSTANCE "parse" "System"
+handlerAppTop2.sh "start" $LOCAL_DD_INSTANCE
+
+handlerAppTime.sh "start" $LOCAL_DD_INSTANCE "dd" "dd if=${LOCAL_DD_IF} of=${LOCAL_DD_OF} bs=${HCDD_DD_BS} count=${LOCAL_DD_SIZE}"
+
+if [ "$LOCAL_DD_SYNC" != "nosync" ]; then
+	handlerAppTime.sh "start" $LOCAL_DD_INSTANCE "sync" "sync"
+fi
+
+handlerAppTop2.sh "stop"  $LOCAL_DD_INSTANCE
+handlerAppTop2.sh "parse" $LOCAL_DD_INSTANCE "User"
+handlerAppTop2.sh "parse" $LOCAL_DD_INSTANCE "System"
+handlerAppTime.sh "parse" $LOCAL_DD_INSTANCE "dd"
+
+if [ "$LOCAL_DD_SYNC" != "nosync" ]; then
+	handlerAppTime.sh "parse" $LOCAL_DD_INSTANCE "sync"
+fi
 
 echo 3 > /proc/sys/vm/drop_caches
 
-#cat $HAT_CPU_USAGE_VALUE_AVR.$LOCAL_INSTANCE
-#cat $HAT_CPU_USAGE_VALUE_MAX.$LOCAL_INSTANCE
-#cat $HAT_CPU_USAGE_VALUE_MIN.$LOCAL_INSTANCE
+TEMP_EXEC_TIME_DD=`cat $HATI_TIME_FINAL.$LOCAL_DD_INSTANCE.dd`
 
-LOCAL_FILE_SIZE=`ls -al ${LOCAL_OF} | awk '{print $5}'`
-LOCAL_FILE_SIZE=`echo "(${LOCAL_FILE_SIZE}/1024)/1024" | bc`
+if [ "$LOCAL_DD_SYNC" != "nosync" ]; then
+	TEMP_EXEC_TIME_SYNC=`cat $HATI_TIME_FINAL.$LOCAL_DD_INSTANCE.sync`
+fi
 
-TEMP_HCDD_EXEC_TIME_DD=`cat $HCDD_EXECUTION_TIME_DD.$LOCAL_INSTANCE | grep real | sed -e 's/[ a-z]//g'`
-TEMP_HCDD_EXEC_TIME_SYNC=`cat $HCDD_EXECUTION_TIME_SYNC.$LOCAL_INSTANCE | grep real | sed -e 's/[ a-z]//g'`
-$UTILS_DIR_HANDLERS/handlerFileThroughput.sh none $LOCAL_INSTANCE $LOCAL_FILE_SIZE $TEMP_HCDD_EXEC_TIME_DD $TEMP_HCDD_EXEC_TIME_SYNC
-THROUGHPUT_MBps=`cat $HFT_THROUGHPUT_MBps.$LOCAL_INSTANCE`
-THROUGHPUT_Mbps=`cat $HFT_THROUGHPUT_Mbps.$LOCAL_INSTANCE`
+if [ "$LOCAL_DD_SIZE_CALC" == "to" ]; then
+	LOCAL_FILE_PATH=$LOCAL_DD_OF
+elif [ "$LOCAL_DD_SIZE_CALC" == "from" ]; then
+	LOCAL_FILE_PATH=$LOCAL_DD_IF
+elif [ "$LOCAL_DD_SIZE_CALC" == "none" ]; then
+	LOCAL_FILE_PATH=$LOCAL_DD_SIZE
+fi
 
-logIt "\nInfo: dd if=${LOCAL_IF} of=${LOCAL_OF} bs=${LOCAL_BS} count=${LOCAL_SIZE}\n"
-logIt "Info: Real File Size (MB) : $LOCAL_FILE_SIZE"
-logIt "Info: Exec Time dd   (s)  : $TEMP_HCDD_EXEC_TIME_DD"
-logIt "Info: Exec Time sync (s)  : $TEMP_HCDD_EXEC_TIME_SYNC"
+$UTILS_DIR_HANDLERS/handlerFileThroughput.sh none $LOCAL_DD_INSTANCE $LOCAL_FILE_PATH $TEMP_EXEC_TIME_DD $TEMP_EXEC_TIME_SYNC
+
+THROUGHPUT_SIZE=`cat $HFT_THROUGHPUT_REAL_FILE_SIZE.$LOCAL_DD_INSTANCE`
+THROUGHPUT_MBps=`cat $HFT_THROUGHPUT_MBps.$LOCAL_DD_INSTANCE`
+THROUGHPUT_Mbps=`cat $HFT_THROUGHPUT_Mbps.$LOCAL_DD_INSTANCE`
+
+logIt "Info: dd if=${LOCAL_DD_IF} of=${LOCAL_DD_OF} bs=${HCDD_DD_BS} count=${LOCAL_DD_SIZE}\n"
+logIt "Info: Real File Size (MB) : $THROUGHPUT_SIZE"
+logIt "Info: Exec Time dd   (s)  : $TEMP_EXEC_TIME_DD"
+
+if [ "$LOCAL_DD_SYNC" != "nosync" ]; then
+	logIt "Info: Exec Time sync (s)  : $TEMP_EXEC_TIME_SYNC"
+fi
+
 logIt "Info: Throughput (MB/s)   : $THROUGHPUT_MBps"
 logIt "Info: Throughput (Mbps)   : $THROUGHPUT_Mbps"
 
-cat $HCDD_DD_LOG.$LOCAL_INSTANCE >> $HS_STATISTICS_GENERAL_FILE
-cat $HAT_TOP2_DATA_LOG.$LOCAL_INSTANCE >> $HS_STATISTICS_GENERAL_FILE
-
+cat $HCDD_DD_LOG.$LOCAL_DD_INSTANCE >> $HS_STATISTICS_GENERAL_FILE
+cat $HAT_TOP2_DATA_LOG.$LOCAL_DD_INSTANCE >> $HS_STATISTICS_GENERAL_FILE
 echo -e "\n---------------------------------------------------------------------\n" >>  $HS_STATISTICS_GENERAL_FILE 
 
 # End of file
+
