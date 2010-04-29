e64b82bff97f548f6479f26ba9715d72d9bcc4fa
Abraham Arce <x0066660@ti.com>
2010-04-28 23:36:06 -0500

    TestSuites: Utils: Affinity in GPIO IRQs
    The affinity for GPIO IRQs shall be modified using the interrupt
    line of the GPIO bank.
    
    No change to the previous interface, irq number shall be the one
    reported by /proc/interrupts
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

--- utils/handlers/handlerIrqAffinity.sh
+++ utils/handlers/handlerIrqAffinity.sh
@@ -10,20 +10,53 @@ LOCAL_EXECUTION_TIMES=$3
 LOCAL_TIME_TO_WAIT=$4
 LOCAL_IRQ_NUMBER=$5
 
+export LOCAL_IRQ_GPIO=0
+export LOCAL_PROC_IRQ_NUMBER=0
+
 # =============================================================================
 # Functions
 # =============================================================================
 
-# None
+getIrqBank() {
+
+	LOCAL_VIRTUAL_IRQ=$1
+	LOCAL_GPIO_LINE=`echo "$LOCAL_VIRTUAL_IRQ-160" | bc`
+
+	if [ "$LOCAL_GPIO_LINE" -ge 0 ] && [ "$LOCAL_GPIO_LINE" -le 31 ]; then
+		export LOCAL_IRQ_GPIO=$INT_GPIO_BANK1
+	elif [ "$LOCAL_GPIO_LINE" -ge 32 ] && [ "$LOCAL_GPIO_LINE" -le 63 ]; then
+		export LOCAL_IRQ_GPIO=$INT_GPIO_BANK2
+	elif [ "$LOCAL_GPIO_LINE" -ge 64 ] && [ "$LOCAL_GPIO_LINE" -le 95 ]]; then
+		export LOCAL_IRQ_GPIO=$INT_GPIO_BANK3
+	elif [ "$LOCAL_GPIO_LINE" -ge 96 ] && [ "$LOCAL_GPIO_LINE" -le 127 ]; then
+		export LOCAL_IRQ_GPIO=$INT_GPIO_BANK4
+	elif [ "$LOCAL_GPIO_LINE" -ge 128 ] && [ "$LOCAL_GPIO_LINE" -le 159 ]; then
+		export LOCAL_IRQ_GPIO=$INT_GPIO_BANK5
+	elif [ "$LOCAL_GPIO_LINE" -ge 160 ] && [ "$LOCAL_GPIO_LINE" -le 191 ]; then
+		export LOCAL_IRQ_GPIO=$INT_GPIO_BANK6
+	fi
+
+}
 
 # =============================================================================
 # Main
 # =============================================================================
 
-echo -e "\nInfo: Command > $LOCAL_COMMAND_LINE\n"
+# Make sure GPIO irqs are handled in a different way
+# We need to change the affinity for the irq of the GPIO bank it belongs to
 
-test -f /proc/irq/$LOCAL_IRQ_NUMBER/smp_affinity
+if [ "$LOCAL_IRQ_NUMBER" -gt "160" ]; then
 
+	getIrqBank $LOCAL_IRQ_NUMBER
+	LOCAL_PROC_IRQ_NUMBER=$LOCAL_IRQ_GPIO
+
+else
+
+	LOCAL_PROC_IRQ_NUMBER=$LOCAL_IRQ_NUMBER
+
+fi
+
+test -f /proc/irq/$LOCAL_PROC_IRQ_NUMBER/smp_affinity
 if [ $? -eq 1 ]
 then
 	echo "Error: Cannot set affinity for irq $LOCAL_IRQ_NUMBER!"
@@ -58,11 +91,15 @@ if [ "$LOCAL_OPERATION" = "switch" ]; then
 
 			initial_value_p1=`cat /proc/interrupts | grep $LOCAL_IRQ_NUMBER | awk '{print $2}'`
 			initial_value_p2=`cat /proc/interrupts | grep $LOCAL_IRQ_NUMBER | awk '{print $3}'`
-			echo 1 > /proc/irq/$LOCAL_IRQ_NUMBER/smp_affinity
+
+			echo 1 > /proc/irq/$LOCAL_PROC_IRQ_NUMBER/smp_affinity
 			sleep $LOCAL_TIME_TO_WAIT
+
 			final_value_p1=`cat /proc/interrupts | grep $LOCAL_IRQ_NUMBER | awk '{print $2}'`
 			final_value_p2=`cat /proc/interrupts | grep $LOCAL_IRQ_NUMBER | awk '{print $3}'`
-			echo $initial_value_p1 $final_value_p1 $initial_value_p2 $final_value_p2
+
+			echo "Values IP1 FP1 IP2 FP2: $initial_value_p1 $final_value_p1 $initial_value_p2 $final_value_p2"
+
 			if [ "$initial_value_p1" -lt "$final_value_p1" ]
 			then
 				continue
@@ -75,11 +112,15 @@ if [ "$LOCAL_OPERATION" = "switch" ]; then
 
 			initial_value_p1=`cat /proc/interrupts | grep $LOCAL_IRQ_NUMBER | awk '{print $2}'`
 			initial_value_p2=`cat /proc/interrupts | grep $LOCAL_IRQ_NUMBER | awk '{print $3}'`
-			echo 2 > /proc/irq/$LOCAL_IRQ_NUMBER/smp_affinity
+
+			echo 2 > /proc/irq/$LOCAL_PROC_IRQ_NUMBER/smp_affinity
 			sleep $LOCAL_TIME_TO_WAIT
+
 			final_value_p1=`cat /proc/interrupts | grep $LOCAL_IRQ_NUMBER | awk '{print $2}'`
 			final_value_p2=`cat /proc/interrupts | grep $LOCAL_IRQ_NUMBER | awk '{print $3}'`
-			echo $initial_value_p1 $final_value_p1 $initial_value_p2 $final_value_p2
+
+			echo "Values IP1 FP1 IP2 FP2: $initial_value_p1 $final_value_p1 $initial_value_p2 $final_value_p2"
+
 			if [ "$initial_value_p2" -lt "$final_value_p2" ]
 			then
 				continue
