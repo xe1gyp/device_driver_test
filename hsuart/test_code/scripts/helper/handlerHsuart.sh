73db9bb5cc52a34964e72166703a95922b9e449b
Govindraj.R <govindraj.raja@ti.com>
2010-05-05 19:19:21 +0530

    HSUART:Add new scenario Ids and modify existing ones.
    Adding new scenario Ids and modifying few scenario ID.
    Changes Involve:
    1.) Adding handlerHsuart.sh which will run an rx in background
    	and tx at foreground when we have setup an Loopback.
    2.) Adding 115Kbps, 921Kbps, 3.5Mbps baudrate for testing
    	with combination of no flowcontrol, H/W flowcontrol,
    	S/W flowcontrol.
    3.) Use test cases based on loopback for all usecases.
    
    Signed-off-by: Govindraj.R <govindraj.raja@ti.com>

---
+++ hsuart/test_code/scripts/helper/handlerHsuart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##############################################################################
+# Author      : Govindraj.R
+# Date        : Apr 30 2010
+# Description : Used to run rx process in background and then tx.
+#		other details needed will be passed from Scenario ID.
+##############################################################################
+
+#ts_uart r $TTY_INTERFACE $BAUDRATE $FLOWCONTROL &
+#ts_uart s $TTY_INTERFACE $BAUDRATE $FLOWCONTROL
+$HSUART_DIR_BINARIES/ts_uart r $1 $2 $3 &
+$HSUART_DIR_BINARIES/ts_uart s $1 $2 $3
+exit 0;
