437179b69e20f46980ae4d0913452003a9fb15ef
Shilpa Maddi <s-maddi@ti.com>
2010-05-10 14:29:55 -0500

    DMA configuration changes
    Signed-off-by: Shilpa Maddi <s-maddi@ti.com>

--- dma/test_code/scripts/conf.sh
+++ dma/test_code/scripts/conf.sh
@@ -16,6 +16,11 @@ export VERBOSE=""
 export OUTPUTFILE=${TESTROOT}/output.$POSTFIX
 export LOGFILE=${TESTROOT}/log.$POSTFIX
 export DURATION=""
-export PATH="${PATH}:${TESTROOT}:${TESTBIN}:${TESTSCRIPT}"
+export UTILS_DIR_BIN=${TESTROOT}/../../utils/bin
+export UTILS_DIR_HANDLERS=${TESTROOT}/../../utils/handlers
+export UTILS_DIR_SCRIPTS=${TESTROOT}/../../utils/scripts
+. ${TESTROOT}/../../utils/configuration/general.configuration
+export PATH="${PATH}:${TESTROOT}:${TESTBIN}:${TESTSCRIPT}:${UTILS_DIR_BIN}:${UTILS_DIR_HANDLERS}:${UTILS_DIR_SCRIPTS}"
 export TC_SCENARIO="${TESTROOT}/scenarios"
 export SCENARIO_NAMES=""
+
