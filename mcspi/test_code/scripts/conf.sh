04df71f442287876706e3a1571141fecf945435a
David Miramontes <david.miramontes@ti.com>
2010-06-02 09:39:52 -0500

    Testsuites: McSPI: Adding test scenarios
    This patch adds the scenarios to test McSPI module
    
    Signed-off-by: David Miramontes <david.miramontes@ti.com>

--- mcspi/test_code/scripts/conf.sh
+++ mcspi/test_code/scripts/conf.sh
@@ -10,6 +10,8 @@ export TMPBASE=${TESTROOT}/tmp
 export TMPFILE=${TMPBASE}/tmp.$POSTFIX
 export CMDFILE=cmd.$POSTFIX
 export TESTDIR=${TESTROOT}/test
+export PROC_DRIVER_DIR=/proc/driver
+export MCSPI_PROC_DIR=${PROC_DRIVER_DIR}/mcspi_test
 export PRETTY_PRT=""
 export VERBOSE=""
 export OUTPUTFILE=${TESTROOT}/output.$POSTFIX
@@ -19,6 +21,15 @@ export PATH="${TESTROOT}:${TESTBIN}:${TESTSCRIPT}:${PATH}"
 export TC_SCENARIO="${TESTROOT}/scenarios"
 export SCENARIO_NAMES=""
 export STRESS=""
+export DEBUG_DIR=/debug/
+export POWERSYSFS=/sys/power
+if [ -f "/sys/power/wakeup_timer_seconds" ] #is this power entry in sysfs?
+then #if yes, we can look in /sys/power for all entries
+       export POWER_ENTRIES=$POWERSYSFS
+else #if not, they must be in debugfs and it should be mounted
+       if [ ! -d $DEBUG_DIR ]; then mkdir $DEBUG_DIR; mount -t debugfs debugfs /debug; fi;
+       export POWER_ENTRIES=$DEBUG_DIR/pm_debug/
+fi
 
 # Utilities
 export UTILBIN=${TESTROOT}/../../utils/bin
