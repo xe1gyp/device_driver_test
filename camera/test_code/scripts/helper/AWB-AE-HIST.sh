652720b92d3868ba8c92700cd6e765942a95805f
Ivaylo Petrov <ivpetrov@mm-sol.com>
2010-07-27 14:05:01 +0300

    Adding scenario for testing i421 errata AF statistics
    Signed-off-by: Ivaylo Petrov <ivpetrov@mm-sol.com>
    Signed-off-by: Akwasi Boateng <akwasi.boateng@ti.com>

--- camera/test_code/scripts/helper/AWB-AE-HIST.sh
+++ camera/test_code/scripts/helper/AWB-AE-HIST.sh
@@ -25,11 +25,19 @@ elif [ $TEST = "AE" ]; then
   $TESTBIN/h3a_stream $DEVICE 30 1 $PARAMETER
   RESULT=$?
   echo "Test returned $RESULT"
-elif [ $TEST="HIST" ]; then
+elif [ $TEST = "HIST" ]; then
   echo "";echo "Run histogram test progam './hist_test'"
   echo "";echo "Check if the application run without problems"
   sleep 5
   $TESTBIN/hist_test $DEVICE
+  RESULT=$?
+  echo "Test returned $RESULT"
+elif [ $TEST = "I421_ERRATA" ]; then
+  echo "";echo "The test application waits for AF and H3A statistics.";
+  echo "It should be in compliance with errata i421."
+  echo "The AF and H3A should be displayed without problems."
+  sleep 6
+  $TESTBIN/i421_errata_af_stats
   RESULT=$?
   echo "Test returned $RESULT"
 fi
