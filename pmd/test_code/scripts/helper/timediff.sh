4b534a0bebe14fa90ce9e5b8ebcf6a6a77733541
Teerth Reddy <teerth@ti.com>
2010-07-28 09:05:14 -0500

    Adding PM test scenario for suspend/resume stress test
    L_DD_POWER_0004_0002: Adding PM test scenario for suspend/resume and checking sys_time during suspend.
    L_DD_POWER_0004_0003: Test scenario for stress testing sus/resume for 1000 times.
    
    Signed-off-by: Teerth Reddy <teerth@ti.com>
    Signed-off-by: Lesly AM <leslyam@ti.com>

---
+++ pmd/test_code/scripts/helper/timediff.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ $# -ne 3 ]
+then
+        echo " "
+        echo "Usage: timediff.sh <time before suspend> <time after wakeup> <wake
+        echo " "
+        exit 1
+fi
+
+tdiff=`expr $2 - $1`
+
+delta=`expr $tdiff - $3`
+
+if [ $delta -lt  5 ]
+then
+        echo "TEST PASSED"
+else
+        echo "TEST FAILED"
+fi
