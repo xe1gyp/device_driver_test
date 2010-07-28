1c8311ae571814b7c5077a8bdace9b96bb106c56
teerth <teerth@ti.com>
2010-07-28 09:04:59 -0500

    Adding PM test scenario for suspend/resume and check sys time
    Adding PM test scenario for suspend/resume and checking sys_time during suspend.
    
    Signed-off-by: Teerth Reddy <teerth@ti.com>
    Signed-off-by: Lesly AM <leslyam@ti.com>

--- pmd/test_code/scripts/helper/suspendResume.sh
+++ pmd/test_code/scripts/helper/suspendResume.sh
@@ -6,17 +6,19 @@ LOCAL_WAKEUP_TIME=$2
 
 if [ "$LOCAL_COMMAND" = "suspend" ]; then
 
-  echo > $KERNEL_MESSAGES
   echo $LOCAL_WAKEUP_TIME > $WAKEUP_TIMER_SECONDS
   echo $MEM > $STATE
   echo $DISABLE > $WAKEUP_TIMER_SECONDS
   sleep 1
-  cat $KERNEL_MESSAGES | grep "Successfully"
+  $KERNEL_MESSAGES | grep "Successfully"
   
-  if [ $? != 0 ]; then
-    echo "Error: All powerdomains did not enter target state"
-    cat $KERNEL_MESSAGES
-    exit 1
+  if [ $? == 0 ]; then
+	echo "TEST PASSED"
+	exit 0
+  else
+	echo "Error: All powerdomains did not enter target state"
+	cat $KERNEL_MESSAGES
+	exit 1
   fi
   
 else
