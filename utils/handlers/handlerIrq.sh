a57ee9aae66b395988a361f78c93875ec500242b
Abraham Arce <x0066660@ti.com>
2010-09-03 15:41:52 -0500

    TestSuites: Utils: Irq handler, new operations
    Add another argument to be processed to allow specific operations
    when working with /proc/interrupts, operations can be:
    
    - handlerIrq.sh "get" "irq" <driver> <file>
      @operation  operation to perform, get
      @option     option to process, irq number
      @driver     driver name used to find its irq number
      @file       file name where to save irq number
    
    - handlerIrq.sh "get" "cpu0" <irq/file>
      @operation  operation to perform, get
      @option     option to process, number of irqs in cpu0
      @irq/file   irq number to get interrupts from, can be also created file
    
    - handlerIrq.sh "get" "cpu1" <irq/file>
      @operation  operation to perform, get
      @option     option to process, number of irqs in cpu1
      @irq/file   irq number to get interrupts from, can be also created file
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

--- utils/handlers/handlerIrq.sh
+++ utils/handlers/handlerIrq.sh
@@ -5,8 +5,7 @@
 # =============================================================================
 
 LOCAL_OPERATION=$1
-LOCAL_DRIVER=$2
-LOCAL_FILE=$3
+LOCAL_OPTION=$2
 
 # =============================================================================
 # Functions
@@ -19,18 +18,55 @@ LOCAL_FILE=$3
 # =============================================================================
 
 if [ "$LOCAL_OPERATION" = "get" ]; then
-	if [ "$LOCAL_DRIVER" = "" ]; then
-		echo "Error: driver value was not provided"
+
+	if [ "$LOCAL_OPTION" = "irq" ]; then
+
+		LOCAL_DRIVER=$3
+		LOCAL_FILE=$4
+
+		if [ "$LOCAL_DRIVER" = "" ]; then
+			echo "Error: driver value was not provided"
+			exit 1
+		fi
+
+		for i in `cat /proc/interrupts | grep -i $LOCAL_DRIVER | awk '{print$1}' | cut -d: -f1`
+		do
+			echo $i > $LOCAL_FILE
+			echo $i
+		done
+
+	elif [ "$LOCAL_OPTION" = "cpu0" ] || [ "$LOCAL_OPTION" = "cpu1" ]; then
+
+    LOCAL_IRQ_NUMBER=$3
+    test -f  $LOCAL_IRQ_NUMBER
+    if [ $? == 0 ]; then
+    	LOCAL_IRQ_NUMBER=`cat $LOCAL_IRQ_NUMBER`
+    fi
+
+    cat /proc/interrupts | grep -i $LOCAL_OPTION &> /dev/null
+		if [ $? != 0 ]; then
+			echo "FATAL: $LOCAL_OPTION irq information not available, please check!"
+			cat /proc/interrupts
+		fi
+		
+		if [ "$LOCAL_OPTION" = "cpu0" ]; then
+			cat /proc/interrupts | grep $LOCAL_IRQ_NUMBER: | awk '{print $2}'
+		elif [ "$LOCAL_OPTION" = "cpu1" ]; then
+			cat /proc/interrupts | grep $LOCAL_IRQ_NUMBER: | awk '{print $3}'
+		fi
+
+	else
+
+		echo "Fatal: Unsupported option"
 		exit 1
+
 	fi
 
-	for i in `cat /proc/interrupts | grep -i $LOCAL_DRIVER | awk '{print$1}' | cut -d: -f1`
-	do
-		 echo $i > $LOCAL_FILE
-	done
 else
+
 	echo "Fatal: Unsupported operation"
 	exit 1
+
 fi
 
 # End of file
