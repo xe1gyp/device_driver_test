73567ad5ca501150e8be9d47827c70839a5e06ed
Abraham Arce <x0066660@ti.com>
2010-09-17 16:50:30 -0500

    TestSuites: Utils: Correct suspend resume handler
    Current implementation add "Successfully" string to /var/log/messages
    so a failure will never be seen. Now use remove any message present in
    previous file and then after resume and grep for
    
    $HSR_SUSPEND_RESUME_MESSAGE_SUCCESS
    
    Other changes
    
    - Mount/umount debug filesystem using handlerDebugFileSystem.sh helper
    - Format changes
    - Correct handling of LOCAL_COMMAND_LINE
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

--- utils/handlers/handlerSuspendResume.sh
+++ utils/handlers/handlerSuspendResume.sh
@@ -17,31 +17,32 @@ suspendResume() {
 	LOCAL_COMMAND_LINE=$2
 	LOCAL_ERROR=0
 
-	if [ -n $LOCAL_COMMAND_LINE ]; then
+	if [ -n "$LOCAL_COMMAND_LINE" ]; then
 		$LOCAL_COMMAND_LINE &
 		LOCAL_COMMAND_PID=`echo $!`
 	fi
 
 	echo $LOCAL_WAKEUP_TIME > $PM_WAKEUP_TIMER_SECONDS
-	echo $LOCAL_WAKEUP_TIME > $PM_WAKEUP_TIMER_SECONDS_VARIABLE
 
 	while [ 1 ]; do
 
-		echo "Successfully" > /var/log/messages
+		echo > /var/log/messages
+		sleep 1
 		echo mem > /sys/power/state
 
-		cat /var/log/messages | grep "Successfully"
+		cat /var/log/messages | grep "$HSR_SUSPEND_RESUME_MESSAGE_SUCCESS"
 		if [ $? != 0 ]; then
 			echo "Error: all powerdomains did not enter target state"
 			cat /var/log/messages
 			LOCAL_ERROR=1
 		fi
 
-		if [ -n $LOCAL_COMMAND_LINE ]; then
-			test -d $LOCAL_COMMAND_PID || break
+		if [ -n "$LOCAL_COMMAND_LINE" ]; then
+			test -d /proc/$LOCAL_COMMAND_PID || break
 		else
 			break
 		fi
+
 	done
 
 	wait
@@ -58,10 +59,6 @@ if [ $? -eq 1 ]; then
 fi
 
 if [ ! -f "$PM_WAKEUP_TIMER_SECONDS" ]; then
-
-	PM_WAKEUP_TIMER_SECONDS=$PM_WAKEUP_TIMER_SECONDS_VARIABLE
-
-elif [ ! -f "$PM_WAKEUP_TIMER_SECONDS" ]; then
 	echo "FATAL: $PM_WAKEUP_TIMER_SECONDS cannot be found!"
 	handlerError.sh "log" "1" "halt" "handlerSuspendResume"
 	exit 1
@@ -73,11 +70,15 @@ if [ ! -f /sys/power/state ]; then
 	exit 1
 fi
 
+handlerDebugFileSystem.sh "mount"
+
 if [ "$LOCAL_OPERATION" = "suspend" ]; then
 	suspendResume $LOCAL_WAKEUP_TIME
 elif [ "$LOCAL_OPERATION" = "run" ]; then
 	LOCAL_COMMAND_LINE=$3
-	suspendResume $LOCAL_WAKEUP_TIME $LOCAL_COMMAND_LINE
+	suspendResume $LOCAL_WAKEUP_TIME "$LOCAL_COMMAND_LINE"
 fi
+
+handlerDebugFileSystem.sh "umount"
 
 # End of file
