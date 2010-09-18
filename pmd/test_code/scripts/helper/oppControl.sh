d298bde47a0015ef1796c1527b4d8e7e0dff6d65
Abraham Arce <x0066660@ti.com>
2010-09-18 04:47:31 -0500

    TestSuites: pmd: add common variables
    Instead of using local variables, make use of the common ones
    found under configuration directory of general utils
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

--- pmd/test_code/scripts/helper/oppControl.sh
+++ pmd/test_code/scripts/helper/oppControl.sh
@@ -1,14 +1,52 @@
 #!/bin/sh
-set -x
+
+
+# =============================================================================
+# Variables
+# =============================================================================
 
 LOCAL_COMMAND=$1
 LOCAL_VDD=$2
 LOCAL_OPP=$3
 
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+handlerError.sh "test"
+if [ $? -eq 1 ]; then
+	exit 1
+fi
+
+if [ ! -f $SYSFS_CPU0_CURRENT_FREQUENCY ]; then
+	echo "FATAL: $SYSFS_CPU0_CURRENT_FREQUENCY cannot be found!"
+	handlerError.sh "log" "1" "halt" "oppControl.sh"
+	exit 1
+fi
+
+if [ ! -f $SYSFS_CPU0_SET_SPEED ]; then
+	echo "FATAL: $SYSFS_CPU0_SET_SPEED cannot be found!"
+	handlerError.sh "log" "1" "halt" "oppControl.sh"
+	exit 1
+fi
+
+
+if [ ! -f $DSP_FREQ ]; then
+	echo "FATAL: $DSP_FREQ cannot be found!"
+	handlerError.sh "log" "1" "halt" "oppControl.sh"
+	exit 1
+fi
+
 if [ "$LOCAL_COMMAND" = "show" ]; then
 
   if [ "$LOCAL_VDD" = "VDD1" ]; then
-    cat $SCALING_CUR_FREQ
+    cat $SYSFS_CPU0_CURRENT_FREQUENCY
   elif [ "$LOCAL_VDD" = "VDD2" ]; then
     cat $DSP_FREQ
   fi
@@ -16,23 +54,23 @@ if [ "$LOCAL_COMMAND" = "show" ]; then
 elif [ "$LOCAL_COMMAND" = "request" ]; then
 
   if [ "$LOCAL_VDD" = "VDD1" ]; then
-    echo "$LOCAL_OPP > $SCALING_SET_SPEED"
-    echo $LOCAL_OPP > $SCALING_SET_SPEED
-    temp_value=`cat $SCALING_CUR_FREQ`
+    echo "$LOCAL_OPP > $SYSFS_CPU0_SET_SPEED"
+    echo $LOCAL_OPP > $SYSFS_CPU0_SET_SPEED
+    temp_value=`cat $SYSFS_CPU0_CURRENT_FREQUENCY`
   elif [ "$LOCAL_VDD" = "VDD2" ]; then
 	echo "Setting corresponding mpu frequency"
   	if [ "$LOCAL_OPP" = "260000000" ]; then
-		echo "$VDD1_OPP1_FREQ > $SCALING_SET_SPEED"
-		echo $VDD1_OPP1_FREQ > $SCALING_SET_SPEED
+		echo "$VDD1_OPP1_FREQ > $SYSFS_CPU0_SET_SPEED"
+		echo $VDD1_OPP1_FREQ > $SYSFS_CPU0_SET_SPEED
 	elif [ "$LOCAL_OPP" = "520000000" ]; then
-		echo "$VDD1_OPP2_FREQ > $SCALING_SET_SPEED"
-		echo $VDD1_OPP2_FREQ > $SCALING_SET_SPEED
+		echo "$VDD1_OPP2_FREQ > $SYSFS_CPU0_SET_SPEED"
+		echo $VDD1_OPP2_FREQ > $SYSFS_CPU0_SET_SPEED
 	elif [ "$LOCAL_OPP" = "660000000" ]; then
-		echo "$VDD1_OPP3_FREQ > $SCALING_SET_SPEED"
-		echo $VDD1_OPP3_FREQ > $SCALING_SET_SPEED
+		echo "$VDD1_OPP3_FREQ > $SYSFS_CPU0_SET_SPEED"
+		echo $VDD1_OPP3_FREQ > $SYSFS_CPU0_SET_SPEED
 	elif [ "$LOCAL_OPP" = "800000000" ]; then
-		echo "$VDD1_OPP4_FREQ > $SCALING_SET_SPEED"
-		echo $VDD1_OPP4_FREQ > $SCALING_SET_SPEED
+		echo "$VDD1_OPP4_FREQ > $SYSFS_CPU0_SET_SPEED"
+		echo $VDD1_OPP4_FREQ > $SYSFS_CPU0_SET_SPEED
 	fi
 	temp_value=`cat $DSP_FREQ`
   fi
@@ -55,19 +93,19 @@ elif [ "$LOCAL_COMMAND" = "request" ]; then
        vdd1_opp_no=`expr $vdd1_opp_no + 1`
        if [ "$vdd1_opp_no" = "1" ]; then
                LOCAL_OPP=300000
-               echo $LOCAL_OPP > $SCALING_SET_SPEED
+               echo $LOCAL_OPP > $SYSFS_CPU0_SET_SPEED
        elif [ "$vdd1_opp_no"  = "2" ]; then
                LOCAL_OPP=600000
-               echo $LOCAL_OPP > $SCALING_SET_SPEED
+               echo $LOCAL_OPP > $SYSFS_CPU0_SET_SPEED
        elif [ "$vdd1_opp_no" = "3" ]; then
                LOCAL_OPP=800000
-               echo $LOCAL_OPP > $SCALING_SET_SPEED
+               echo $LOCAL_OPP > $SYSFS_CPU0_SET_SPEED
        elif [ "$vdd1_opp_no" = "4" ]; then
                LOCAL_OPP=1000000
-               echo $LOCAL_OPP > $SCALING_SET_SPEED
+               echo $LOCAL_OPP > $SYSFS_CPU0_SET_SPEED
        fi
 
-       temp_value=`cat $SCALING_CUR_FREQ`
+       temp_value=`cat $SYSFS_CPU0_CURRENT_FREQUENCY`
 
        if [ "$LOCAL_OPP" = "$temp_value" ]; then
                echo "OPP value was correctly set in $LOCAL_VDD"
