47d2e313f077b3ce36356daca0f701ee96fc5e5a
Abraham Arce <x0066660@ti.com>
2010-09-18 02:22:08 -0500

    TestSuites: Utils: set op to debugfs handler
    Instead of executing direct modification of a debugfs entry, use
    "add" command, this will allow us to handle error properly.
    
    + other minor changes
    
    - Grep for "debugfs on $HDFS_DEBUG_FILESYSTEM_DIRECTORY"
      when coming from suspend
    - Add handlerError.sh check
    - Add script template structure
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

--- utils/handlers/handlerDebugFileSystem.sh
+++ utils/handlers/handlerDebugFileSystem.sh
@@ -1,17 +1,52 @@
 #!/bin/sh
 
+# =============================================================================
+# Variables
+# =============================================================================
+
 LOCAL_COMMAND=$1
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+handlerError.sh "test"
+if [ $? -eq 1 ]; then
+	exit 1
+fi
 
 if [ "$LOCAL_COMMAND" = "mount" ]; then
 
 	test -d $HDFS_DEBUG_FILESYSTEM_DIRECTORY || mkdir -p $HDFS_DEBUG_FILESYSTEM_DIRECTORY
 
-	mount | grep debugfs
+	mount | grep debugfs | grep "debugfs on $HDFS_DEBUG_FILESYSTEM_DIRECTORY"
 
 	if [ $? != 0 ]; then
 		mount -t debugfs debugfs $HDFS_DEBUG_FILESYSTEM_DIRECTORY
 	fi
 
+elif [ "$LOCAL_COMMAND" = "set" ]; then
+
+	LOCAL_DEBUGFS_ENTRY_VALUE=$2
+	LOCAL_DEBUGFS_ENTRY_NAME=$3
+
+	handlerDebugFileSystem.sh "mount"
+
+	test -f $LOCAL_DEBUGFS_ENTRY_NAME
+	if [ $? != 0 ]; then
+		handlerError.sh "log" "1" "halt" "handlerSysFs.sh"
+		exit 1
+	fi
+
+	echo $LOCAL_DEBUGFS_ENTRY_VALUE > $LOCAL_DEBUGFS_ENTRY_NAME
+
+	handlerDebugFileSystem.sh "umount"
 
 elif [ "$LOCAL_COMMAND" = "umount" ]; then
 
