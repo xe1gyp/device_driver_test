8ed0b3b0dff72232687addf1c13c6e4ca011e087
Abraham Arce <x0066660@ti.com>
2010-09-18 04:29:12 -0500

    TestSuites: pmd: change to template convention
    - Change main variables under test_runner.sh and conf.sh
      to follow convention
    
      export DRIVER_ROOT=`pwd`
      ...
      export DRIVER_DIR_BINARIES=$DRIVER_ROOT/../bin
    
    - Remove all variables, they will be placed now under utils conf
      directory, all drivers will get benefit from power management test
      code
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

--- pmd/test_code/scripts/conf.sh
+++ pmd/test_code/scripts/conf.sh
@@ -1,141 +1,58 @@
 #!/bin/sh
-######################################################################
-# Configuration script
-# Author  : Misael Lopez Cruz
-# Date    : 04 Jul 07
-# Description: Configuration script with user specified value for some
-# environment variables
-######################################################################
 
-# These variables dont' need to be modified
-export POSTFIX=`date "+%Y%m%d-%H%M%S"`
-export TESTROOT=${PWD}
-export TESTBIN=${PWD}/../bin
-export UTILBIN=${PWD}/../../utils/bin
-export TESTMODS=${PWD}/../modules
-export TESTSCRIPT=${PWD}/helper
-export TMPBASE=${TESTROOT}/tmp
-export TMPFILE=${TMPBASE}/tmp.$POSTFIX
-export CMDFILE=cmd.$POSTFIX
-export TESTDIR=${TESTROOT}/test
-export PRETTY_PRT=""
-export VERBOSE=""
-export OUTPUTFILE=${TESTROOT}/output.$POSTFIX
-export LOGFILE=${TESTROOT}/log.$POSTFIX
-export DURATION=""
-export PATH="${PATH}:${TESTROOT}:${TESTBIN}:${TESTSCRIPT}"
-export TC_SCENARIO="${TESTROOT}/scenarios"
-export SCENARIO_NAMES=""
+# TestSuite General Variables
+export PMD_POSTFIX=`date "+%Y%m%d-%H%M%S"`
+export PMD_ROOT=`pwd`
 
-# Utilities
-export WAIT_ANSWER=${UTILBIN}/akey
+export PMD_DIR_BINARIES=${PMD_ROOT}/../bin
+export PMD_DIR_MODULES=${PMD_ROOT}/../modules
+export PMD_DIR_HELPER=${PMD_ROOT}/helper
+export PMD_DIR_TMP=${PMD_ROOT}/tmp
+export PMD_DIR_TEST=${PMD_ROOT}/test
+export PMD_DIR_SCENARIOS="${PMD_ROOT}/scenarios"
 
-# Logs
-export LOG_OUTPUT="$TESTROOT/log.custom.powermanagement"
+export PMD_FILE_OUTPUT=${PMD_ROOT}/output.$PMD_POSTFIX
+export PMD_FILE_LOG=${PMD_ROOT}/log.$PMD_POSTFIX
+export PMD_FILE_TMP=${PMD_DIR_TMP}/tmp.$PMD_POSTFIX
+export PMD_FILE_CMD=cmd.$PMD_POSTFIX
 
-# Specific Variables
-export NAME=""
-export MESSAGE_DELAY=5
-export DESCRIPTION=""
+export PMD_DURATION=""
+export PMD_PRETTY_PRT=""
+export PMD_VERBOSE=""
+export PMD_SCENARIO_NAMES=""
+export PMD_STRESS=""
+export PMD_PANIC=$PMD_DIR_TMP/ethernet.panic
 
-export MIN_INT_PER_SECOND=0
-export MAX_INT_PER_SECOND=1500
+export PATH="${PMD_ROOT}:${PMD_DIR_HELPER}:$PMD_DIR_BINARIES:${PATH}"
 
-# Power Management
-#export DPM_SCRIPT=$TESTSCRIPT/dpm.bash
-export DPM_SCRIPT=$TESTSCRIPT/dpm_test_8x.bash
-export PARAMETERS=""
-export TIME_TO_REPEAT=0
-export QUIT_OPTION=0
-export SCALE_OPTION=6
-export SLEEP_OPTION=4
-export SLOW_OPERATING_POINT=s
-export FAST_OPERATING_POINT=f
-export STRESS_OPERATING_POINTS=7
-export STRESS_FREQUENCIES=6
+# Utils General Variables
+export UTILS_DIR=$PMD_ROOT/../../utils/
+export UTILS_DIR_BIN=$UTILS_DIR/bin
+export UTILS_DIR_HANDLERS=$UTILS_DIR/handlers
+export UTILS_DIR_SCRIPTS=$UTILS_DIR/scripts
 
-export UTILOMAP3=${TESTROOT}/../utils/omap3
+. $UTILS_DIR/configuration/general.configuration
 
-# General Variables
-export TESTING_DRIVER=""
+export PATH="$PATH:$UTILS_DIR_BIN:$UTILS_DIR_HANDLERS:$UTILS_DIR_SCRIPTS"
 
-#export KERNEL_MESSAGES=/var/log/messages
-export KERNEL_MESSAGES=dmesg
+# Specific pmd Variables
+export PMD_WAKEUP_TIMER_DEFAULT=1
 
-
-# Power Specific Variables
-export ENABLE=1
-export DISABLE=0
-export WAKEUP_TIMER_DEFAULT_VALUE=1
-export MEM="mem"
-export SUSPEND_RESUME_PASS_MESSAGE="resume of devices complete"
-
-# debugFileSystem.sh Script
-export DEBUGFS_DIRECTORY=/debug
-export DEBUGFS_PMCOUNT=$DEBUGFS_DIRECTORY/pm_debug/count
-
-# All sysfs entries for Power
-export SYSFS_POWER_ENTRY=/sys/power/
-export DEBUGFS_POWER_ENTRY=$DEBUGFS_DIRECTORY/pm_debug
-export STATE=$SYSFS_POWER_ENTRY/state
-export SLEEP_WHILE_IDLE=$DEBUGFS_POWER_ENTRY/sleep_while_idle
-export ENABLED_OFF_MODE=$DEBUGFS_POWER_ENTRY/enable_off_mode
-export VOLTAGE_OFF_WHILE_IDLE=$DEBUGFS_POWER_ENTRY/voltage_off_while_idle
-export WAKEUP_TIMER_SECONDS=$DEBUGFS_POWER_ENTRY/wakeup_timer_seconds
-export CPU1=/sys/devices/system/cpu/cpu1/online
-
-# SmartReflex
-export SR_VDD1_AUTOCOMP=$DEBUGFS_POWER_ENTRY/sr1_autocomp
-export SR_VDD2_AUTOCOMP=$DEBUGFS_POWER_ENTRY/sr2_autocomp
-
-
-# OPP
-export VDD1_OPP=$SYSFS_POWER_ENTRY/vdd1_opp
-export VDD2_OPP=$SYSFS_POWER_ENTRY/vdd2_opp
-export VDD1_LOCK=$SYSFS_POWER_ENTRY/vdd1_lock
-export VDD2_LOCK=$SYSFS_POWER_ENTRY/vdd2_lock
-
-# All sysfs entries for cpufreq
-export CPU=cpu0
-export SYSFS_CPUFREQ=/sys/devices/system/cpu/$CPU/cpufreq
-export DSP_FREQ_TMP=$DEBUGFS_DIRECTORY/clock/virt_26m_ck/osc_sys_ck/sys_ck
-export DSP_FREQ=$DSP_FREQ_TMP/dpll2_ck/dpll2_m2_ck/iva2_ck/rate
-
-export SCALING_AVAILABLE_GOVERNORS=$SYSFS_CPUFREQ/scaling_available_governors
-export SCALING_GOVERNOR=$SYSFS_CPUFREQ/scaling_governor
-
-export SCALING_AVAILABLE_FREQUENCIES=$SYSFS_CPUFREQ/\
-scaling_available_frequencies
-export SCALING_CUR_FREQ=$SYSFS_CPUFREQ/scaling_cur_freq
-export SCALING_SET_SPEED=$SYSFS_CPUFREQ/scaling_setspeed
-
-export STATS_TOTAL_TRANS=$SYSFS_CPUFREQ/stats/total_trans
-export STATS_TIME_IN_STATE=$SYSFS_CPUFREQ/stats/time_in_state
-
-
-export GOVERNORS_LIST_OK=$TMPBASE/governors.list.ok
-export GOVERNORS_LIST_ERROR=$TMPBASE/governors.list.error
-
-export GOVERNOR_CONSERVATIVE=conservative
-export GOVERNOR_ONDEMAND=ondemand
-export GOVERNOR_POWERSAVE=powersave
-export GOVERNOR_USERSPACE=userspace
-export GOVERNOR_PERFORMANCE=performance
-
+# oppControl.sh, need update
 export VDD1_OPP1_FREQ=300000
 export VDD1_OPP2_FREQ=600000
 export VDD1_OPP3_FREQ=800000
 export VDD1_OPP4_FREQ=1000000
 
-export AUDIO_SAMPLE=/testsuites/audio-alsa/scripts/helper/audio-samples
-
-if [ ! -e "$AUDIO_SAMPLE" ]; then
-	echo "FATAL: Audio Samples cannot be found -> $AUDIO_SAMPLE"
-	return 1
-fi
+export DSP_FREQ_TMP=$PM_DEBUGFS_DIRECTORY/clock/virt_26m_ck/osc_sys_ck/sys_ck
+export DSP_FREQ=$DSP_FREQ_TMP/dpll2_ck/dpll2_m2_ck/iva2_ck/rate
 
 if [ ! `echo 1+1 | bc` ]; then
 	echo "FATAL: BC is unavailable, cannot continue"
 	return 1
 fi
 
+# Remove any error file
+handlerError.sh "clean"
+
+# End of file
--- pmd/test_code/scripts/test_runner.sh
+++ pmd/test_code/scripts/test_runner.sh
@@ -1,127 +1,112 @@
 #!/bin/sh
 #-----------------------
-# Based on runltp script  from LTP
-# much of the functions are copied over from there - copyright remains
+# Based on runltp script from LTP
+# Much of the functions are copied over from there
+# Copyright remains
 #-----------------------
 
 # Give standard error message and die
 die()
 {
-        echo "FATAL: $*"
-        usage
-        exit 1
+	echo "FATAL: $*"
+	usage
+	exit 1
 }
 
+# Give information message
 info()
 {
 	echo "INFO: $*"
 }
 
+# Give debug message
+
 debug_a()
 {
 	echo "DEBUG: $*"
 }
-# comment out and suit urself..Debug help
+
+# Comment out and suit urself..Debug help
 alias debug=/bin/true
-#alias debug=debug_a
+# alias debug=debug_a
 
 # Defaults and sanity stuff
 setup()
 {
-    cd `dirname $0` || \
-    {
-        die "unable to change directory to $(dirname $0)"
-    }
-    # Load config file if found
-    if [ -f "./test.cf" ]; then
-        . ./test.cf
-    else
-	  echo "test.cf file was not found! Using defaults..."
-	# Load defaults..
-	# if we have a working rtc, this is good.. else use next line
-    export POSTFIX=`date "+%Y%m%d-%H%M%S"`
-    #export POSTFIX=$$
-    # Load up the defaults
-    export TESTROOT=${PWD}
-	  export TESTBIN=${PWD}/../bin
-	  export TESTSCRIPT=${PWD}/scripts
-    export TMPBASE=${TESTROOT}/tmp
-	  export TMPFILE=${TMPBASE}/tmp.$POSTFIX
-    export CMDFILE=cmd.$POSTFIX
-    export TESTDIR=${TESTROOT}/test
-    export PRETTY_PRT=""
-    export VERBOSE=""
-    export OUTPUTFILE=${TESTROOT}/output.$POSTFIX
-    export LOGFILE=${TESTROOT}/log.$POSTFIX
-    export DURATION="1h"
-	  export PATH="${PATH}:${TESTROOT}:${TESTBIN}:${TESTSCRIPT}"
-    export TC_SCENARIO="${TESTROOT}/scenarios"
-    export SCENARIO_NAMES=""
-    export UTILBIN=../../utils/bin
-    fi
 
-    # scenario less tests?? have the user organize it properly at least..
-    [ -d $TC_SCENARIO ] ||
-    {
-        die "Test suite not installed correctly - no scenarios"
-    }
+	cd `dirname $0` || \
+	{
+		die "unable to change directory to $(dirname $0)"
+	}
 
-    # we'd need the reporting tool ofcourse..
-    echo $UTILBIN
-    [ -e $UTILBIN/pan ] ||
-    {
-        die "FATAL: Test suite driver 'pan' not found"
-    }
+	# Load config file
+	if [ -f "./conf.sh" ]; then
+		. ./conf.sh
+		if [ $? -eq 0 ]; then
+			echo "INFO: Requested tests will be started"
+		else
+			echo "FATAL: Configuration file with errors"
+		fi
+	else
+		die "FATAL: Configuration file not found"
+	fi
+
+	# scenario less tests?? have the user organize it properly at least..
+	[ -d $PMD_DIR_SCENARIOS ] ||
+	{
+		die "Test suite not installed correctly - no scenarios"
+	}
+
+	# we'd need the reporting tool ofcourse..
+	[ -e $UTILS_DIR_BIN/pan ] ||
+	{
+		die "FATAL: Test suite driver 'pan' not found"
+	}
 }
 
 usage()
 {
-    # Human redable please
-    local PP=` if [ -z "$PRETTY_PRT" ]; then echo "off"; else echo "on"; fi`
-    local VV=` if [ -z "$VERBOSE" ]; then echo "off"; else echo "on"; fi`
-    # Give the gyan
-    cat <<-EOF >&2
+	# Human redable please
+	local PP=` if [ -z "$PMD_PRETTY_PRT" ]; then echo "off"; else echo "on"; fi`
+	local VV=` if [ -z "$PMD_VERBOSE" ]; then echo "off"; else echo "on"; fi`
 
-    usage: ./${0##*/} [-z] [-h] [-v] [-d TESTDIR] [-o OUTPUTFILE] [-l LOGFILE]
-    [-n DURATION ] [-t TMPDIR] [SCENARIO_NAMES..]
+	# Give the gyan
+	cat <<-EOF >&2
+	usage: ./${0##*/} [-z] [-h] [-v] [-d PMD_DIR_TEST] [-o PMD_FILE_OUTPUT] [-l PMD_FILE_LOG]
+	[-n PMD_DURATION ] [-t TMPDIR] [PMD_SCENARIO_NAMES..]
 
-    -d TESTDIR      Run LTP to test the filesystem mounted here.
-                    [Current - $TESTDIR]
-                    At the end of test, the testdir gets cleaned out
-    -s TC_SCENARIO  Test scenarios are located here. [Current - $TC_SCENARIO]
-    -o OUTPUTFILE   Redirect test output to a file.
-                    [Current- $OUTPUTFILE {psid}]
-    -p              Human readable(dont laugh too much) format logfiles.
-                    [Current - ($PP)]
-    -z              Dont Merge Scenario Name with tcid to create final tcid
-    -E              Use Extended Test cases also - these are painful and can
-                    take real long time
-    -l LOGFILE      Log results of test in a logfile.
-                    [Current- $LOGFILE {psid}]
-    -t TMPDIR       Run LTP using tmp dir [Current - $TMPBASE]
-    -n DURATION     Execute the testsuite for given duration. Examples:
-                      -n 60s = 60 seconds
-                      -n 45m = 45 minutes
-                      -n 24h = 24 hours
-                      -n 2d  = 2 days
-                    [Current - $DURATION]
-    -v              Print more verbose output to screen.[Current - ($VV)]
-    -q              No messages from this script. no info too - Brave eh??
-    -h              This screen [Current - guess...]
-    -x INSTANCES    Run multiple instances of this testsuite.(think well...)
-    -r PRE_DEF      Run predefined set of scenarios[Not Implemented yet]
-                     List to appear here
-    -I              Run test cases which require user interaction
-    -S              Special Mode - Stress Testing
-    SCENARIO_NAMES  List of scenarios to test.. else, take all scenarios
-                    [Current - These are all filenames from $TC_SCENARIO]
+	-d PMD_DIR_TEST      Run LTP to test the filesystem mounted here. [Current - $PMD_DIR_TEST]
+			At the end of test, the testdir gets cleaned out
+	-s PMD_DIR_SCENARIOS  Test scenarios are located here. [Current - $PMD_DIR_SCENARIOS]
+	-o PMD_FILE_OUTPUT   Redirect test output to a file. [Current- $PMD_FILE_OUTPUT {psid}]
+	-p              Human readable(dont laugh too much) format logfiles. [Current - ($PP)]
+	-z              Dont Merge the Scenario Name with tcid to create final tc id
+	-E              Use Extended Test cases also - these are painful and can take real long time
+	-l PMD_FILE_LOG      Log results of test in a logfile. [Current- $PMD_FILE_LOG {psid}]
+	-t TMPDIR       Run LTP using tmp dir [Current - $PMD_DIR_TMP]
+	-n PMD_DURATION     Execute the testsuite for given duration. Examples:
+			-n 60s = 60 seconds
+			-n 45m = 45 minutes
+			-n 24h = 24 hours
+			-n 2d  = 2 days
+			[Current - $PMD_DURATION]
 
-    Good News: Ctrl+c stops and cleans up for you :)
-    More help: Read the $TESTROOT/README
+	-v              Print more verbose output to screen.[Current - ($VV)]
+	-q              No messages from this script. no info too - Brave eh??
+	-h              This screen [Current - guess...]
+	-x INSTANCES    Run multiple instances of this testsuite.(think well...)
+	-r PRE_DEF      Run predefined set of scenarios[Not Implemented yet]
+			List to appear here
+	-S              Run in Stress mode
 
+	PMD_SCENARIO_NAMES  List of scenarios to test.. else, take all scenarios
+			[Current - These are all filenames from $PMD_DIR_SCENARIOS]
+
+	Good News: Ctrl+c stops and cleans up for you :)
+	More help: Read the $PMD_ROOT/README
 
 	EOF
-exit 0
+	exit 0
 }
 
 # Insane world.. insane set of human mistakes!!
@@ -129,194 +114,190 @@ sanity_check()
 {
     # Check the current values...
     # Just ensure that pan can run with a bit of peace of mind...
-    [ ! -d "$TMPBASE" -o ! -w "$TMPBASE" ] && \
-die "$TMPBASE - cannot work as temporary directory"
 
-    [ ! -d "$TESTBIN" ] &&\
- die "$TESTBIN - cannot find test binary directory"
-    [ ! -d "$TESTDIR" -o ! -w "$TESTDIR" ] && \
-die "$TESTDIR - cannot work as test directory"
-    [ ! -d "$TC_SCENARIO" ] && \
-die "$TC_SCENARIO - No such directories"
-    [ -z "$SCENARIO_NAMES" ] && die "No Scenarios"
-    #[ ! -z "$VERBOSE" -a ! -z "$QUIET_MODE" ] && \
-#die "Make up your mind - verbose or quiet??"
-    export CMDFILE=$TMPBASE/$CMDFILE
-    rm -f $CMDFILE
-    for SCEN in $SCENARIO_NAMES
+    [ ! -d "$PMD_DIR_TMP" -o ! -w "$PMD_DIR_TMP" ] && die "$PMD_DIR_TMP - cannot work as temporary directory"
+    [ ! -d "$PMD_DIR_TEST" -o ! -w "$PMD_DIR_TEST" ] && die "$PMD_DIR_TEST - cannot work as test directory"
+    [ ! -d "$PMD_DIR_SCENARIOS" ] && die "$PMD_DIR_SCENARIOS - No such directories"
+    [ -z "$PMD_SCENARIO_NAMES" ] && die "No Scenarios"
+		[ ! -z "$PMD_VERBOSE" -a ! -z "$PMD_QUIET_MODE" ] && die "Make up your mind - verbose or quiet??"
+
+    export PMD_FILE_CMD=$PMD_DIR_TMP/$PMD_FILE_CMD
+    rm -f $PMD_FILE_CMD
+
+		for SCEN in $PMD_SCENARIO_NAMES
     do
-        [ ! -f "$TC_SCENARIO/$SCEN" -o ! -r "$TC_SCENARIO/$SCEN" ] && \
-die "$TC_SCENARIO/$SCEN - not a scenario file"
-        cat $TC_SCENARIO/$SCEN|grep -v "#"|sed -e "s/^[  ]*$//g"|\
-sed -e "/^$/d">$TMPFILE|| die "Count not create tmp file $TMPFILE"
-	if [ -z "$DONT" ]; then
-	cat $TMPFILE|sed -e "s/^/$SCEN-/g"|sed -e "s/-/_/" >>$CMDFILE ||\
- die "Count not create command file $CMDFILE"
-	else
-	cat $TMPFILE>>$CMDFILE || die "Count not create command file $CMDFILE"
-	fi
-	if [ -z "$EXTENDED_TEST" ]; then
-		#Remove the extended test cases
-		cat $CMDFILE|grep -v "^[_A-Za-z0-9]*_EXT ">$TMPFILE ||\
- die "intermediate file gen failed"
-		cat $TMPFILE>$CMDFILE || die "Second intermediate \
-creation failed"
-	fi
-	if [ -z "$INTERACTIVE" ]; then
-		#Remove the test cases which require user interaction
-		cat $CMDFILE|grep -v "^[_A-Za-z0-9]*_INT ">$TMPFILE ||\
- die "intermediate file gen failed"
-		cat $TMPFILE>$CMDFILE || die "Second intermediate \
-creation failed"
-	fi
+		  [ ! -f "$PMD_DIR_SCENARIOS/$SCEN" -o ! -r "$PMD_DIR_SCENARIOS/$SCEN" ] && die "$PMD_DIR_SCENARIOS/$SCEN - not a scenario file"
+			cat $PMD_DIR_SCENARIOS/$SCEN|grep -v "#"|sed -e "s/^[  ]*$//g"|sed -e "/^$/d">$PMD_FILE_TMP|| die "Count not create tmp file $PMD_FILE_TMP"
+			if [ -z "$DONT" ]; then
+				cat $PMD_FILE_TMP|sed -e "s/^/$SCEN-/g"|sed -e "s/-/_/" >>$PMD_FILE_CMD || die "Count not create command file $PMD_FILE_CMD"
+				else
+				cat $PMD_FILE_TMP>>$PMD_FILE_CMD || die "Count not create command file $PMD_FILE_CMD"
+			fi
 
-	rm -f $TMPFILE
+			# Remove the extended test cases
+			if [ -z "$EXTENDED_TEST" ]; then
+
+				cat $PMD_FILE_CMD|grep -v "^[_A-Za-z0-9]*_EXT ">$PMD_FILE_TMP || die "intermediate file gen failed"
+				cat $PMD_FILE_TMP>$PMD_FILE_CMD || die "Second intermediate creation failed"
+			fi
+
+			rm -f $PMD_FILE_TMP
+
     done
-    local PP=` if [ -z "$PRETTY_PRT" ]; then echo "off"; else echo "on"; fi`
-    local VV=` if [ -z "$VERBOSE" ]; then echo "off"; else echo "on"; fi`
-    export TMPDIR=${TESTDIR}
-    #print some nice info
-    if [ ! -z "$VERBOSE" ]; then
-        debug "POSTFIX        $POSTFIX       "
-        info  "TESTROOT       $TESTROOT      "
-        info  "TMPBASE        $TMPBASE       "
-        info  "TMPFILE        $TMPFILE       "
-        debug "CMDFILE        $CMDFILE       "
-        info  "TESTDIR        $TESTDIR       "
-        info  "PRETTY_PRT     $PP            "
-        info  "VERBOSE        $VV            "
-        info  "OUTPUTFILE     $OUTPUTFILE    "
-        info  "LOGFILE        $LOGFILE       "
-        info  "DURATION       $DURATION      "
+
+		local PP=` if [ -z "$PMD_PRETTY_PRT" ]; then echo "off"; else echo "on"; fi`
+    local VV=` if [ -z "$PMD_VERBOSE" ]; then echo "off"; else echo "on"; fi`
+    export TMPDIR=${PMD_DIR_TEST}
+
+		# Print some nice info
+    if [ ! -z "$PMD_VERBOSE" ]; then
+        debug "PMD_POSTFIX        $PMD_POSTFIX       "
+        info  "PMD_ROOT       $PMD_ROOT      "
+        info  "PMD_DIR_TMP        $PMD_DIR_TMP       "
+        info  "PMD_FILE_TMP        $PMD_FILE_TMP       "
+        debug "PMD_FILE_CMD        $PMD_FILE_CMD       "
+        info  "PMD_DIR_TEST        $PMD_DIR_TEST       "
+        info  "PMD_PRETTY_PRT     $PP            "
+        info  "PMD_VERBOSE        $VV            "
+        info  "PMD_FILE_OUTPUT     $PMD_FILE_OUTPUT    "
+        info  "PMD_FILE_LOG        $PMD_FILE_LOG       "
+        info  "PMD_DURATION       $PMD_DURATION      "
         debug "PATH           $PATH          "
-        info  "TC_SCENARIO    $TC_SCENARIO   "
+        info  "PMD_DIR_SCENARIOS    $PMD_DIR_SCENARIOS   "
         info  "TMPDIR         $TMPDIR        "
-        info  "SCENARIO_NAMES $SCENARIO_NAMES"
+        info  "PMD_SCENARIO_NAMES $PMD_SCENARIO_NAMES"
     fi
 }
 
 main()
 {
-   count=0
-    while getopts zx:Sd:qt:po:l:vn:hs:E:I arg
-    do  case $arg in
-        d)
-           TESTDIR=${OPTARG} ;;
-        t)
-           TMPBASE=${OPTARG} ;;
-	      E)
-	         EXTENDED_TEST=y ;;
-        q)
-           QUIET_MODE=" -q " ;;
-        z)
-           DONT=" " ;;
-        p)
-           PRETTY_PRT=" -p " ;;
-        o)
-           OUTPUTFILE=${OPTARG};OO_LOG=1 ;;
-        l)
-           LOGFILE=${OPTARG} ;;
-        v)
-           VERBOSE="-v" ;;
-        n)
-           DURATION=" -t ${OPTARG}" ;;
-        h)
-            usage ;;
-        x)  # number of ltp's to run
-            cat <<-EOF >&1
-            WARNING: The use of -x can cause unpredictable failures, as a
-                     result of concurrently running multiple tests designed
-                     to be ran exclusively.
-                     Pausing for 10 seconds...Last chance to hit that ctrl+c
-	EOF
-            sleep 10
-            INSTANCES="-x $OPTARG -O ${TMP}";;
+	count=0
+	while getopts zx:Sd:qt:po:l:vn:hs:E:I arg
+	do  case $arg in
+		d)
+			PMD_DIR_TEST=${PMD_OPTARG} ;;
+		t)
+			PMD_DIR_TMP=${PMD_OPTARG} ;;
+		E)
+			EXTENDED_TEST=y ;;
+	        q)
+			PMD_QUIET_MODE=" -q " ;;
+	        z)
+			DONT=" " ;;
+		p)
+			PMD_PRETTY_PRT=" -p " ;;
+		o)
+			PMD_FILE_OUTPUT=${PMD_OPTARG};OO_LOG=1 ;;
+		l)
+			PMD_FILE_LOG=${PMD_OPTARG} ;;
+		v)
+			PMD_VERBOSE="-v" ;;
+		n)
+			PMD_DURATION=" -t ${PMD_OPTARG}" ;;
+		h)
+			usage ;;
+		x)  # number of ltp's to run
 
-        s)
-           TC_SCENARIO=${OPTARG} ;;
-        I)
-           INTERACTIVE=y ;;
-        S)
-           STRESS=y ;;
+			cat <<-EOF >&1
+			WARNING: The use of -x can cause unpredictable failures, as a
+			result of concurrently running multiple tests designed
+			to be ran exclusively.
+			Pausing for 10 seconds...Last chance to hit that ctrl+c
+			EOF
+					sleep 10
+			PMD_INSTANCES="-x $PMD_OPTARG -O ${TMP}" ;;
+		s)
+			PMD_DIR_SCENARIOS=${PMD_OPTARG} ;;
+		S)
+			PMD_STRESS=y
+			PMD_STRESSARG="-S";;
 
-        \?) # Handle illegals
-            usage ;;
-        esac
-     if [ ! -z "${OPTARG}" ]; then
-	count=" $count + 2"
-     else
-	count=" $count + 1"
-     fi
-    done
-    count=$(( $count ))
-    while [ $count -ne 0 ]
-    do
-       shift;
-       count=$(($count - 1))
-    done
-    SCENARIO_NAMES=$@
-    sanity_check
-    # test start
-    [ -z "$QUIET_MODE" ] && { info "Test start time: $(date)" ; }
-    # run pan
-    #$PAN_COMMAND #Duplicated code here, because otherwise
-    # if we fail, only "PAN_COMMAND" gets output
-        #Usage: pan -n name [ -SyAehp ] [ -s starts ]
-        # [-t time[s|m|h|d] [ -x nactive ] [
-        #-l logfile ]
-        #[ -a active-file ] [ -f command-file ] [ -d debug-level ]
-        #[-o output-file] [-O output-buffer-directory] [cmd]
-    cd $TESTDIR
-    echo $UTILBIN
-    PAN_COMMAND="${UTILBIN}/pan $QUIET_MODE -e -S $INSTANCES $DURATION \
--a $$ -n $$ $PRETTY_PRT -f ${CMDFILE} -l $LOGFILE "
-    [ ! -z "$VERBOSE" ] && { info "PAN_COMMAND=$PAN_COMMAND"; }
-    if [ -z "$OO_LOG" ]; then
-    $PAN_COMMAND
-    else
-	    $PAN_COMMAND|tee $OUTPUTFILE
-    fi
+		\?) # Handle illegals
+			usage ;;
 
-    if [ $? -eq 0 ]; then
-      echo "INFO: pan reported all tests PASS"
-      VALUE=0
-    else
-      echo "INFO: pan reported some tests FAIL"
-      VALUE=1
-    fi
-    # test end
-    [ -z "$QUIET_MODE" ] && { info "Test end time: $(date)" ; }
-    [ -z "$QUIET_MODE" ] && {
+	esac
 
-    cat <<-EOF >&1
+	if [ ! -z "${PMD_OPTARG}" ]; then
+		count=" $count + 2"
+	else
+		count=" $count + 1"
+	fi
+
+	done
+
+	count=$(( $count ))
+	while [ $count -ne 0 ]
+	do
+		shift;
+		count=$(($count - 1))
+	done
+
+	PMD_SCENARIO_NAMES=$@
+
+	sanity_check
+
+	# Test start
+
+	[ -z "$PMD_QUIET_MODE" ] && { info "Test start time: $(date)" ; }
+
+	# Usage: pan -n name [ -SyAehp ] [ -s starts ] [-t time[s|m|h|d] [ -x nactive ] [-l logfile ]
+	# [ -a active-file ] [ -f command-file ] [ -d debug-level ]
+	# [-o output-file] [-O output-buffer-directory] [cmd]
+
+	cd $PMD_DIR_TEST
+	PAN_COMMAND="${UTILS_DIR_BIN}/pan $PMD_QUIET_MODE -e -S $PMD_INSTANCES $PMD_DURATION -a $$ -n $$ $PMD_PRETTY_PRT -f ${PMD_FILE_CMD} -l $PMD_FILE_LOG"
+
+	[ ! -z "$PMD_VERBOSE" ] && { info "PAN_COMMAND=$PAN_COMMAND"; }
+
+	if [ -z "$OO_LOG" ]; then
+		$PAN_COMMAND
+	else
+		$PAN_COMMAND|tee $PMD_FILE_OUTPUT
+	fi
+
+	if [ $? -eq 0 ]; then
+		echo "INFO: pan reported all tests PASS"
+		VALUE=0
+	else
+		echo "INFO: pan reported some tests FAIL"
+		VALUE=1
+	fi
+
+	# Test end
+	[ -z "$PMD_QUIET_MODE" ] && { info "Test end time: $(date)" ; }
+	[ -z "$PMD_QUIET_MODE" ] && {
+
+	cat <<-EOF >&1
 
 	###############################################################"
-
-	    Done executing testcases."
-            result log is in the $LOGFILE "
-
-       ###############################################################"
+		Done executing testcases."
+		Result log is in the $PMD_FILE_LOG "
+	###############################################################"
 
 	EOF
-	cat $LOGFILE
-    }
-    cleanup
-    exit $VALUE
+	cat $PMD_FILE_LOG
+
+	}
+	cleanup
+	exit $VALUE
 }
+
 
 cleanup()
 {
-    [  -z "$QUIET_MODE" ] && echo -n "INFO: Cleaning up..."
-    if [ -n "${TMPFILE}" -a -n "${CMDFILE}" -a -n \
-"${TESTDIR}" -a -n "${TMPBASE}" ]; then
-        rm -rf ${TMPFILE} ${CMDFILE} ${TESTDIR}/* ${TMPBASE}/*
-    else
-        echo "INFO: Clean up process won't be executed
- because variables for directories to be removed are not set..."
-    fi
-    [  -z "$QUIET_MODE" ] && echo "done."
+	[  -z "$PMD_QUIET_MODE" ] && echo -n "INFO: Cleaning up..."
+	if [ -n "${PMD_FILE_TMP}" -a -n "${PMD_FILE_CMD}" -a -n "${PMD_DIR_TEST}" -a -n "${PMD_DIR_TMP}" ]; then
+		rm -rf ${PMD_FILE_TMP} ${PMD_FILE_CMD} ${PMD_DIR_TEST}/* ${PMD_DIR_TMP}/*
+	else
+		echo "INFO: Clean up process won't be executed because variables for directories to be removed are not set..."
+	fi
+
+	[  -z "$PMD_QUIET_MODE" ] && echo "done."
 }
+
 
 trap "cleanup" 0
 setup
 main "$@"
+
+# End of file
