4438e1255e14a9eda6e51533bb176eaab70a95b4
Abraham Arce <x0066660@ti.com>
2010-09-18 04:49:54 -0500

    TestSuites: pmd: timediff clean up
    Do some clean up
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

--- pmd/test_code/scripts/helper/timediff.sh
+++ pmd/test_code/scripts/helper/timediff.sh
@@ -2,10 +2,10 @@
 
 if [ $# -ne 3 ]
 then
-        echo " "
-        echo "Usage: timediff.sh <time before suspend> <time after wakeup> <wake
-        echo " "
-        exit 1
+	echo " "
+	echo "Usage: timediff.sh <time before suspend> <time after wakeup> <wakeup time>"
+	echo " "
+	exit 1
 fi
 
 tdiff=`expr $2 - $1`
@@ -14,7 +14,9 @@ delta=`expr $tdiff - $3`
 
 if [ $delta -lt  5 ]
 then
-        echo "TEST PASSED"
+	echo "TEST PASSED"
 else
-        echo "TEST FAILED"
+	echo "TEST FAILED"
 fi
+
+# End of file
