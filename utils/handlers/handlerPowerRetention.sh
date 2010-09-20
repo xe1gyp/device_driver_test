efbfcdbf66a2a8ca44dcaa36e54b5ebd167ae189
Abraham Arce <x0066660@ti.com>
2010-09-19 23:38:48 -0500

    TestSuites: Utils: Power retention initial code
    - handlerPoweRetention.sh "enable"
      @operation  operation to perform, enable
                  echo 1 > /sys/class/graphics/fb0/blank
                  echo 0 > /sys/devices/system/cpu/cpu1/online
                  The way it is enable may change in future
    
    - handlerPoweRetention.sh "check" <power domain>
      @operation  operation to perform, check
      @domain     power domain where to check retention
                  e.g. core_pwrdm, abe_pwrdm
    
      "check" operation can be used when checking if retention
      is not broken after a set of processes are executed
    
    - handlerPoweRetention.sh "disable"
      @operation  operation to perform, disable
                  echo 0 > /sys/class/graphics/fb0/blank
                  echo 1 > /sys/devices/system/cpu/cpu1/online
                  The way it is disable may change in future
    
    - handlerPoweRetention.sh "run" <power domain> <command>
      @operation  operation to perform, run
      @domain     power domain where to check retention
                  e.g. core_pwrdm, abe_pwrdm
      @command    the command to run
    
      "run" operation can be used when we need to check if a
       process is not breaking retention
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

---
+++ utils/handlers/handlerPowerRetention.sh
@@ -0,0 +1,101 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+LOCAL_OPERATION=$1
+LOCAL_ERROR=0
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+enableRetention() {
+	handlerSysFs.sh  "set"  $SYSFS_FB_BLANK  1
+	handlerSysFs.sh  "set"  $SYSFS_CPU1_ONLINE  0
+}
+
+disableRetention() {
+	handlerSysFs.sh  "set"  $SYSFS_CPU1_ONLINE  1
+	handlerSysFs.sh  "set"  $SYSFS_FB_BLANK  0
+}
+
+checkRetention() {
+	LOCAL_POWER_DOMAIN=$1
+	LOCAL_COMMAND_LINE=$2
+
+	handlerPowerTransitionStats.sh "log" $LOCAL_POWER_DOMAIN "RET" "1"
+
+	if [ -n "$LOCAL_COMMAND_LINE" ]; then
+		echo "Info: Running command -> $LOCAL_COMMAND_LINE"
+		$LOCAL_COMMAND_LINE
+	fi
+
+	echo "Info: Sleeping for $PM_RETENTION_TIMER_DEFAULT seconds..."
+  sleep $PM_RETENTION_TIMER_DEFAULT
+
+	handlerPowerTransitionStats.sh "log" $LOCAL_POWER_DOMAIN "RET" "2"
+	handlerPowerTransitionStats.sh "compare" "1" "2"
+  if [ $? -eq 1 ]; then
+    LOCAL_ERROR=1
+  fi
+
+}
+
+# =============================================================================
+# Main
+# =============================================================================
+
+handlerError.sh "test"
+if [ $? -eq 1 ]; then
+	exit 1
+fi
+
+if [ ! -f $SYSFS_FB_BLANK ]; then
+	echo "Fatal: $SYSFS_FB_BLANK cannot be found!"
+	handlerError.sh "log" "1" "halt" "handlerPowerRetention.sh"
+	LOCAL_ERROR=1
+fi
+
+if [ ! -f $SYSFS_CPU1_ONLINE ]; then
+	echo "Fatal: $SYSFS_CPU1_ONLINE cannot be found!"
+	handlerError.sh "log" "1" "halt" "handlerPowerRetention.sh"
+	LOCAL_ERROR=1
+fi
+
+if [ $LOCAL_ERROR -eq 1 ]; then
+	exit $LOCAL_ERROR
+fi
+
+if [ "$LOCAL_OPERATION" = "enable" ]; then
+
+	enableRetention
+
+elif [ "$LOCAL_OPERATION" = "check" ]; then
+
+	LOCAL_POWER_DOMAIN=$2
+
+	checkRetention $LOCAL_POWER_DOMAIN
+
+elif [ "$LOCAL_OPERATION" = "disable" ]; then
+
+	disableRetention
+
+elif [ "$LOCAL_OPERATION" = "run" ]; then
+
+	LOCAL_POWER_DOMAIN=$2
+	LOCAL_COMMAND_LINE=$3
+
+	checkRetention $LOCAL_POWER_DOMAIN $LOCAL_COMMAND_LINE
+
+else
+
+  echo "Fatal: Operation in smartReflexAutoComp is not supported!"
+  LOCAL_ERROR=1
+
+fi
+
+exit $LOCAL_ERROR
+
+# End of file
