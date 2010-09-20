74626ddc7bffda393df5c81d144ea4cea320a2ef
Abraham Arce <x0066660@ti.com>
2010-09-19 23:50:10 -0500

    TestSuites: Utils: Powerdomain state statistics
    Add initial code to check for a particular statistic of a given
    powerdomain
    
    - handlerPowerTransitionStats.sh "log" <domain> <field> <position>
      @operation  operation to perform, log a value to a file
      @domain     powerdomain from where to check statistic
                  e.g. core_pwrdm, abe_pwrdm
      @field      the field from where to extrac counter
                  e.g. OFF, RET, INA
      @position   the position where to log this value
                  e.g. 1, 2, 3
    
      This option will save the value of the requested powerdomain:field
      and save it to a file
    
    - handlerPowerTransitionStats.sh "compare" <position> <position>
      @operation  operation to perform, compare 2 values
      @position   the position 1 to compare
                  e.g. 1, 2, 3
      @position   the position 2 to compare
                  e.g. 1, 2, 3
    
      This option will compare the logged values, return values are
      0 Values are different, counter was increased
      1 Values are the same, counter did not increase
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

---
+++ utils/handlers/handlerPowerTransitionStats.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+LOCAL_OPERATION=$1
+LOCAL_DOMAIN=$2
+LOCAL_ERROR=0
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+handlerError.sh "test"
+if [ $? -eq 1 ]; then
+	exit 1
+fi
+
+handlerDebugFileSystem.sh "mount"
+
+if [ "$LOCAL_OPERATION" = "log" ]; then
+
+	LOCAL_POWER_DOMAIN=$2
+	LOCAL_HIT=$3
+	LOCAL_VALUE=$4
+	LOCAL_HIT_PLACE=0
+
+	if [ "$LOCAL_HIT" = "OFF" ]; then
+		LOCAL_HIT_PLACE=2
+	elif [ "$LOCAL_HIT" = "RET" ]; then
+		LOCAL_HIT_PLACE=3
+	elif [ "$LOCAL_HIT" = "INA" ]; then
+		LOCAL_HIT_PLACE=4
+	elif [ "$LOCAL_HIT" = "ON" ]; then
+		LOCAL_HIT_PLACE=5
+	fi
+
+	LOCAL_TEMP=`cat $PM_COUNT | grep ^$LOCAL_POWER_DOMAIN | cut -d "," -f $LOCAL_HIT_PLACE`
+	echo "Info: Powerdomain requested: $LOCAL_POWER_DOMAIN"
+	echo "Info: Powerdomain transition statistic requested: $LOCAL_TEMP"
+	LOCAL_RESULT=`echo $LOCAL_TEMP | cut -d ":" -f 2`
+	echo $LOCAL_RESULT > $UTILS_DIR_TMP/pts.$LOCAL_VALUE
+
+elif [ "$LOCAL_OPERATION" = "compare" ]; then
+
+	LOCAL_VALUE_ONE=$2
+	LOCAL_VALUE_TWO=$3
+
+	echo "Info: Initial Value -> `cat $UTILS_DIR_TMP/pts.$LOCAL_VALUE_ONE`"
+	echo "Info: Final Value -> `cat $UTILS_DIR_TMP/pts.$LOCAL_VALUE_TWO`"
+
+	sleep 5
+
+	diff $UTILS_DIR_TMP/pts.$LOCAL_VALUE_ONE $UTILS_DIR_TMP/pts.$LOCAL_VALUE_TWO
+
+	if [ $? -eq 0 ]; then
+		echo "Error: Values does match, counter did not increase"
+		LOCAL_ERROR=1
+	else
+		echo "Info: Values do not match, counter was increased"
+		echo "TEST PASSED"
+		LOCAL_ERROR=0
+	fi
+
+fi
+
+handlerDebugFileSystem.sh "umount"
+
+exit $LOCAL_ERROR
+
+# End of file
