1c82749e4acbf1b5086d0ee821ae66b7000dc4d7
Abraham Arce <x0066660@ti.com>
2010-10-05 10:43:31 -0500

    TestSuites: Keypad: evtest handler addition
    Due to manipulation of information required from evtest, create
    handlerEvtest.sh, initial operations are:
    
    - handlerEvtest.sh "run" <devfsnode> <iterations>
      @operation   operation to perform, run
      @devfsnode   devfs node where driver was registered, /dev/input/eventX
      @iterations  number of iterations to wait for
    
    - handlerEvtest.sh "test" <devfsnode> <iterations>
      @operation   operation to perform, test
      @devfsnode   devfs node where driver was registered, /dev/input/eventX
      @iterations  number of iterations to wait for
    
      Operation test with no argument at the end will check if events were
      generated, if not, return 1
    
    - handlerEvtest.sh "test" <devfsnode> <iterations> "noevents"
      @operation   operation to perform, test
      @devfsnode   devfs node where driver was registered, /dev/input/eventX
      @iterations  number of iterations to wait for
      @mode        specify no interaction shall be performed
    
      Operation test with "noevents" argument at the end will check if events were
      not generated, if they were, return 1
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

---
+++ keypad/test_code/scripts/helper/handlerEvtest.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+LOCAL_OPERATION=$1
+LOCAL_DEVFS_NODE=$2
+LOCAL_NUMBER_ITERATIONS=$3
+
+LOCAL_TEMP_FILE=$TOUCHSCREEN_DIR_TMP/touchscreen.temp.file
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+handlerError.sh "test"
+if [ $? -eq 1 ]; then
+	exit 1
+fi
+
+if [ "$LOCAL_OPERATION" = "run" ]; then
+
+	echo -e "\n\n\nINFO: Please interact with keypad in 2 seconds"
+	echo -e "INFO: Working with $LOCAL_DEVFS_NODE\n\n\n"
+
+	sleep 2
+
+	evtest $LOCAL_DEVFS_NODE $LOCAL_NUMBER_ITERATIONS
+
+elif [ "$LOCAL_OPERATION" = "test" ]; then
+
+	LOCAL_MODE=$4
+
+	if [ "$LOCAL_MODE" = "noevents" ]; then
+
+		echo -e "\n\n\nINFO: Please DO NOT interact with keypad, wait for process to finish"
+		echo -e "INFO: Working with $LOCAL_DEVFS_NODE\n\n\n"
+		sleep 2
+		eval evtest $LOCAL_DEVFS_NODE $LOCAL_NUMBER_ITERATIONS | grep Event | grep time | grep type | grep code > $LOCAL_TEMP_FILE &
+		sleep 10
+		test -s $LOCAL_TEMP_FILE
+		if [ $? -eq 0 ]; then
+			echo "FATAL: Events were present under $LOCAL_DEVFS_NODE, please check!"
+			exit 1
+		fi
+
+	else
+
+		echo -e "\n\n\nINFO: Please interact with keypad in 2 seconds"
+		echo -e "INFO: Working with $LOCAL_DEVFS_NODE, no events will be displayed"
+		sleep 2
+		echo -e "INFO: Now!\n\n\n"
+		eval evtest $LOCAL_DEVFS_NODE $LOCAL_NUMBER_ITERATIONS | grep Event | grep time | grep type | grep code > $LOCAL_TEMP_FILE &
+		sleep 10
+		test -s $LOCAL_TEMP_FILE
+		if [ $? -eq 1 ]; then
+			echo "FATAL: Events were not present under $LOCAL_DEVFS_NODE, please check!"
+			exit 1
+		fi
+	fi
+
+else
+	echo "Unsupported Operation"
+	exit 1
+fi
+
+# End of file
