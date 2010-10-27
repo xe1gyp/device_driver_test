bde2cfea0087b6fff5141ab779b98544b77d701d
Abraham Arce <x0066660@ti.com>
2010-10-27 14:02:30 -0500

    TestSuites: Utils: Log of states in power domains
    Power transitions shall log statistics based on the state we are
    requesting, not only on numbers, this is, to be able to log several
    states for a power domain in one single scenario
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

--- utils/handlers/handlerPowerTransitionStats.sh
+++ utils/handlers/handlerPowerTransitionStats.sh
@@ -27,45 +27,46 @@ handlerDebugFileSystem.sh "mount"
 if [ "$LOCAL_OPERATION" = "log" ]; then
 
 	LOCAL_POWER_DOMAIN=$2
-	LOCAL_HIT=$3
+	LOCAL_STATE=$3
 	LOCAL_VALUE=$4
-	LOCAL_HIT_PLACE=0
+	LOCAL_STATE_PLACE=0
 
-	if [ "$LOCAL_HIT" = "OFF" ]; then
-		LOCAL_HIT_PLACE=2
-	elif [ "$LOCAL_HIT" = "RET" ]; then
-		LOCAL_HIT_PLACE=3
-	elif [ "$LOCAL_HIT" = "INA" ]; then
-		LOCAL_HIT_PLACE=4
-	elif [ "$LOCAL_HIT" = "ON" ]; then
-		LOCAL_HIT_PLACE=5
-	elif [ "$LOCAL_HIT" = "RET-LOGIC-OFF" ]; then
-		LOCAL_HIT_PLACE=6
+	if [ "$LOCAL_STATE" = "OFF" ]; then
+		LOCAL_STATE_PLACE=2
+	elif [ "$LOCAL_STATE" = "RET" ]; then
+		LOCAL_STATE_PLACE=3
+	elif [ "$LOCAL_STATE" = "INA" ]; then
+		LOCAL_STATE_PLACE=4
+	elif [ "$LOCAL_STATE" = "ON" ]; then
+		LOCAL_STATE_PLACE=5
+	elif [ "$LOCAL_STATE" = "RET-LOGIC-OFF" ]; then
+		LOCAL_STATE_PLACE=6
 	fi
 
-	LOCAL_TEMP=`cat $PM_COUNT | grep ^$LOCAL_POWER_DOMAIN | cut -d "," -f $LOCAL_HIT_PLACE`
+	LOCAL_TEMP=`cat $PM_COUNT | grep ^$LOCAL_POWER_DOMAIN | cut -d "," -f $LOCAL_STATE_PLACE`
 	echo "Info: Powerdomain requested: $LOCAL_POWER_DOMAIN"
 	echo "Info: Powerdomain transition statistic requested: $LOCAL_TEMP"
 	LOCAL_RESULT=`echo $LOCAL_TEMP | cut -d ":" -f 2`
-	echo $LOCAL_RESULT > $UTILS_DIR_TMP/pts.$LOCAL_VALUE
+	echo $LOCAL_RESULT > $UTILS_DIR_TMP/pts.$LOCAL_STATE.$LOCAL_VALUE
 
 elif [ "$LOCAL_OPERATION" = "compare" ]; then
 
-	LOCAL_VALUE_ONE=$2
-	LOCAL_VALUE_TWO=$3
+	LOCAL_STATE=$2
+	LOCAL_VALUE_ONE=$3
+	LOCAL_VALUE_TWO=$4
 
-	echo "Info: Initial Value -> `cat $UTILS_DIR_TMP/pts.$LOCAL_VALUE_ONE`"
-	echo "Info: Final Value -> `cat $UTILS_DIR_TMP/pts.$LOCAL_VALUE_TWO`"
+	echo "Info: $LOCAL_POWER_DOMAIN Initial Value -> `cat $UTILS_DIR_TMP/pts.$LOCAL_STATE.$LOCAL_VALUE_ONE`"
+	echo "Info: $LOCAL_POWER_DOMAIN Final Value -> `cat $UTILS_DIR_TMP/pts.$LOCAL_STATE.$LOCAL_VALUE_TWO`"
 
 	sleep 5
 
-	diff $UTILS_DIR_TMP/pts.$LOCAL_VALUE_ONE $UTILS_DIR_TMP/pts.$LOCAL_VALUE_TWO
+	diff $UTILS_DIR_TMP/pts.$LOCAL_STATE.$LOCAL_VALUE_ONE $UTILS_DIR_TMP/pts.$LOCAL_STATE.$LOCAL_VALUE_TWO
 
 	if [ $? -eq 0 ]; then
-		echo "Error: Values does match, counter did not increase"
+		echo "Error: $LOCAL_STATE values does match, counter did not increase"
 		LOCAL_ERROR=1
 	else
-		echo "Info: Values do not match, counter was increased"
+		echo "Info: $LOCAL_STATE values do not match, counter was increased"
 		echo "TEST PASSED"
 		LOCAL_ERROR=0
 	fi
