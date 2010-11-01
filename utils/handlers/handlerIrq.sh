6ef2e243323688f18b5c437c7626ac56aea5a413
Dan Murphy <dmurphy@ti.com>
2010-11-01 14:29:45 -0500

    utils/handlers: Modify the handlerIRQ to return the number of interrupts
    Update the handlerIRQ to return the number of interrupts for a particular
    device on a particular CPU.
    
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- utils/handlers/handlerIrq.sh
+++ utils/handlers/handlerIrq.sh
@@ -57,6 +57,29 @@ if [ "$LOCAL_OPERATION" = "get" ]; then
 			cat /proc/interrupts | grep $LOCAL_IRQ_NUMBER: | awk '{print $3}'
 		fi
 
+	elif [ "$LOCAL_OPTION" = "cpu0_irq" ] || [ "$LOCAL_OPTION" = "cpu1_irq" ]; then
+
+		LOCAL_DRIVER=$3
+		LOCAL_FILE=$4
+
+		if [ "$LOCAL_DRIVER" = "" ]; then
+			echo "Error: driver value was not provided"
+			exit 1
+		fi
+
+		if [ "$LOCAL_OPTION" = "cpu0_irq" ]; then
+			for i in `cat /proc/interrupts | grep -i $LOCAL_DRIVER | awk '{print$2}' | cut -d: -f1`
+			do
+				echo $i > $LOCAL_FILE
+				echo $i
+			done
+		elif [ "$LOCAL_OPTION" = "cpu1_irq" ]; then
+			for i in `cat /proc/interrupts | grep -i $LOCAL_DRIVER | awk '{print$3}' | cut -d: -f1`
+			do
+				echo $i > $LOCAL_FILE
+				echo $i
+			done
+		fi
 	else
 
 		echo "Fatal: Unsupported option"
