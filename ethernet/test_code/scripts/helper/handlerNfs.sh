6504e12fd76a63fde98b377a2d43ed422ca8799c
Abraham Arce <x0066660@ti.com>
2010-11-19 14:57:26 -0600

    TestSuites: Ethernet: NFS handler log information
    Increase the information when NFS handler is run by showing the content from:
    
     /proc/cmdline
     /proc/mounts
    
    This will be useful when looking at the ethernet testsuite issues
    and discard any problem if requirements from the scenarios are not met.
    
    Signed-off-by: Abraham Arce <x0066660@ti.com>

--- ethernet/test_code/scripts/helper/handlerNfs.sh
+++ ethernet/test_code/scripts/helper/handlerNfs.sh
@@ -21,10 +21,19 @@ if [ $? -eq 1 ]; then
 	exit 1
 fi
 
+echo -e "\nThe content of /proc/cmdline is\n"
+cat /proc/cmdline
+
+echo -e "\nThe content of /proc/mounts for /dev/root is\n"
+cat /proc/mounts | grep /dev/root
+
 if [ "$LOCAL_OPERATION" = "verify" ]; then
 
 	cat /proc/mounts | grep /dev/root | awk '{print $3}' | grep nfs
-	handlerError.sh "log" "$?" "halt" "handlerNfs.sh"
+	if [ $? -eq 1 ]; then
+		echo -e "\nFATAL: Root filesystem is not mounted over nfs\n"
+		handlerError.sh "log" "$?" "halt" "handlerNfs.sh | verify operation"
+	fi
 
 	LOCAL_ETHERNET_NFS_DIRECTORY=`mount | grep nfs | awk '{print $3}'`
 	echo $LOCAL_ETHERNET_NFS_DIRECTORY > $ETHERNET_NFS_DIR
@@ -34,8 +43,8 @@ elif [ "$LOCAL_OPERATION" = "deny" ]; then
 	cat /proc/mounts | grep /dev/root | awk '{print $3}' | grep nfs
 
 	if [ $? -eq 0 ]; then
-		echo "FATAL: This test case should NOT be executed on a Network Filesystem"
-		handlerError.sh "log" "1" "halt" "handlerNfs.sh"
+		echo -e "\nFATAL: This test case should NOT be executed on a Network Filesystem\n"
+		handlerError.sh "log" "1" "halt" "handlerNfs.sh | deny operation"
 	fi
 
 fi
