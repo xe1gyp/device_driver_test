3a777cfa4f1ffd0637616a26bd215199285b5d72
Leed Aguilar <leed.aguilar@ti.com>
2011-02-01 12:38:39 -0600

    handler: power-transition: Add GNU license
    Signed-off-by: Leed Aguilar <leed.aguilar@ti.com>

--- utils/handlers/handlerPowerTransitionStats.sh
+++ utils/handlers/handlerPowerTransitionStats.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+#
+#  Power Transitions Statistics handler
+#
+#  Copyright (c) 2010 Texas Instruments
+#
+#  Author: Arce Abraham <x0066660@ti.com>
+#  Author: Leed Aguilar <leed.aguilar@ti.com>
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+#  USA
+#
+
 # =============================================================================
 # Variables
 # =============================================================================
