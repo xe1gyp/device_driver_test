57fdc8868eeebcb839333ca1a0b0030ac5a798c3
Leed Aguilar <leed.aguilar@ti.com>
2011-02-02 17:50:52 -0600

    handler: code cleanup in android activity manager
    * Remove "LOCAL_" prefix for local variables
    * Use lower case letters for local variables
    * Setting a cleaner debug messages
    * Global variables are indetified with capital letters
    * Adding GNU General Public License
    
    Signed-off-by: Leed Aguilar <leed.aguilar@ti.com>

--- utils/handlers/handlerActivityManager.sh
+++ utils/handlers/handlerActivityManager.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
+
 #
+#  Android Activity Manager handler
+#
+#  Copyright (c) 2010 Texas Instruments
+#
+#  Author: Leed Aguilar <leed.aguilar@ti.com>
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+#  USA
+
 # Android Activity Manager handler: This handler is able to open
-# any Android APP by registering the Intent of the process using
+# any Android app by registering the Intent of the process using
 # the Activity Manager
 #
 # Processes supported:
@@ -16,76 +38,74 @@
 # For more information on the Android Activity Manager, visit:
 # http://developer.android.com/reference/android/app/Activity.html
 #
-# @author: Leed Aguilar
-#
 
 # TODO List
 # 1. Include more Android processes as needed
 # 2. Add more Android Errors and Warnings cases
 
 # =============================================================================
-# Variables
+# Local Variables
 # =============================================================================
 
-LOCAL_PROCESS=$1
-LOCAL_ACTION=$2
-LOCAL_FILE=$3
-LOCAL_ERROR=0
-LOCAL_NEED_SDCARD=0
-LOCAL_NEED_FILE=0
-LOCAL_SDCARD_MOUNT_POINT="/mnt/sdcard/"
+android_usecase=$1
+execution_status=$2
+multimedia_file=$3
+error_val=0
+need_scard=0
+need_mm_file=0
+sd_mount_point="/mnt/sdcard/"
 
 # Android Errors and Warnings
-LOCAL_AM_WARNING="Warning: Activity not started"
-LOCAL_AM_ERROR_CLASS="Error: Activity class"
-LOCAL_AM_ERROR_NAME="Error: Bad component name"
-LOCAL_AM_ERROR_FILE="Failed to open file"
-LOCAL_AM_ERROR_PREPARE="prepare failed"
-LOCAL_AM_ERROR_PARAM="unsupported parameter"
-LOCAL_AM_ERROR_TIMEOUT="Launch timeout has expired"
-LOCAL_AM_ERROR_CRASH="Activity destroy timeout for HistoryRecord"
+am_warning="Warning: Activity not started"
+am_error_class="Error: Activity class"
+am_error_name="Error: Bad component name"
+am_error_file="Failed to open file"
+am_error_prepare="prepare failed"
+am_error_param="unsupported parameter"
+am_error_timeout="Launch timeout has expired"
+am_error_crash="Activity destroy timeout for HistoryRecord"
 
 # Android Shell
-LOCAL_AM_SHELL="/system/bin/sh"
-LOCAL_AM="/system/bin/am"
-LOCAL_AM_EXECUTE="$LOCAL_AM_SHELL $LOCAL_AM  start -n "
+am_shell="/system/bin/sh"
+am_process="/system/bin/am"
+am_process_execute="$am_shell $am_process  start -n "
 
 
 # Android Processes
-LOCAL_ANDROID_MEDIA="com.cooliris.media"
-LOCAL_ANDROID_MUSIC="com.android.music"
-LOCAL_ANDROID_OMAP4_CAMERA="com.ti.omap4.android.camera"
-LOCAL_ANDROID_CAMERA="com.android.camera"
-LOCAL_ANDROID_RECORDER="com.android.soundrecorder"
-LOCAL_ANDROID_GEARS="luabear.gears4android"
-LOCAL_ANDROID_BROWSER="com.android.browser"
-LOCAL_ANDROID_SETTINGS="com.android.settings"
+android_media_process="com.cooliris.media"
+android_music_process="com.android.music"
+android_omap4_cam_process="com.ti.omap4.android.camera"
+android_cam_process="com.android.camera"
+android_sound_recorder_process="com.android.soundrecorder"
+android_gears_process="luabear.gears4android"
+android_browser_process="com.android.browser"
+android_setings_process="com.android.settings"
 
 # Android APPs
-LOCAL_APP_GALLERY="$LOCAL_ANDROID_MEDIA/.Gallery"
-LOCAL_APP_AV_PLAYBACK="$LOCAL_ANDROID_MEDIA/.MovieView"
-LOCAL_APP_OMAP4_CAMERA="$LOCAL_ANDROID_OMAP4_CAMERA/.Camera"
-LOCAL_APP_VIDEO_CAMERA="$LOCAL_ANDROID_OMAP4_CAMERA/.VideoCamera"
-LOCAL_APP_CAMERA="$LOCAL_ANDROID_CAMERA/.Camera"
-LOCAL_APP_SOUND_RECORDED="$LOCAL_ANDROID_RECORDER/.SoundRecorder"
-LOCAL_APP_AUDIO_PLAYBACK="$LOCAL_ANDROID_MUSIC/.MediaPlaybackActivity"
-LOCAL_APP_GEARS4ANDROID="$LOCAL_ANDROID_GEARS/.Gears4Android"
-LOCAL_APP_BROWSER="$LOCAL_ANDROID_BROWSER/.BrowserActivity"
-LOCAL_APP_SETTINGS="$LOCAL_ANDROID_SETTINGS/.Settings"
+app_gallery="$android_media_process/.Gallery"
+app_movie_view="$android_media_process/.MovieView"
+app_omap4_camera="$android_omap4_cam_process/.Camera"
+app_video_camera="$android_omap4_cam_process/.VideoCamera"
+app_camera="$android_cam_process/.Camera"
+app_sound_recorder="$android_sound_recorder_process/.SoundRecorder"
+app_media_playback="$android_music_process/.MediaPlaybackActivity"
+app_gears4android="$android_gears_process/.Gears4Android"
+app_browser="$android_browser_process/.BrowserActivity"
+app_setting="$android_setings_process/.Settings"
 
 # Execute APP commands
-LOCAL_RUN_AUDIO_PLAYBACK="$LOCAL_AM_EXECUTE $LOCAL_APP_AUDIO_PLAYBACK"
-LOCAL_RUN_AUDIO_RECORD="$LOCAL_AM_EXECUTE $LOCAL_APP_SOUND_RECORDED"
-LOCAL_RUN_AV_PLAYBACK="$LOCAL_AM_EXECUTE $LOCAL_APP_AV_PLAYBACK"
-LOCAL_RUN_AV_RECORD="$LOCAL_AM_EXECUTE $LOCAL_APP_VIDEO_CAMERA"
-LOCAL_RUN_IMAGE_CAPTURE_OMAP4="$LOCAL_AM_EXECUTE $LOCAL_APP_OMAP4_CAMERA"
-LOCAL_RUN_IMAGE_CAPTURE="$LOCAL_AM_EXECUTE $LOCAL_APP_CAMERA"
-LOCAL_RUN_IMAGE_DECODE="$LOCAL_AM_EXECUTE $LOCAL_APP_GALLERY"
-LOCAL_RUN_GEARS4ANDROID="$LOCAL_AM_EXECUTE $LOCAL_APP_GEARS4ANDROID"
-LOCAL_RUN_BROWSER="$LOCAL_AM_EXECUTE $LOCAL_APP_BROWSER"
-LOCAL_RUN_SETTINGS="$LOCAL_AM_EXECUTE $LOCAL_APP_SETTINGS"
+run_media_playback="$am_process_execute $app_media_playback"
+run_sound_recorder="$am_process_execute $app_sound_recorder"
+run_movie_view="$am_process_execute $app_movie_view"
+run_video_camera="$am_process_execute $app_video_camera"
+run_omap4_camera="$am_process_execute $app_omap4_camera"
+run_camera="$am_process_execute $app_camera"
+run_gallery="$am_process_execute $app_gallery"
+run_gears4android="$am_process_execute $app_gears4android"
+run_browser="$am_process_execute $app_browser"
+run_settings="$am_process_execute $app_setting"
 
-LOCAL_ANDROID_USECASE=( "audio_playback" "audio_record" "av_playback" \
+usecase_list=( "audio_playback" "audio_record" "av_playback" \
 			"av_record" "image_capture" "image_capture_omap4" \
 			"image_decode" "gears4android" "browser" "settings" )
 
@@ -94,50 +114,50 @@ LOCAL_ANDROID_USECASE=( "audio_playback" "audio_record" "av_playback" \
 # =============================================================================
 
 # Display the script usage
-# @ Function  : usage
+# @ Function: usage
 # @ parameters: None
-# @ Return    : Error flag value
+# @ Return: Error flag value
 usage() {
-	echo -e "\n-----------------------------------------------------\n"
+	echo -e "\n################# handlerActivityManager #################\n"
 	echo -e "  Usage: Try one of the folllowing options:\n"
-	echo -e "    - handlerActivityManager.sh <process> [start] <file>\n"
-	echo -e "    - handlerActivityManager.sh <process> [stop]\n"
-	echo -e "\n  Where <process> can be one of the following:\n"
-	for index in ${!LOCAL_MM_USECASE[*]}; do
-		echo -d "\t** ${LOCAL_ANDROID_USECASE[$index]}"
+	echo -e "    - handlerActivityManager.sh <usecase> [start] <file>\n"
+	echo -e "    - handlerActivityManager.sh <usecase> [stop]\n"
+	echo -e "\n  Where <usecase> can be one of the following:\n"
+	for index in ${!usecase_list[*]}; do
+		echo -d "\t** ${usecase_list[$index]}"
 	done
 	echo -e "\n  Third parameter <file> is optional, if it is given\n" \
-		" it can be used by some of the Android APPs\n"
-	echo -e "\n-----------------------------------------------------\t"
-	LOCAL_ERROR=1
+		" it can be used by some of the Android apps\n"
+	echo -e "\n################# handlerActivityManager #################\n"
+	error_val=1
 }
 
 # Detect if sdcard is mounted
 # Android sdcard mount point: /mnt/sdcard
 # Android creates a symlink: sdcard -> /mnt/sdcard
-# @ Function  : detectSdCard
+# @ Function: detectSdCard
 # @ Parameters: None
-# @ Return    : Error flag value
+# @ Return: Error flag value
 detectSdCard() {
 	if [ `df | grep -ic sdcard` -gt 0 ]; then
 		showInfo "DEBUG: SDCARD is mounted"
 	else
 		showInfo "ERROR: SDCARD is not mounted" 1>&2
-		LOCAL_ERROR=1
+		error_val=1
 	fi
 }
 
 # Find a file in an specific location
-# @ Function  : findFile
+# @ Function: findFile
 # @ Parameters: <file name> <location>
-# @ Return    : Error flag value
+# @ Return: Error flag value
 findFile() {
 	file=$1
 	location=$2
 	# Verify that "location" exist
 	if [ ! -d $location ]; then
 		showInfo "ERROR: $location path does not exist" 1>&2
-		LOCAL_ERROR=1
+		error_val=1
 		return
 	fi
 	# Search file in the path specified
@@ -145,15 +165,15 @@ findFile() {
 		showInfo "DEBUG: $file file found in $location"
 	else
 		showInfo "ERROR: $file file is not found, we can not proceed" 1>&2
-		LOCAL_ERROR=1
+		error_val=1
 	fi
 }
 
 # Execute an Android process and search for errors
 # and warnings when the process is being registered
-# @ Function  : executeAndroidProcess
+# @ Function: executeAndroidProcess
 # @ Parameters: <Android process>
-# @ Return    : Error flag value
+# @ Return: Error flag value
 executeAndroidProcess() {
 	android_process="$@"
 	# Clear logcat buffer
@@ -164,70 +184,67 @@ executeAndroidProcess() {
 	logcat -d > app_logcat
 	# When an app crashes the error log is not sent to standard error
 	# output, but it can be obtained from the logcat
-	if [ `cat app_logcat | grep -rc "$LOCAL_AM_ERROR_CRASH"` -gt 0 ]; then
-		showInfo "ERROR: The application $LOCAL_ANDROID_INTENT" \
+	if [ `cat app_logcat | grep -rc "$am_error_crash"` -gt 0 ]; then
+		showInfo "ERROR: The application $android_intent" \
 			 "has stopped unexpectedly" 1>&2
-		LOCAL_ERROR=1
+		error_val=1
 		return
-	elif [ `cat app_err | grep -rc "$LOCAL_AM_WARNING"` -gt 0  ]; then
+	elif [ `cat app_err | grep -rc "$am_warning"` -gt 0  ]; then
 		showInfo "WARNING: app is already running" 1>&2
 		# Keeping this warning as inonfensive by now
-		LOCAL_ERROR=0
+		error_val=0
 		return
-	elif [ `cat app_err  | grep -rc "$LOCAL_AM_ERROR_CLASS"` -gt 0 ]; then
+	elif [ `cat app_err  | grep -rc "$am_error_class"` -gt 0 ]; then
 		showInfo "ERROR: Activity class does not exist" 1>&2
-		LOCAL_ERROR=1
+		error_val=1
 		return
-	elif [ `cat app_err | grep -rc "$LOCAL_AM_ERROR_NAME"` -gt 0 ]; then
+	elif [ `cat app_err | grep -rc "$am_error_name"` -gt 0 ]; then
 		showInfo "ERROR: bad component name" 1>&2
-		LOCAL_ERROR=1
+		error_val=1
 		return
-	elif [ `cat app_err  | grep -rc "$LOCAL_AM_ERROR_FILE"` -gt 0 ]; then
+	elif [ `cat app_err  | grep -rc "$am_error_file"` -gt 0 ]; then
 		showInfo "ERROR: Failed to open file" 1>&2
-		LOCAL_ERROR=1
+		error_val=1
 		return
-	elif [ `cat app_err  | grep -rc "$LOCAL_AM_ERROR_PREPARE"` -gt 0 ]; then
+	elif [ `cat app_err  | grep -rc "$am_error_prepare"` -gt 0 ]; then
 		showInfo "ERROR: prepare failed: -17" 1>&2
-		LOCAL_ERROR=1
+		error_val=1
 		return
-	elif [ `cat app_err  | grep -rc "$LOCAL_AM_ERROR_PARAM"` -gt 0 ]; then
+	elif [ `cat app_err  | grep -rc "$am_error_param"` -gt 0 ]; then
 		showInfo "ERROR: unsupported parameter" 1>&2
-		LOCAL_ERROR=1
+		error_val=1
 		return
-	elif [ `cat app_err  | grep -rc "$LOCAL_AM_ERROR_TIMEOUT"` -gt 0 ]; then
+	elif [ `cat app_err  | grep -rc "$am_error_timeout"` -gt 0 ]; then
 		showInfo "ERROR: unsupported parameter" 1>&2
-		LOCAL_ERROR=1
+		error_val=1
 		return
 	else
-		showInfo "DEBUG: Testing $LOCAL_PROCESS"
+		showInfo "DEBUG: Testing $android_usecase"
 	fi
 }
 
 # Prints a message with a specific format
-# @ Function  : showInfo
+# @ Function: showInfo
 # @ Parameters: <message to display>
-# @ Return    : None
+# @ Return: None
 showInfo() {
-	echo -e "\n\n------------------- handlerActivityManager -------------------\n"
-	messages=( "$@" )
-	for index in ${!messages[@]}; do
-		echo -e "\t${messages[$index]}"
-	done
-	echo -e "\n--------------------------------------------------------------\n\n"
+	messages="$@"
+	echo "[ handlerActivityManager ] $messages"
 }
 
-# Verify LOCAL_ERROR flag
+# Verify the value of the error flag
 # if flag is set to '1' exit the script and register the failure
 # The message parameter helps to debug the script
-# @ Function  : verifyErrorFlag
+# @ Function: verifyErrorFlag
 # @ Parameters: <debug message>
-# @ Return    : None
+# @ Return: None
 verifyErrorFlag() {
 	debug_message=$1
-	if [ $LOCAL_ERROR -eq 1 ]; then
+	if [ $error_val -eq 1 ]; then
 		handlerError.sh "log" "1" "halt" "handlerActivityManager.sh"
-		showInfo "DEBUG: LOCAL ERROR DETECTED:" "$debug_message"  1>&2
-		exit $LOCAL_ERROR
+		showInfo "DEBUG: LOCAL ERROR DETECTED:" 1>&2
+		showInfo "$debug_message"  1>&2
+		exit $error_val
 	fi
 }
 
@@ -243,11 +260,11 @@ fi
 # Define Script Usage and validate all parameters
 # Third parameter <file> is optional. It will be evaluated later
 
-if [ ! `echo ${LOCAL_ANDROID_USECASE[@]} | grep -wc $LOCAL_PROCESS` -eq 1 ]; then
+if [ ! `echo ${usecase_list[@]} | grep -wc $android_usecase` -eq 1 ]; then
 	showInfo "USAGE: First parameter introduced is invalid" 1>&2
 	usage
 	verifyErrorFlag "usage(): evaluating 1st parameter"
-elif [ ! `echo "start stop" | grep -wc $LOCAL_ACTION` -eq 1 ]; then
+elif [ ! `echo "start stop" | grep -wc $execution_status` -eq 1 ]; then
 	showInfo "USAGE: Second parameter introduced is invalid" 1>&2
 	usage
 	verifyErrorFlag "usage(): evaluating 2nd parameter"
@@ -255,79 +272,79 @@ fi
 
 # Run/Stop Android usecases
 
-case $LOCAL_PROCESS in
+case $android_usecase in
 "audio_playback")
-	LOCAL_NEED_SDCARD=1
-	LOCAL_ANDROID_INTENT=$LOCAL_ANDROID_MUSIC
-	LOCAL_RUN=$LOCAL_RUN_AUDIO_PLAYBACK
+	need_scard=1
+	android_intent=$android_music_process
+	execute=$run_media_playback
 	;;
 "audio_record")
-	LOCAL_NEED_SDCARD=1
-	LOCAL_ANDROID_INTENT=$LOCAL_ANDROID_RECORDER
-	LOCAL_RUN=$LOCAL_RUN_AUDIO_RECORD
+	need_scard=1
+	android_intent=$android_sound_recorder_process
+	execute=$run_sound_recorder
 	;;
 "av_playback")
-	LOCAL_NEED_SDCARD=1
-	LOCAL_ANDROID_INTENT=$LOCAL_ANDROID_MEDIA
-	LOCAL_RUN=$LOCAL_RUN_AV_PLAYBACK
+	need_scard=1
+	android_intent=$android_media_process
+	execute=$run_movie_view
 	;;
 "av_record")
-	LOCAL_NEED_SDCARD=1
-	LOCAL_ANDROID_INTENT=$LOCAL_ANDROID_OMAP4_CAMERA
-	LOCAL_RUN=$LOCAL_RUN_AV_RECORD
+	need_scard=1
+	android_intent=$android_omap4_cam_process
+	execute=$run_video_camera
 	;;
 "image_capture_omap4")
-	LOCAL_NEED_SDCARD=1
-	LOCAL_ANDROID_INTENT=$LOCAL_ANDROID_OMAP4_CAMERA
-	LOCAL_RUN=$LOCAL_RUN_IMAGE_CAPTURE_OMAP4
+	need_scard=1
+	android_intent=$android_omap4_cam_process
+	execute=$run_omap4_camera
 	;;
 "image_capture")
-	LOCAL_NEED_SDCARD=1
-	LOCAL_ANDROID_INTENT=$LOCAL_ANDROID_CAMERA
-	LOCAL_RUN=$LOCAL_RUN_IMAGE_CAPTURE
+	need_scard=1
+	android_intent=$android_cam_process
+	execute=$run_camera
 	;;
 "image_decode")
-	LOCAL_NEED_SDCARD=1
-	LOCAL_ANDROID_INTENT=$LOCAL_ANDROID_MEDIA
-	LOCAL_RUN=$LOCAL_RUN_IMAGE_DECODE
+	need_scard=1
+	android_intent=$android_media_process
+	execute=$run_gallery
 	;;
 "gears4android")
-	LOCAL_ANDROID_INTENT=$LOCAL_ANDROID_GEARS
-	LOCAL_RUN=$LOCAL_RUN_GEARS4ANDROID
+	android_intent=$android_gears_process
+	execute=$run_gears4android
 	;;
 "browser")
-	LOCAL_ANDROID_INTENT=$LOCAL_ANDROID_BROWSER
-	LOCAL_RUN=$LOCAL_RUN_BROWSER
+	android_intent=$android_browser_process
+	execute=$run_browser
 	;;
 "settings")
-	LOCAL_ANDROID_INTENT=$LOCAL_ANDROID_SETTINGS
-	LOCAL_RUN=$LOCAL_RUN_SETTINGS
+	android_intent=$android_setings_process
+	execute=$run_settings
 	;;
 *)
 	usage
 	verifyErrorFlag "usage(): Local process is not valid"
 esac
 
-case $LOCAL_ACTION in
+case $execution_status in
 "start")
 	# Verify if the SDCARD is needed
-	if [ $LOCAL_NEED_SDCARD -eq 1 ]; then
+	if [ $need_scard -eq 1 ]; then
 		detectSdCard
 		verifyErrorFlag "detectSdCard(): Verify if the SDCARD is needed"
 	fi
-	# If LOCAL_FILE is set, verify it and use it
-	if [[ ! -z $LOCAL_FILE ]]; then
-		findFile $LOCAL_FILE $LOCAL_SDCARD_MOUNT_POINT
-		verifyErrorFlag "findFile(): evaluating LOCAL_FILE parameter"
+	# If multimedia_file is set, verify it and use it
+	if [[ ! -z $multimedia_file ]]; then
+		findFile $multimedia_file $sd_mount_point
+		verifyErrorFlag "Evaluating multimedia file parameter"
 		# Let's use the file with the Android app
-		new_execute_command="${LOCAL_RUN} -d $LOCAL_SDCARD_MOUNT_POINT/$LOCAL_FILE"
-		LOCAL_RUN=$new_execute_command
+		new_exec_cmd="${execute} -d $sd_mount_point/$multimedia_file"
+		execute=$new_exec_cmd
 	fi
-	executeAndroidProcess $LOCAL_RUN
+	executeAndroidProcess $execute
 	verifyErrorFlag "executeAndroidProcess()"
 	;;
 "stop")
-	killall $LOCAL_ANDROID_INTENT
+	killall $android_intent
 	;;
 *)
 	usage
@@ -335,6 +352,6 @@ case $LOCAL_ACTION in
 	;;
 esac
 
-exit $LOCAL_ERROR
+exit $error_val
 
 # End of file
