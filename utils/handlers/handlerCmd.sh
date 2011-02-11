0f567d1d33cd75f6214599a699548752a24a2b2d
Leed Aguilar <leed.aguilar@ti.com>
2011-02-11 16:37:35 -0600

    handler: code cleanup in command handler
    * Remove "LOCAL_" prefix for local variables
    * Use lower case letters for local variables
    * Setting a cleaner debug messages
    * Global variables are indetified with capital letters
    * Adding GNU General Public License
    
    Signed-off-by: Leed Aguilar <leed.aguilar@ti.com>
    Signed-off-by: G, Manjunath Kondaiah <manjugk@ti.com>

--- utils/handlers/handlerCmd.sh
+++ utils/handlers/handlerCmd.sh
@@ -7,79 +7,99 @@
 #                                                                             #
 ###############################################################################
 
+#  Copyright (c) 2010 Texas Instruments
+#
+#  Author: Leed Aguilar <leed.aguilar@ti.com>
+#  Author: Arce Abraham <abraham.arce.moreno@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+#  USA
+#
+
 # =============================================================================
 # Variables
 # =============================================================================
 
-LOCAL_OPERATION=$1
-LOCAL_OPERAND2=$2
-LOCAL_OPERAND3=$3
-LOCAL_OPERAND4=$4
-LOCAL_COUNTER=0
-LOCAL_ERROR=0
+operation=$1
+operand2=$2
+operand3=$3
+operand4=$4
+counter=0
+error_val=0
 
 # =============================================================================
 # Functions
 # =============================================================================
 
 # Prints a message with a specific format
-# @ Function  : showInfo
+# @ Function: showInfo
 # @ Parameters: <message to display>
-# @ Return    : None
+# @ Return: None
 showInfo() {
-	echo -e "\n\n----------------------- handlerCmd ---------------------------\n"
 	messages=( "$@" )
 	for index in ${!messages[@]}; do
-		echo -e "\t${messages[$index]}"
+		echo  "[ handlerCmd ]: ${messages[$index]}"
 	done
-	echo -e "\n--------------------------------------------------------------\n\n"
 }
 
-# Verify LOCAL_ERROR flag
+# Verify error_val flag
 # if flag is set to '1' exit the script and register the failure
 # The message parameter helps to debug the script
-# @ Function  : verifyErrorFlag
+# @ Function: verifyErrorFlag
 # @ Parameters: <debug message>
-# @ Return    : None
+# @ Return: None
 verifyErrorFlag() {
 	debug_message=$1
-	if [ $LOCAL_ERROR -eq 1 ]; then
+	if [ $error_val -eq 1 ]; then
 		handlerError.sh "log" "1" "halt" "handlerCmd.sh"
-		showInfo "\tDEBUG: LOCAL ERROR DETECTED:" "$debug_message"  1>&2
-		exit $LOCAL_ERROR
+		showInfo "ERROR: $debug_message"  1>&2
+		exit $error_val
 	fi
 }
 
 # Verify is the parameter is a valid number (integer)
-# @ Function  : isPositiveInteger
+# @ Function: isPositiveInteger
 # @ Parameters: <number>
-# @ Return    : Error flag value
+# @ Return: Error flag value
 isPositiveInteger() {
 	num=$1
 	if ! [[ $num =~ ^[0-9]+$ ]]; then
-		showInfo "\tERROR: [ $num ] is not a number" 1>&2
-		LOCAL_ERROR=1
+		showInfo "ERROR: ( $num ) is not a number" 1>&2
+		error_val=1
 	fi
 }
 
 # Display the script usage
-# @ Function  : generalUsage
+# @ Function: usage
 # @ parameters: None
-# @ Return    : Error flag value
-generalUsage() {
+# @ Return: Error flag value
+usage() {
 	cat <<-EOF >&1
 
-	################################################################
+	####################### handlerCmd #######################
 
 	 Script Usage:
 
-	$ handlerCmd.sh run {commands}
+	  $ handlerCmd.sh run {commands}
 	  $ handlerCmd.sh iterate {commands} {iterations} {delay}
 
 	Where:
-	  @ commands: commands to be executed. Multiple commands can be
-		      given by wraping the list of commands with single
-		      quotes and separate each command with a semicolon
+	  @ commands: commands to be executed. Multiple commands
+		      can be given by wraping the list of commands
+		      with single quotes and separate each command
+		      with a semicolon
 	  @ iterations: number of iterations
 	  @ delay: delay between iteration (seconds)
 
@@ -89,29 +109,30 @@ generalUsage() {
 	  $ handlerCmd.sh iterate'echo A' 10 2
 
 	  Example: Iterate with multiple commands:
-	  B) iterates "echo A" and "echo B" 10 times with a delay of 2s
+	  B) iterates "echo A" and "echo B" 10 times with a delay
+	     of 2 seconds
 
 	  $ handlerCmd.sh iterate 'echo A;echo B' 10 2
 
-	################################################################
+	####################### handlerCmd #######################
 
 	EOF
-	LOCAL_ERROR=1
+	error_val=1
 }
 
 # Create an array of different commands and then
 # iterates the array to execute each command
-# @ Function  : executeCommands
+# @ Function: executeCommands
 # @ Parameters: <commands to execute>
-# @ Return    : Error flag value
+# @ Return: Error flag value
 executeCommands() {
 	toexecute=( "$@" )
 	for index in ${!toexecute[*]}; do
 		execute=`echo "${toexecute[$index]}" | tr "," " "`
 		eval $execute
 		if [ $? -gt 0 ]; then
-			showInfo "ERROR: [ $execute ] is not a valid command" 2>&1
-			LOCAL_ERROR=1
+			showInfo "ERROR: < $execute > execution failed" 2>&1
+			error_val=1
 		fi
 	done
 }
@@ -127,60 +148,56 @@ fi
 
 # Evaluate parameters and script usage
 
-if [ "$LOCAL_OPERATION" = "run" ]; then
+if [ "$operation" = "run" ]; then
 	if [ $# -ne 2 ]; then
-		showInfo "ERROR: Number of parameters is invalid" 2>&1
-		generalUsage
-		verifyErrorFlag "generaUsage(): Number of parameters is invalid"
+		usage
+		verifyErrorFlag "Number of parameters is invalid"
 	fi
-elif [ "$LOCAL_OPERATION" = "iterate" ]; then
+elif [ "$operation" = "iterate" ]; then
 	if [ $# -ne 4  ]; then
-		showInfo "ERROR: Number of parameters is invalid" 2>&1
-		generalUsage
-		verifyErrorFlag "generaUsage(): Number of parameters is invalid"
+		usage
+		verifyErrorFlag "Number of parameters is invalid"
 	else
-		isPositiveInteger $LOCAL_OPERAND3
-		verifyErrorFlag "generaUsage(): second parameter is incorrect"
-		isPositiveInteger $LOCAL_OPERAND4
-		verifyErrorFlag "generaUsage(): third parameter is incorrect"
+		isPositiveInteger $operand3
+		verifyErrorFlag "Second parameter is incorrect"
+		isPositiveInteger $operand4
+		verifyErrorFlag "Third parameter is incorrect"
 	fi
 else
-	generalUsage
-	verifyErrorFlag "generaUsage(): operation introduced is incorrect"
+	usage
+	verifyErrorFlag "Operation introduced is incorrect"
 fi
 
 # Execute scripts operations
 
-case $LOCAL_OPERATION in
+case $operation in
 "run")
-	LOCAL_COMMANDS=$LOCAL_OPERAND2
-	commands=`echo -e $LOCAL_COMMANDS | tr " " "," | tr ';' ' '`
+	commands=`echo -e $operand2 | tr " " "," | tr ';' ' '`
 	executeCommands $commands
-	verifyErrorFlag "executeCommands(): command not valid"
+	verifyErrorFlag "executeCommands reported an error"
 	;;
 "iterate")
 	# set the required variables
-	LOCAL_COMMANDS=$LOCAL_OPERAND2
-	LOCAL_ITERATIONS=$LOCAL_OPERAND3
-	LOCAL_DELAY=$LOCAL_OPERAND4
+	iterations=$operand3
+	delay=$operand4
 
-	while [ $LOCAL_COUNTER -lt $LOCAL_ITERATIONS ]; do
+	while [ $counter -lt $iterations ]; do
 		# print iteration number in a non zero based indexing
-		let LOCAL_ITERATION=$LOCAL_COUNTER+1
-		showInfo "\t      ITERATION: $LOCAL_ITERATION"
-		commands=`echo -e $LOCAL_COMMANDS | tr " " "," | tr ';' ' '`
+		let report_iteration=$counter+1
+		showInfo "ITERATION::::::::::::::: $report_iteration"
+		commands=`echo -e $operand2 | tr " " "," | tr ';' ' '`
 		executeCommands $commands
-		verifyErrorFlag "executeCommands(): command not valid"
-		sleep $LOCAL_DELAY
-		let LOCAL_COUNTER=LOCAL_COUNTER+1
+		verifyErrorFlag "executeCommands reported an error"
+		sleep $delay
+		let counter=counter+1
 	done
 	;;
 *)
-	generalUsage
-	verifyErrorFlag "generalUsage(): invalid operation"
+	usage
+	verifyErrorFlag "usage(): invalid operation"
 	;;
 esac
 
-exit $LOCAL_ERROR
+exit $error_val
 # End of file
 
