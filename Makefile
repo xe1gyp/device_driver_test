xe1gyp/device_driver_test

commit 097bf93cccc543996ae184898e9a2bad2e20f779

Fix: make dma tests compilable for omap1
The dma test code will not compile for omap1 with main line kernel.
Fixes the same and other changes are done to handle android kernel.

Signed-off-by: G, Manjunath Kondaiah <manjugk@ti.com>

Makefile
@@ -35,21 +35,16 @@ SCENARIOS	:= scripts/scenarios
 OLD_KDIR:=$(shell cat .config | grep KDIR | cut -d'=' -f2)
 OLD_SUITES:=$(shell cat .config | grep TESTSUITES | cut -d'=' -f2)
 
+ifdef CONFIG_ARCH_OMAP1
+TARGET_PLATFORM:=OMAP_1710
+else
+TARGET_PLATFORM:=OMAP2PLUS
+endif
+
 ifdef CONFIG_ARCH_OMAP4
-TARGET_PLATFORM:=OMAP_4430
+TARGET_BOARD:=OMAP_4430
 endif
-ifdef CONFIG_MACH_OMAP_3430SDP
-TARGET_PLATFORM:=OMAP_3430
-endif
-ifdef CONFIG_MACH_OMAP_ZOOM2
-TARGET_PLATFORM:=OMAP_3430
-endif
-ifdef CONFIG_MACH_OMAP_3630SDP
-TARGET_PLATFORM:=OMAP_3630
-endif
-ifdef CONFIG_MACH_OMAP_ZOOM3
-TARGET_PLATFORM:=OMAP_3630
-endif
+
 ifdef CONFIG_ANDROID
 TARGET_FILESYSTEM:=ANDROID
 CFLAGS+=-static

video/test_code/utils/video/Makefile
@@ -5,7 +5,7 @@ setbgcolor settransck enablecolkey setcolorconv setdefcolcon streaming_process \
 tv_detect_app createRGB streaming_usrptr v4l2_rolling_bars streaming_tear v4l2_ramdomcolorblocks \
 streaming_repeat \
 
-ifeq ($(TARGET_PLATFORM), OMAP_4430)
+ifeq ($(TARGET_BOARD), OMAP_4430)
 	OBJS += streaming_wb
 endif
 

catchall/test_code/utils/apps/Makefile
@@ -14,10 +14,6 @@ endif
 
 MODULES = debug_module_test.o
 
-ifeq ($(TARGET_PLATFORM), OMAP_4430)
-MODULES += debug_module_test.o
-endif
-
 obj-m:=$(MODULES)
 
 all: $(TARGETS)

dma/test_code/utils/dma/Makefile
@@ -13,7 +13,6 @@ SUITE_DIR:=$(PWD)
 endif
 
 MODULES = dma_single_channel.o \
-  dma_chain_transfer.o \
   dma_priority_high.o \
   dma_priority_low.o \
   dma_stop_transfer.o \
@@ -25,30 +24,34 @@ MODULES = dma_single_channel.o \
   dma_burst_mode4.o \
   dma_burst_mode8.o \
   dma_burst_mode16.o \
+  dma_static_self_link.o \
+  dma_dynamic_self_link.o \
+  dma_unlink_nonlinked_channels.o  \
+  dma_unlink_before_stop_channels.o \
+  dma_stop_transfer_invalid.o \
+  dma_stop_transfer_twice.o \
+  dma_thread_access.o
+
+ ifeq ($(TARGET_PLATFORM), OMAP2PLUS)
+ MODULES +=  dma_chain_transfer.o \
   dma_chain_static.o \
   dma_chain_dynamic.o \
   dma_chain_dynamic_removed.o \
   dma_chain_static_removed.o \
   dma_chain_static_invalid.o \
   dma_chain_dynamic_invalid.o \
-  dma_static_self_link.o \
-  dma_dynamic_self_link.o \
-  dma_unlink_nonlinked_channels.o  \
-  dma_unlink_before_stop_channels.o \
-  dma_stop_transfer_invalid.o \
   dma_chain_dynamic_break.o \
   dma_chain_static_break.o \
-  dma_stop_transfer_twice.o \
-  dma_thread_access.o \
   dma_chain_thread_access.o \
-  dma_chain_thread_stress.o \
-  dma_addrmode_constant_oswr.o \
-  dma_register_dump.o \
-  dma_oswr_trigger.o
+  dma_chain_thread_stress.o
+ endif
 
- ifeq ($(TARGET_PLATFORM), OMAP_4430)
+ ifeq ($(TARGET_FILESYSTEM), ANDROID)
  MODULES += dma_dload.o \
-	    dma_dload_thread_access.o
+  	    dma_addrmode_constant_oswr.o \
+	    dma_dload_thread_access.o \
+            dma_oswr_trigger.o \
+            dma_register_dump.o
  endif
 
 obj-m:=$(MODULES)

dma/test_code/utils/dma/dma_single_channel.h
@@ -49,12 +49,14 @@
 
 #define PROC_FILE	"driver/dma_test_status"
 
+#ifdef CONFIG_ANDROID
 enum {
 CCR = 0, CLNK_CTRL, CICR, CSR, CSDP,
 CEN, CFN, CSSA, CDSA, CSEI, CSFI,
 CDEI, CDFI, CSAC, CDAC, CCEN, CCFN,
 COLOR, CH_END
 };
+#endif
 
 /*
  * An struct that represents the buffers used for the transfer

commit e53585a5a6fbfb89a39613b959f2699300bc7926

TestSuite: Makefile: Fix issue with TARGET_PLATFORM definition
This commit 097bf93cccc543996ae184898e9a2bad2e20f779 introduced
a problem with the definition of TARGET_PLATFORM for OMAP4.

If CONFIG_ARCH_OMAP4 is defined, then TARGET_PLATFORM should be
set to OMAP_4430 and not TARGET_BOARD. With the current Makefile,
make will always build for OMAP2PLUS even when CONFIG_ARCH_OMAP4
has been defined. This patch fixes the problem.

Signed-off-by: Christina Warren <cawarren@ti.com>
Acked-by: Leed Aguilar <leed.aguilar@ti.com>

Makefile
@@ -42,7 +42,7 @@ TARGET_PLATFORM:=OMAP2PLUS
 endif
 
 ifdef CONFIG_ARCH_OMAP4
-TARGET_BOARD:=OMAP_4430
+TARGET_PLATFORM:=OMAP_4430
 endif
 
 ifdef CONFIG_ANDROID

video/test_code/utils/video/Makefile
@@ -5,7 +5,7 @@ setbgcolor settransck enablecolkey setcolorconv setdefcolcon streaming_process \
 tv_detect_app createRGB streaming_usrptr v4l2_rolling_bars streaming_tear v4l2_ramdomcolorblocks \
 streaming_repeat \
 
-ifeq ($(TARGET_BOARD), OMAP_4430)
+ifeq ($(TARGET_PLATFORM), OMAP_4430)
 	OBJS += streaming_wb
 endif
 