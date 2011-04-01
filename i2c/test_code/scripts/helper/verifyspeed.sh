16011da5a9c2af0b213335a7edd97e2f7621cac8
Leed Aguilar <leed.aguilar@ti.com>
2011-04-01 04:49:16 -0500

    Testsuites: fix verifyspeed utility in i2c testsuites
    verifiyspeed was trying to read the Kernel ring buffer
    from a non-existing file. By using system_message_buffer
    utility, veryspeed can obtain the i2c bus 1 speed info
    
    This is a demo for verifyspeed.sh on a Blaze OMAP4
    platform running 2.6.35 Kernel:
    
    $ verifyspeed.sh
    
    Working with Bus 1 with speed 400
    i2c_omap i2c_omap.1: bus 1 rev4.0 at 400 kHz
    
    This solves some issues on the i2c testsuite execution
    
    Signed-off-by: Leed Aguilar <leed.aguilar@ti.com>

--- i2c/test_code/scripts/helper/verifyspeed.sh
+++ i2c/test_code/scripts/helper/verifyspeed.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-ADAPTER_INFO=`cat /tmp/result.tmp | grep i2c | grep "bus 1"`
+system_message_buffer.sh "system.log" || exit 1
+ADAPTER_INFO=`cat "system.log" | grep i2c | grep "bus 1"`
 SPEED_ACTUAL=`echo $ADAPTER_INFO | sed -e "s/ */ /g" | cut -d ' ' -f8`
+rm "system.log" || exit 1
 
-echo
+echo ""
 echo "Working with Bus 1 with speed $SPEED_ACTUAL"
-echo $ADAPTER_INFO
-echo
+echo "$ADAPTER_INFO"
+echo ""
 
 # End of file
